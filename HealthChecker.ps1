3a116f59945e98753e2a5a1824a9f2bb897b09fe
dpaulson45 <dpaul@microsoft.com>
2017-01-26 10:43:41 -0600

    Version Change v2.2
    Version Change v2.2

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -85,7 +85,7 @@ param(
 Note to self. "New Release Update" are functions that i need to update when a new release of Exchange is published
 #>
 
-$healthCheckerVersion = "2.1"
+$healthCheckerVersion = "2.2"
 $VirtualizationWarning = @"
 Virtual Machine detected.  Certain settings about the host hardware cannot be detected from the virtual machine.  Verify on the VM Host that: 
 
