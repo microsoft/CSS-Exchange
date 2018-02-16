4eab46232d9d40b4af4ef8d45fce6643d309f97b
David Paulson <dpaul@microsoft.com>
2018-02-16 11:01:08 -0600

    Check In
    First check in. Not finished, just need to switch branches

---
+++ ActionItemsSummary.ps1
@@ -0,0 +1,84 @@
+param(
+[string]$DirectoryPath = (Get-Location).Path
+)
+
+Function Get-HealthCheckFilesItemsFromLocation{
+    $items = Get-ChildItem $DirectoryPath | Where-Object{$_.Name -like "HealthCheck-*-*.xml"}
+    if($items -eq $null)
+    {
+        Write-Host("Doesn't appear to be any Health Check XML files here....stopping the script")
+        exit 
+    }
+    return $items
+}
+
+Function Get-OnlyRecentUniqueServersXMLs {
+param(
+[Parameter(Mandatory=$true)][array]$FileItems 
+)   
+
+    $aObject = @() 
+    foreach($item in $FileItems)
+    {
+        $obj = New-Object PSCustomobject 
+        [string]$itemName = $item.Name
+        $ServerName = $itemName.Substring(($itemName.IndexOf("-") + 1), ($itemName.LastIndexOf("-") - $itemName.IndexOf("-") - 1))
+        $obj | Add-Member -MemberType NoteProperty -Name ServerName -Value $ServerName
+        $obj | Add-Member -MemberType NoteProperty -Name FileName -Value $itemName
+        $obj | Add-Member -MemberType NoteProperty -Name FileObject -Value $item 
+        $aObject += $obj
+    }
+
+    $grouped = $aObject | Group-Object ServerName 
+
+    $FilePathList = @()
+    foreach($gServer in $grouped)
+    {
+        
+        if($gServer.Count -gt 1)
+        {
+            #going to only use the most current file for this server providing that they are using the newest updated version of Health Check we only need to sort by name
+            $groupData = $gServer.Group #because of win2008
+            $FilePathList += ($groupData | Sort-Object FileName -Descending | Select-Object -First 1).FileObject.VersionInfo.FileName
+
+        }
+        else {
+            $FilePathList += ($gServer.Group).FileObject.VersionInfo.FileName
+        }
+        
+    }
+
+    return $FilePathList
+}
+
+Function Import-MyData {
+param(
+[Parameter(Mandatory=$true)][array]$FilePaths
+)
+    [System.Collections.Generic.List[System.Object]]$myData = New-Object -TypeName System.Collections.Generic.List[System.Object]
+    foreach($filePath in $FilePaths)
+    {
+        $importData = Import-Clixml -Path $filePath
+        $myData.Add($importData)
+    }
+    return $myData
+}
+
+Function Action-Review{
+param(
+[Parameter(Mandatory=$true)][array]$ServersHealthReport
+)
+    
+}
+
+Function Main {
+    
+    $Files = Get-HealthCheckFilesItemsFromLocation
+    $FullPaths = Get-OnlyRecentUniqueServersXMLs $Files
+    $importData = Import-MyData -FilePaths $FullPaths
+
+
+
+}
+
+Main 
