microsoft/CSS-Exchange

commit 5560134f06e4d1021cdeeb2d4e0a6b29a1dcf2e7

Addressed possible null Driver Date on adapter objects
If we aren't able to get the advanced driver options within Windows 2012 R2 machines, we can't get the driver date. This causes the value to be null and will throw an error when trying to do New-TimeSpan. To avoid this we are doing the following:

-Placing the value to [DateTime]::MaxValue
-Also checking for $adapter.DriverDate if it is null

Resolved #149

HealthChecker.ps1
@@ -753,6 +753,7 @@ param(
             Invoke-CatchActions
             Write-VerboseOutput("Failed to get Windows2012R2 or greater advanced NIC settings. Error {0}." -f $Error[0].Exception)
             Write-VerboseOutput("Going to attempt to get WMI Object Win32_NetworkAdapter on this machine instead")
+            Write-VerboseOutput("NOTE: this means we aren't able to provide the driver date")
             $NetworkCards2008 = Get-WmiObject -ComputerName $Machine_Name -Class Win32_NetworkAdapter | ?{$_.NetConnectionStatus -eq 2}
             foreach($adapter in $NetworkCards2008)
             {
@@ -761,6 +762,7 @@ param(
                 $nicObject.Name = $adapter.Name
                 $nicObject.LinkSpeed = $adapter.Speed
                 $nicObject.NICObject = $adapter 
+                $nicObject.DriverDate = [DateTime]::MaxValue;
                 $aNICObjects += $nicObject
             }
         }
@@ -3810,11 +3812,18 @@ param(
             Write-Grey(("`tInterface Description: {0} [{1}] " -f $adapter.Description, $adapter.Name))
             if($HealthExSvrObj.HardwareInfo.ServerType -eq [HealthChecker.ServerType]::Physical)
             {
-                if((New-TimeSpan -Start $date -End $adapter.DriverDate).Days -lt [int]-365)
+                if($adapter.DriverDate -ne $null -and (New-TimeSpan -Start $date -End $adapter.DriverDate).Days -lt [int]-365)
                 {
                     Write-Yellow("`t`tWarning: NIC driver is over 1 year old. Verify you are at the latest version.")
                 }
-                Write-Grey("`t`tDriver Date: " + $adapter.DriverDate)
+                elseif($adapter.DriverDate -eq $null -or $adapter.DriverDate -eq [DateTime]::MaxValue)
+                {
+                    Write-Grey("`t`tDriver Date: Unknown")
+                }
+                else 
+                {
+                    Write-Grey("`t`tDriver Date: " + $adapter.DriverDate)
+                }
                 Write-Grey("`t`tDriver Version: " + $adapter.DriverVersion)
                 Write-Grey("`t`tLink Speed: " + $adapter.LinkSpeed)
             }

commit f69af7dd3e0a5b1d2c80eeff03484e0f5381437d

Need to provide driver date within the newly created if else statement
Addressed issue caused in 5560134f06e4d1021cdeeb2d4e0a6b29a1dcf2e7

Full Solved #149

HealthChecker.ps1
@@ -3815,6 +3815,7 @@ param(
                 if($adapter.DriverDate -ne $null -and (New-TimeSpan -Start $date -End $adapter.DriverDate).Days -lt [int]-365)
                 {
                     Write-Yellow("`t`tWarning: NIC driver is over 1 year old. Verify you are at the latest version.")
+                    Write-Grey("`t`tDriver Date: " + $adapter.DriverDate)
                 }
                 elseif($adapter.DriverDate -eq $null -or $adapter.DriverDate -eq [DateTime]::MaxValue)
                 {