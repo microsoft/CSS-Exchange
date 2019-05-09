587ba2ee55550aeafe6fe5d2f24e8333cfaac9fe
David Paulson <dpaul@microsoft.com>
2019-05-09 10:39:59 -0500

    Fixed Typo

--- ExchangeLogCollector.ps1
+++ ExchangeLogCollector.ps1
@@ -2,7 +2,7 @@
 .NOTES
     Name: ExchangeLogCollector.ps1
     Author: David Paulson
-    Requires: Powershell on an Exchange 2010+ Server with Adminstrator rights
+    Requires: Powershell on an Exchange 2010+ Server with Administrator rights
 
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
 	BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
