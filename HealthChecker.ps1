a198da0b2501058f640aaab83ec064cf9f993e17
David Paulson <dpaul@microsoft.com>
2019-06-06 15:45:04 -0500

    Fixed null-valued expression in Write-NICPacketReceivedDiscarded (#186)
    return out of the Write-NICPacketReceivedDiscarded function if the $HealthExSvrObj.OSVersion.PacketsReceivedDiscarded value is null
    
    Resolved #183

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -3792,6 +3792,11 @@ param(
     )
         $cookedValue = 0
         $foundCounter = $false 
+        if($HealthExSvrObj.OSVersion.PacketsReceivedDiscarded -eq $null)
+        {
+            Write-VerboseOutput("HealthExSvrObj.OSVersion.PacketsReceivedDiscarded is null")
+            return
+        }
         foreach($instance in $HealthExSvrObj.OSVersion.PacketsReceivedDiscarded)
         {
             $instancePath = $instance.Path 
