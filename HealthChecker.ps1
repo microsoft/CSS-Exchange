4617df7d607a627c7ab3e57d3bc532916a4ceac9
David Paulson <dpaul@microsoft.com>
2019-06-12 16:58:43 -0500

    Updated Get-NICInformation to Get-AllNicInformation template
    Created a template to handle the abilitity of getting all basic NIC information.
    
    Removed the redundant code from before when doing this.
    
    Addressed #168

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -835,102 +835,119 @@ param(
     return $page_obj
 }
 
-#TODO: V3.0 https://github.com/dpaulson45/HealthChecker/issues/168
-Function Get-NICInformation {
-param(
-[Parameter(Mandatory=$true)][string]$MachineName,
-[Parameter(Mandatory=$true)][HealthChecker.OSVersionName]$OSVersion
-)
-
-    Write-VerboseOutput("Calling: Get-NICInformation")
-    Write-VerboseOutput("Passed: $MachineName")
-    Write-VerboseOutput("Passed: $OSVersion")
-
-    [array]$aNICObjects = @() 
-    if($OSVersion -ge [HealthChecker.OSVersionName]::Windows2012R2)
+#Master Template: https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Get-AllNicInformation/Get-AllNicInformation.ps1
+Function Get-AllNicInformation {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)][string]$ComputerName,
+    [Parameter(Mandatory=$false)][bool]$Windows2012R2AndAbove = $true,
+    [Parameter(Mandatory=$false)][string]$ComputerFQDN,
+    [Parameter(Mandatory=$false)][scriptblock]$CatchActionFunction
+    )
+    #Function Version 1.0
+    <# 
+    Required Functions: 
+        https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
+        https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Get-WmiObjectHandler/Get-WmiObjectHandler.ps1
+    #>
+    Write-VerboseWriter("Calling: Get-AllNicInformation")
+    Write-VerboseWriter("Passed [string]ComputerName: {0} | [bool]Windows2012R2AndAbove: {1} | [string]ComputerFQDN: {2}" -f $ComputerName, $Windows2012R2AndAbove, $ComputerFQDN)
+    
+    Function Get-NetworkCards {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)][string]$ComputerName
+    )
+        try 
+        {
+            $cimSession = New-CimSession -ComputerName $ComputerName -ErrorAction Stop 
+            $networkCards = Get-NetAdapter -CimSession $cimSession | Where-Object{$_.MediaConnectionState -eq "Connected"} -ErrorAction Stop
+            return $networkCards
+        }
+        catch 
+        {
+            Write-VerboseWriter("Failed to attempt to get Windows2012R2 or greater advanced NIC settings in Get-NetworkCards. Error {0}." -f $Error[0].Exception)
+            throw 
+        }
+    }
+    
+    Function Get-WmiNetworkCards {
+        return (Get-WmiObjectHandler -ComputerName $ComputerName -Class "Win32_NetworkAdapter" -Filter "NetConnectionStatus ='2'" -CatchActionFunction $CatchActionFunction)
+    }
+    
+    Function New-NICInformation {
+    param(
+    [array]$Adapters,
+    [bool]$Windows2012R2AndAbove = $true
+    )
+        if($Adapters -eq $null)
+        {
+            Write-VerboseWriter("Adapters are null in New-NICInformation. Returning a null object.")
+            return $null
+        }
+        [array]$nicObjects = @()
+        foreach($adapter in $Adapters)
+        {
+            if($Windows2012R2AndAbove){$descritpion = $adapter.InterfaceDescription}else {$descritpion = $adapter.Description}
+            if($Windows2012R2AndAbove){$driverVersion = $adapter.DriverVersionString}else {$driverVersion = [string]::Empty}
+            if($Windows2012R2AndAbove){$driverDate = $adapter.DriverDate}else{$driverDate = [DateTime]::MaxValue}
+            if($Windows2012R2AndAbove){$mtuSize = $adapter.MtuSize}else{$mtuSize = 0}
+            $nicInformationObj = New-Object PSCustomObject
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "Name" -Value ($adapter.Name)
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "LinkSpeed" -Value ((($adapter.Speed)/1000000).ToString() + " Mbps")
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "DriverDate" -Value $driverDate
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "NICObject" -Value $adapter
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "IPv6Enabled" -Value $false
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "Description" -Value $descritpion 
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "DriverVersion" -Value $driverVersion
+            $nicInformationObj | Add-Member -MemberType NoteProperty -Name "MTUSize" -Value $mtuSize
+            $nicObjects += $nicInformationObj 
+        }
+        Write-VerboseWriter("Found {0} active adapters on the computer." -f $nicObjects.Count)
+        Write-VerboseWriter("Exiting: Get-AllNicInformation")
+        return $nicObjects 
+    }
+    
+    if($Windows2012R2AndAbove)
     {
-        Write-VerboseOutput("Detected OS Version greater than or equal to Windows 2012R2")
+        Write-VerboseWriter("Windows OS Version greater than or equal to Windows 2012R2. Going to run Get-NetAdapter")
         try 
         {
             try 
             {
-                $cimSession = New-CimSession -ComputerName $MachineName -ErrorAction Stop 
-                $NetworkCards = Get-NetAdapter -CimSession $cimSession | ?{$_.MediaConnectionState -eq "Connected"} -ErrorAction Stop 
+                $networkCards = Get-NetworkCards -ComputerName $ComputerName -ErrorAction Stop 
             }
             catch 
             {
-                Invoke-CatchActions
-                Write-VerboseOutput("Failed first attempt to get Windows2012R2 or greater advanced NIC settings. Error {0}." -f $Error[0].Exception)
-                Write-VerboseOutput("Going to attempt to get the FQDN from Get-ExchangeServer")
-                $fqdn = (Get-ExchangeServer $MachineName).FQDN 
-                $cimSession = New-CimSession -ComputerName $fqdn -ErrorAction Stop
-                $NetworkCards = Get-NetAdapter -CimSession $cimSession | ?{$_.MediaConnectionState -eq "Connected"} -ErrorAction Stop 
+                
+                if($CatchActionFunction -ne $null) {& $CatchActionFunction }
+                if($ComputerFQDN -ne $null -and $ComputerFQDN -ne [string]::Empty)
+                {
+                    Write-VerboseWriter("Going to attempt FQDN")
+                    $networkCards = Get-NetworkCards -ComputerName $ComputerFQDN
+                }
+                else {$bypassCatchAction = $true; Write-VerboseWriter("No FQDN was passed, going to rethrow error."); throw}
+                
             }
+            return (New-NICInformation -Adapters $networkCards)
         }
         catch 
         {
-            Invoke-CatchActions
-            Write-VerboseOutput("Failed to get Windows2012R2 or greater advanced NIC settings. Error {0}." -f $Error[0].Exception)
-            Write-VerboseOutput("Going to attempt to get WMI Object Win32_NetworkAdapter on this machine instead")
-            Write-VerboseOutput("NOTE: this means we aren't able to provide the driver date")
-            $NetworkCards2008 = Get-WmiObjectHandler -ComputerName $MachineName -Class "Win32_NetworkAdapter" -Filter "NetConnectionStatus ='2'" -CatchActionFunction ${Function:Invoke-CatchActions}
-            foreach($adapter in $NetworkCards2008)
-            {
-                [HealthChecker.NICInformation]$nicObject = New-Object -TypeName HealthChecker.NICInformation 
-                $nicObject.Description = $adapter.Description
-                $nicObject.Name = $adapter.Name
-                $nicObject.LinkSpeed = $adapter.Speed
-                $nicObject.NICObject = $adapter 
-                $nicObject.DriverDate = [DateTime]::MaxValue;
-                $aNICObjects += $nicObject
-            }
+            Write-VerboseWriter("Failed to get Windows2012R2 or greater advanced NIC settings. Error {0}." -f $Error[0].Exception)
+            Write-VerboseWriter("Going to attempt to get WMI Object Win32_NetworkAdapter on this machine instead")
+            Write-VerboseWriter("NOTE: This means we aren't able to provide the driver date")
+            if(!$bypassCatchAction -and $CatchActionFunction -ne $null) {& $CatchActionFunction }
+            $wmiNetCards = Get-WmiNetworkCards -ComputerName $ComputerName 
+            return (New-NICInformation -Adapters $wmiNetCards -Windows2012R2AndAbove $false)
         }
-        foreach($adapter in $NetworkCards)
-        {
-            Write-VerboseOutput("Working on getting netAdapeterRSS information for adapter: " + $adapter.InterfaceDescription)
-            [HealthChecker.NICInformation]$nicObject = New-Object -TypeName HealthChecker.NICInformation 
-            try
-            {
-                $RSS_Settings = $adapter | Get-netAdapterRss -ErrorAction Stop
-                $nicObject.RSSEnabled = $RSS_Settings.Enabled
-            }
-            catch 
-            {
-                Invoke-CatchActions
-                Write-Yellow("Warning: Unable to get the netAdapterRSS Information for adapter: {0}" -f $adapter.InterfaceDescription)
-                $nicObject.RSSEnabled = "NoRSS"
-            }
-            $nicObject.Description = $adapter.InterfaceDescription
-            $nicObject.DriverDate = $adapter.DriverDate
-            $nicObject.DriverVersion = $adapter.DriverVersionString
-            $nicObject.LinkSpeed = (($adapter.Speed)/1000000).ToString() + " Mbps"
-            $nicObject.Name = $adapter.Name
-            $nicObject.NICObject = $adapter 
-            $nicObject.MTUSize = $adapter.MtuSize
-            $aNICObjects += $nicObject
-        }
-
+    }
+    else 
+    {
+        Write-VerboseWriter("Windows OS Version is less than Windows 2012R2. Going to run Get-WmiObject.")
+        $wmiNetCards = Get-WmiNetworkCards -ComputerName $ComputerName
+        return (New-NICInformation -Adapters $wmiNetCards -Windows2012R2AndAbove $false)
     }
     
-    #Else we don't have all the correct powershell options to get more detailed information remotely 
-    else
-    {
-        Write-VerboseOutput("Detected OS Version less than Windows 2012R2")
-        $NetworkCards2008 = Get-WmiObjectHandler -ComputerName $MachineName -Class "Win32_NetworkAdapter" -Filter "NetConnectionStatus ='2'" -CatchActionFunction ${Function:Invoke-CatchActions}
-        foreach($adapter in $NetworkCards2008)
-        {
-            [HealthChecker.NICInformation]$nicObject = New-Object -TypeName HealthChecker.NICInformation 
-            $nicObject.Description = $adapter.Description
-            $nicObject.Name = $adapter.Name
-            $nicObject.LinkSpeed = $adapter.Speed
-            $nicObject.NICObject = $adapter 
-            $aNICObjects += $nicObject
-        }
-
-    }
-
-    Write-VerboseOutput("Exiting: Get-NICInformation")
-    return $aNICObjects 
 }
 
 Function Get-HttpProxySetting {
@@ -1502,7 +1519,8 @@ param(
     $os_obj.PowerPlan = $plan 
     $os_obj.PageFile = (Get-PageFileInformation -Machine_Name $Machine_Name)
     $os_obj.NetworkAdaptersConfiguration = Get-WmiObjectHandler -ComputerName $Machine_Name -Class "Win32_NetworkAdapterConfiguration" -Filter "IPEnabled = True" -CatchActionFunction ${Function:Invoke-CatchActions}
-    $os_obj.NetworkAdapters = (Get-NICInformation -MachineName $Machine_Name -OSVersion $os_obj.OSVersion)
+    if($os_obj.OSVersion -lt [HealthChecker.OSVersionName]::Windows2012R2){$isWindows2012R2OrNewer = $false}else{$isWindows2012R2OrNewer = $true}
+    $os_obj.NetworkAdapters = (Get-AllNicInformation -ComputerName $Machine_Name -Windows2012R2AndAbove $isWindows2012R2OrNewer -CatchActionFunction ${Function:Invoke-CatchActions} -ComputerFQDN ((Get-ExchangeServer $Machine_Name -ErrorAction SilentlyContinue).FQDN))
     foreach($adapter in $os_obj.NetworkAdaptersConfiguration)
     {
         Write-VerboseOutput("Working on {0}" -f $adapter.Description)
