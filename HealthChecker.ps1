33000b61264a62a2ecea404f5a15c27a2e7d38b5
David Paulson <dpaul@microsoft.com>
2019-07-17 17:09:02 -0500

    Included Get-AllTlsSettingsFromRegistry template to the script - without removing old code
    Added the template to the script and extra object attribute around this to avoid horrible difference between the commits.
    
    going to remove the old code in the next commit.

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -331,6 +331,7 @@ using System.Collections;
             public string TimeZone; //value to stores the current timezone of the server. 
             public System.Array TLSSettings;
             public NetDefaultTlsVersionInformation NetDefaultTlsVersion;
+            public Hashtable TLSSettingsv2;
 	        public string BootUpTimeInDays;
             public string BootUpTimeInHours;
             public string BootUpTimeInMinutes;
@@ -1479,6 +1480,138 @@ param(
     return $netTlsVersion
 }
 
+#Master Template: https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Get-AllTlsSettingsFromRegistry/Get-AllTlsSettingsFromRegistry.ps1
+Function Get-AllTlsSettingsFromRegistry {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory=$true)][string]$MachineName,
+    [Parameter(Mandatory=$false)][scriptblock]$CatchActionFunction
+    )
+    #Function Version 1.0
+    <# 
+    Required Functions: 
+        https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Write-VerboseWriters/Write-VerboseWriter.ps1
+        https://raw.githubusercontent.com/dpaulson45/PublicPowerShellScripts/master/Functions/Invoke-RegistryGetValue/Invoke-RegistryGetValue.ps1
+    #>
+    
+    Write-VerboseWriter("Calling: Get-AllTlsSettingsFromRegistry")
+    Write-VerboseWriter("Passed: [string]MachineName: {0}" -f $MachineName)
+    
+    $registryBase = "SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS {0}\{1}"
+    $tlsVersions = @("1.0","1.1","1.2")
+    
+    $tlsResults = @{}
+    $keyValues = ("Enabled","DisabledByDefault")
+    
+    Function Set-TLSMemberValue {
+    param(
+    [Parameter(Mandatory=$true)][string]$GetKeyType,
+    [Parameter(Mandatory=$false)][object]$KeyValue,
+    [Parameter(Mandatory=$true)][string]$ServerClientType,
+    [Parameter(Mandatory=$true)][string]$TlsVersion 
+    )
+        switch($GetKeyType)
+        {
+            "Enabled" {
+                if($KeyValue -eq $null)
+                {
+                    Write-VerboseWriter("Failed to get TLS {0} {1} Enabled Key on Server {2}. We are assuming this means it is enabled." -f $TlsVersion, $ServerClientType, $MachineName)
+                    return $true
+                }
+                else 
+                {
+                    Write-VerboseWriter("{0} Enabled Value '{1}'" -f $ServerClientType, $serverValue)
+                    if($KeyValue -eq 1)
+                    {
+                        return $true 
+                    }
+                    return $false 
+                }
+             }
+            "DisabledByDefault" {
+                if($KeyValue -ne $null)
+                {
+                    Write-VerboseWriter("Failed to get TLS {0} {1} Disabled By Default Key on Server {2}. Setting to false." -f $TlsVersion, $ServerClientType, $MachineName)
+                    return $false 
+                }
+                else 
+                {
+                    Write-VerboseWriter("{0} Disabled By Default Value '{1}'" -f $ServerClientType, $serverValue)
+                    if($KeyValue -eq 1)
+                    {
+                        return $true
+                    }
+                    return $false 
+                }
+            }
+        }
+    }
+    
+    Function Set-NETDefaultTLSValue {
+    param(
+    [Parameter(Mandatory=$false)][object]$KeyValue,
+    [Parameter(Mandatory=$true)][string]$NetVersion,
+    [Parameter(Mandatory=$true)][string]$KeyName
+    )
+        if($KeyValue -eq $null)
+        {
+            Write-VerboseWriter("Failed to get {0} registry value for .NET {1} version. Setting to false." -f $KeyName, $NetVersion)
+            return $false
+        }
+        else 
+        {
+            Write-VerboseWriter("{0} value '{1}'" -f $KeyName, $KeyValue)
+            if($KeyValue -eq 1)
+            {
+                return $true 
+            }
+            return $false 
+        }
+    }
+    
+    [hashtable]$allTlsObjects = @{}
+    foreach($tlsVersion in $tlsVersions)
+    {
+        $registryServer = $registryBase -f $tlsVersion, "Server" 
+        $registryClient = $registryBase -f $tlsVersion, "Client" 
+        $currentTLSObject = New-Object PSCustomObject 
+        $currentTLSObject | Add-Member -MemberType NoteProperty -Name "TLSVersion" -Value $tlsVersion
+    
+        foreach($getKey in $keyValues)
+        {
+            $memberServerName = "Server{0}" -f $getKey
+            $memberClientName = "Client{0}" -f $getKey 
+    
+            $serverValue = Invoke-RegistryGetValue -RegistryHive "LocalMachine" -MachineName $MachineName -SubKey $registryServer -GetValue $getKey -CatchActionFunction $CatchActionFunction
+            $clientValue = Invoke-RegistryGetValue -RegistryHive "LocalMachine" -MachineName $MachineName -SubKey $registryClient -GetValue $getKey -CatchActionFunction $CatchActionFunction
+    
+            $currentTLSObject | Add-Member -MemberType NoteProperty -Name $memberServerName -Value (Set-TLSMemberValue -GetKeyType $getKey -KeyValue $serverValue -ServerClientType "Server" -TlsVersion $tlsVersion)
+            $currentTLSObject | Add-Member -MemberType NoteProperty -Name $memberClientName -Value (Set-TLSMemberValue -GetKeyType $getKey -KeyValue $clientValue -ServerClientType "Client" -TlsVersion $tlsVersion)
+    
+        }
+        $allTlsObjects.Add($tlsVersion, $currentTLSObject)
+    }
+    
+    $netVersions = @("v2.0.50727","v4.0.30319")
+    $registryBase = "SOFTWARE\{0}\.NETFramework\{1}"
+    foreach($netVersion in $netVersions)
+    {
+        $currentNetTlsDefaultVersionObject = New-Object PSCustomObject 
+        $currentNetTlsDefaultVersionObject | Add-Member -MemberType NoteProperty -Name "NetVersion" -Value $netVersion
+    
+        $SystemDefaultTlsVersions = Invoke-RegistryGetValue -RegistryHive "LocalMachine" -MachineName $MachineName -SubKey ($registryBase -f "Microsoft", $netVersion) -GetValue "SystemDefaultTlsVersions" -CatchActionFunction $CatchActionFunction
+        $WowSystemDefaultTlsVersions = Invoke-RegistryGetValue -RegistryHive "LocalMachine" -MachineName $MachineName -SubKey ($registryBase -f "Wow6432Node\Microsoft", $netVersion) -GetValue "SystemDefaultTlsVersions" -CatchActionFunction $CatchActionFunction
+    
+        $currentNetTlsDefaultVersionObject | Add-Member -MemberType NoteProperty -Name "SystemDefaultTlsVersions" -Value (Set-NETDefaultTLSValue -KeyValue $SystemDefaultTlsVersions -NetVersion $netVersion -KeyName "SystemDefaultTlsVersions")
+        $currentNetTlsDefaultVersionObject | Add-Member -MemberType NoteProperty -Name "WowSystemDefaultTlsVersions" -Value (Set-NETDefaultTLSValue -KeyValue $WowSystemDefaultTlsVersions -NetVersion $netVersion -KeyName "WowSystemDefaultTlsVersions")
+        
+        $hashKeyName = "NET{0}" -f ($netVersion.Split(".")[0])
+        $allTlsObjects.Add($hashKeyName, $currentNetTlsDefaultVersionObject) 
+    }
+    
+    return $allTlsObjects
+}
+
 Function Get-OperatingSystemInformation {
 param(
 [Parameter(Mandatory=$true)][string]$Machine_Name
@@ -1569,6 +1702,8 @@ param(
     $os_obj.TimeZone = ([System.TimeZone]::CurrentTimeZone).StandardName
     $os_obj.TLSSettings = Get-TLSSettings -Machine_Name $Machine_Name
     $os_obj.NetDefaultTlsVersion = Get-NetTLSDefaultVersions -Machine_Name $Machine_Name
+
+    $os_obj.TLSSettingsv2 = Get-AllTlsSettingsFromRegistry -MachineName $Machine_Name -CatchActionFunction ${Function:Invoke-CatchActions} 
 
     Write-VerboseOutput("Exiting: Get-OperatingSystemInformation")
     return $os_obj
@@ -4154,6 +4289,37 @@ param(
     ################
 
     Write-Grey("`r`nTLS Settings:")
+    $tlsVersions = @("1.0","1.1","1.2")
+    foreach($tlsKey in $tlsVersions)
+    {
+        $netKey = "NETv4"
+        if($HealthExSvrObj.ExchangeInformation.ExchangeVersion -eq [HealthChecker.ExchangeVersion]::Exchange2010)
+        {
+            $netKey = "NETv2"
+        }
+        $currentTlsVersion = $HealthExSvrObj.OSVersion.TLSSettingsv2[$tlsKey]
+        $currentNetVersion = $HealthExSvrObj.OSVersion.TLSSettingsv2[$netKey]
+        Write-Grey("`tTLS {0}" -f $tlsKey)
+        Write-Grey("`tServer Enabled: {0}" -f $currentTlsVersion.ServerEnabled)
+        Write-Grey("`tServer Disabled By Default: {0}" -f $currentTlsVersion.ServerDisabledByDefault)
+        Write-Grey("`tClient Enabled: {0}" -f $currentTlsVersion.ClientEnabled)
+        Write-Grey("`tClient Disabled By Default: {0}" -f $currentTlsVersion.ClientDisabledByDefault)
+        if($currentTlsVersion.ServerEnabled -ne $currentTlsVersion.ClientEnabled)
+        {
+            Write-Red("`t`tError: Mismatch in TLS version for client and server. Exchange can be both client and a server. This can cause issues within Exchange for communication.")
+        }
+        if(($tlsKey -eq "1.0" -or $tlsKey -eq "1.1") -and 
+            ($currentTlsVersion.ServerEnabled -eq $false -or 
+            $currentTlsVersion.ClientEnabled -eq $false -or 
+            $currentTlsVersion.ServerDisabledByDefault -or 
+            $currentTlsVersion.ClientDisabledByDefault) -and 
+            ($currentNetVersion.SystemDefaultTlsVersions -eq $false -or
+            $currentNetVersion.WowSystemDefaultTlsVersions -eq $false))
+            {
+                Write-Red("`t`tError: Failed to set .NET SystemDefaultTlsVersions. Please visit on how to properly enable TLS 1.2 https://blogs.technet.microsoft.com/exchange/2018/04/02/exchange-server-tls-guidance-part-2-enabling-tls-1-2-and-identifying-clients-not-using-it/")
+            }
+    }
+    <#
     foreach($TLS in $HealthExSvrObj.OSVersion.TLSSettings)
     {
         Write-Grey("`tTLS {0}" -f $TLS.TLSName)
@@ -4173,7 +4339,7 @@ param(
                 Write-Red("`t`tError: Failed to set .NET SystemDefaultTlsVersions. Please visit on how to properly enable TLS 1.2 https://blogs.technet.microsoft.com/exchange/2018/04/02/exchange-server-tls-guidance-part-2-enabling-tls-1-2-and-identifying-clients-not-using-it/")
             }
     }
-
+    #>
 	##############
 	#Hotfix Check#
 	##############
