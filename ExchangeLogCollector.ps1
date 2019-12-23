5b0d98905c7a554eb0ab9b81763c5652252888f4
David Paulson <dpaul@microsoft.com>
2019-12-23 15:49:11 -0600

    Included Write-*Writer
    included the Write-*Writers for the script.

--- ExchangeLogCollector.ps1
+++ ExchangeLogCollector.ps1
@@ -1748,6 +1748,43 @@ param(
         }
     }
 
+    #Function Version 1.1
+    Function Write-HostWriter {
+    param(
+    [Parameter(Mandatory=$true)][string]$WriteString 
+    )
+        if($Script:Logger -ne $null)
+        {
+            $Script:Logger.WriteHost($WriteString)
+        }
+        elseif($HostFunctionCaller -eq $null)
+        {
+            Write-Host $WriteString
+        }
+        else
+        {
+            &$HostFunctionCaller $WriteString    
+        }
+    }
+
+    #Function Version 1.1
+    Function Write-VerboseWriter {
+    param(
+    [Parameter(Mandatory=$true)][string]$WriteString
+    )
+        if($Script:Logger -ne $null)
+        {
+            $Script:Logger.WriteHost($WriteString)
+        }
+        elseif($VerboseFunctionCaller -eq $null)
+        {
+            Write-Verbose $WriteString
+        }
+        else 
+        {
+            &$VerboseFunctionCaller $WriteString
+        }
+    }
 
     Function Write-ScriptHost{
     param(
