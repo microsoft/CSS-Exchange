d7b6d68fe7597aa3966d474f4b7785338f11ec71
David Paulson <dpaul@microsoft.com>
2020-09-16 10:58:43 -0500

    Updated Script Version v2.44.2
    Updated the script Version

--- HealthChecker.ps1
+++ HealthChecker.ps1
@@ -108,7 +108,7 @@ param(
 Note to self. "New Release Update" are functions that i need to update when a new release of Exchange is published
 #>
 
-$healthCheckerVersion = "2.44.1"
+$healthCheckerVersion = "2.44.2"
 $VirtualizationWarning = @"
 Virtual Machine detected.  Certain settings about the host hardware cannot be detected from the virtual machine.  Verify on the VM Host that: 
 
