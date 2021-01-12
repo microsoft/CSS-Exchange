19695307bc0733936e234720050d36a1c35ad220
David Paulson <dpaul@microsoft.com>
2021-01-11 22:47:58 -0600

    Included AmazonEC2 server Type

--- src/DataCollection/extern/Get-ServerType.ps1
+++ src/DataCollection/extern/Get-ServerType.ps1
@@ -1,16 +1,17 @@
 #https://github.com/dpaulson45/PublicPowerShellScripts/blob/master/Functions/ComputerInformation/Get-ServerType/Get-ServerType.ps1
-#v21.01.08.2133
+#v21.01.11.2243
 Function Get-ServerType {
     [CmdletBinding()]
     [OutputType("System.String")]
     param(
         [Parameter(Mandatory = $true)][string]$ServerType
     )
-    #Function Version #v21.01.08.2133
+    #Function Version #v21.01.11.2243
 
     Write-VerboseWriter("Calling: Get-ServerType")
     $returnServerType = [string]::Empty
     if ($ServerType -like "VMware*") { $returnServerType = "VMware" }
+    elseif ($ServerType -like "*Amazon EC2*") { $returnServerType = "AmazonEC2" }
     elseif ($ServerType -like "*Microsoft Corporation*") { $returnServerType = "HyperV" }
     elseif ($ServerType.Length -gt 0) { $returnServerType = "Physical" }
     else { $returnServerType = "Unknown" }
