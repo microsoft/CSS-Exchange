e7517b19883f2e1672d63cd86c6e3f48b195e23c
David Paulson <dpaul@microsoft.com>
2021-01-24 18:03:50 -0600

    Added known issue 'Setup /PrepareSchema fails with "The error code is: 8245"'

--- Setup/SetupLogReviewer.ps1
+++ Setup/SetupLogReviewer.ps1
@@ -274,6 +274,16 @@ Function Test-OtherKnownIssues {
     }
 }
 
+Function Test-KnownLdifErrors {
+    $schemaImportProcessFailure = Select-String "\[ERROR\] There was an error while running 'ldifde.exe' to import the schema file '(.*)'. The error code is: (\d+). More details can be found in the error file: '(.*)'" $SetupLog | Select-Object -Last 1
+
+    if ($null -ne $schemaImportProcessFailure) {
+        Write-ActionPlan("Failed to import schema setting from file '{0}'`r`n`tReview ldif.err file '{1}' to help determine which object in the file '{0}' was trying to be imported that was causing problems.`r`n`tIf you can't find the ldf file in the C:\Windows\Temp location, then find the file in the ISO." -f $schemaImportProcessFailure.Matches.Groups[1].Value,
+            $schemaImportProcessFailure.Matches.Groups[3].Value)
+        return 1
+    }
+}
+
 Function Main {
     try {
         if (-not ([IO.File]::Exists($SetupLog))) {
@@ -295,6 +305,10 @@ Function Main {
         }
 
         if (Test-PrerequisiteCheck) {
+            return
+        }
+
+        if (Test-KnownLdifErrors) {
             return
         }
 
