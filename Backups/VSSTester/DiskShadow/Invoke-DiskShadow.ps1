595d0106386e906f7d425f34c8842080d8bf3f18
David Paulson <dpaul@microsoft.com>
2021-01-27 12:06:14 -0600

    Change into the output path location for diskshadow
    If we don't change into the output path location, we get the following error message:
    
    "The .cab metadata file cannot be stored in the current working directory, because it is read-only"
    
    and the script just continue without there seeming like an issue. Quickly change into the output location and pop back to the current working directory to avoid any issues like this.

--- Backups/VSSTester/DiskShadow/Invoke-DiskShadow.ps1
+++ Backups/VSSTester/DiskShadow/Invoke-DiskShadow.ps1
@@ -8,5 +8,12 @@ function Invoke-DiskShadow {
     Write-Host "`"C:\Windows\System32\diskshadow.exe /s $path\diskshadow.dsh /l $path\diskshadow.log`"" $nl
     Write-Host " "
 
-    diskshadow.exe /s $path\diskshadow.dsh /l $path\diskshadow.log
+    #in case the $path and the script location is different we need to change location into the $path directory to get the results to work as expected.
+    try {
+        $here = (Get-Location).Path
+        Set-Location $path
+        diskshadow.exe /s $path\diskshadow.dsh /l $path\diskshadow.log
+    } finally {
+        Set-Location $here
+    }
 }
