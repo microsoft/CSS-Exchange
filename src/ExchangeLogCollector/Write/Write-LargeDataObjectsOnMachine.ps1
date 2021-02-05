fc020651cea0cc2f207d70ae9fa041dae03507e7
David Paulson <dpaul@microsoft.com>
2021-02-05 16:11:14 -0600

    Provided parameter -Server in Get-ServerMonitoringOverride
    This is to avoid a few issues when running on an Edge Server.
    
    This also resolves #133

--- src/ExchangeLogCollector/Write/Write-LargeDataObjectsOnMachine.ps1
+++ src/ExchangeLogCollector/Write/Write-LargeDataObjectsOnMachine.ps1
@@ -308,7 +308,7 @@ Function Write-LargeDataObjectsOnMachine {
 
             $basicServerObject | Add-Member -MemberType NoteProperty -Name "HealthReport" -Value (Get-HealthReport $server)
             $basicServerObject | Add-Member -MemberType NoteProperty -Name "ServerComponentState" -Value (Get-ServerComponentState $server)
-            $basicServerObject | Add-Member -MemberType NoteProperty -Name "ServerMonitoringOverride" -Value (Get-ServerMonitoringOverride $server)
+            $basicServerObject | Add-Member -MemberType NoteProperty -Name "ServerMonitoringOverride" -Value (Get-ServerMonitoringOverride -Server $server)
             $basicServerObject | Add-Member -MemberType NoteProperty -Name "ServerHealth" -Value (Get-ServerHealth $server)
 
             $exchangeServerData += $basicServerObject
