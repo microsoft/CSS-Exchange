fdd872bc5e9915a92b033b18fddb84058341168b
David Paulson <dpaul@microsoft.com>
2021-02-08 16:56:28 -0600

    Improved Error Handling
    - Dumping out more error information in `Invoke-CatchActions` to assist with where about the error might be coming from.
    - Cleaned up logic in `Get-ErrorsThatOccurred` and also dumping out stack information to the file and providing an output of the `$Error` to upload as well.

--- src/Helpers/Invoke-CatchActions.ps1
+++ src/Helpers/Invoke-CatchActions.ps1
@@ -1,13 +1,15 @@
 Function Invoke-CatchActions {
     param(
-        [object]$CopyThisError
+        [object]$CopyThisError = $Error[0]
     )
     Write-VerboseOutput("Calling: Invoke-CatchActions")
     $Script:ErrorsExcludedCount++
+    $Script:ErrorsExcluded += $CopyThisError
+    Write-VerboseOutput("Error Excluded Count: $Script:ErrorsExcludedCount")
+    Write-VerboseOutput("Error Count: $($Error.Count)")
+    Write-VerboseOutput($CopyThisError)
 
-    if ($null -eq $CopyThisError) {
-        $Script:ErrorsExcluded += $Error[0]
-    } else {
-        $Script:ErrorsExcluded += $CopyThisError
+    if ($null -ne $CopyThisError.ScriptStackTrace) {
+        Write-VerboseOutput($CopyThisError.ScriptStackTrace)
     }
 }
