17e56ed6487cd72c815589b6a95650da9376231e
David Paulson <dpaul@microsoft.com>
2021-02-08 11:58:46 -0600

    Removed Test-Connection for RemoteExecutionOfServers
    If ICMP is disabled on the server or on some device between the servers, this could prevent the script from executing on that server. We should just check to see if we can run Invoke-Command against the server.

--- src/ExchangeLogCollector/Helpers/Test-RemoteExecutionOfServers.ps1
+++ src/ExchangeLogCollector/Helpers/Test-RemoteExecutionOfServers.ps1
@@ -3,29 +3,14 @@ Function Test-RemoteExecutionOfServers {
         [Parameter(Mandatory = $true)][Array]$ServerList
     )
     Write-ScriptDebug("Function Enter: Test-RemoteExecutionOfServers")
-    $serversUp = @()
     Write-ScriptHost -WriteString "Checking to see if the servers are up in this list:" -ShowServer $false
-    foreach ($server in $ServerList) {
-        Write-ScriptHost -WriteString $server -ShowServer $false
-    }
-    Write-ScriptHost -WriteString " " -ShowServer $false
-    Write-ScriptHost -WriteString "Checking their status...." -ShowServer $false
-    foreach ($server in $ServerList) {
-        Write-ScriptHost -WriteString ("Checking server {0}...." -f $server) -ShowServer $false -NoNewLine $true
-        if ((Test-Connection $server -Quiet)) {
-            Write-ScriptHost -WriteString "Online" -ShowServer $false -ForegroundColor "Green"
-            $serversUp += $server
-        } else {
-            Write-ScriptHost -WriteString "Offline" -ShowServer $false -ForegroundColor "Red"
-            Write-ScriptHost -WriteString ("Removing Server {0} from the list to collect data from" -f $server) -ShowServer $false
-        }
-    }
-    #Now we should check to see if can use WRM with invoke-command
+    $ServerList | ForEach-Object { Write-ScriptHost -WriteString $_ -ShowServer $false }
+    #Going to just use Invoke-Command to see if the servers are up. As ICMP might be disabled in the environment.
     Write-ScriptHost " " -ShowServer $false
-    Write-ScriptHost -WriteString "For all the servers that are up, we are going to see if remote execution will work" -ShowServer $false
+    Write-ScriptHost -WriteString "For all the servers in the list, checking to see if Invoke-Command will work against them." -ShowServer $false
     #shouldn't need to test if they are Exchange servers, as we should be doing that locally as well.
     $validServers = @()
-    foreach ($server in $serversUp) {
+    foreach ($server in $ServerList) {
 
         try {
             Write-ScriptHost -WriteString ("Checking Server {0}....." -f $server) -ShowServer $false -NoNewLine $true
