53fc6882566e6d216f95eb343543fdca6c5f3c40
Lukas Sassl <lusassl@microsoft.com>
2021-02-09 12:40:39 +0100

    Certificate check optimizations

--- src/DataCollection/ExchangeInformation/Get-ExchangeServerCertificates.ps1
+++ src/DataCollection/ExchangeInformation/Get-ExchangeServerCertificates.ps1
@@ -21,9 +21,22 @@ Function Get-ExchangeServerCertificates {
                     $certificateLifetime = ([DateTime]($cert.NotAfter) - (Get-Date)).Days
                     $sanCertificateInfo = $false
 
+                    $currentErrors = $Error.Count
                     if ($null -ne $cert.DnsNameList -and
                         ($cert.DnsNameList).Count -gt 1) {
                         $sanCertificateInfo = $true
+                        $certDnsNameList = $cert.DnsNameList
+                    } elseif ($null -eq $cert.DnsNameList) {
+                        $certDnsNameList = "None"
+                    } else {
+                        $certDnsNameList = $cert.DnsNameList
+                    }
+                    if ($currentErrors -lt $Error.Count) {
+                        $i = 0
+                        while ($i -lt ($Error.Count - $currentErrors)) {
+                            Invoke-CatchActions $Error[$i]
+                            $i++
+                        }
                     }
 
                     if ($authConfigDetected) {
@@ -37,7 +50,7 @@ Function Get-ExchangeServerCertificates {
                     }
 
                     if ([String]::IsNullOrEmpty($cert.FriendlyName)) {
-                        $certFriendlyName = $cert.DnsNameList[0].Unicode
+                        $certFriendlyName = ($certDnsNameList[0]).ToString()
                     } else {
                         $certFriendlyName = $cert.FriendlyName
                     }
@@ -47,7 +60,7 @@ Function Get-ExchangeServerCertificates {
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "Thumbprint" -Value $cert.Thumbprint
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "PublicKeySize" -Value $cert.PublicKey.Key.KeySize
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "IsSanCertificate" -Value $sanCertificateInfo
-                    $certInformationObj | Add-Member -MemberType NoteProperty -Name "Namespaces" -Value $cert.DnsNameList
+                    $certInformationObj | Add-Member -MemberType NoteProperty -Name "Namespaces" -Value $certDnsNameList
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "Services" -Value $cert.Services
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "IsCurrentAuthConfigCertificate" -Value $isAuthConfigInfo
                     $certInformationObj | Add-Member -MemberType NoteProperty -Name "LifetimeInDays" -Value $certificateLifetime
