a1ca100b6a12e56b2af484e6c2c11740b95b21cd
Kevin Beaumont <34346399+GossiTheDog@users.noreply.github.com>
2021-03-04 12:00:36 +0000

    Fix CVE-27065 reference

--- Security/Test-Hafnium.ps1
+++ Security/Test-Hafnium.ps1
@@ -51,7 +51,7 @@ function Get-26857() {
 }
 
 function Get-27065() {
-    Write-Host "`r`nChecking for CVE-2021-26857 in the ECP Logs"
+    Write-Host "`r`nChecking for CVE-2021-27065 in the ECP Logs"
     $logs = Get-ChildItem -Recurse -Path "$exchangePath\Logging\ECP\Server\*.log" | Select-String "Set-.*VirtualDirectory" -List | Select-Object Path
     if ($logs.Path.Count -gt 0) {
         Write-Warning "Suspicious virtual directory modifications found in the following logs, please review them for `"Set-*VirtualDirectory`" entries:"
