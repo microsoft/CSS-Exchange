bfe43f278abbcb90d417d597ec8385ff5a547a57
Bill Long <bilong@microsoft.com>
2021-03-05 15:55:49 -0600

    Bring back -OutputPath

--- Security/Test-ProxyLogon.ps1
+++ Security/Test-ProxyLogon.ps1
@@ -1,7 +1,19 @@
 ﻿#Checks for signs of exploit from CVE-2021-26855, 26858, 26857, and 27065.
 
-function Test-ExchangeHafnium {
-    <#
+[CmdletBinding()]
+param (
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [string[]]
+    $ComputerName = $env:COMPUTERNAME,
+
+    [string]
+    $OutPath
+)
+
+process {
+
+    function Test-ExchangeHafnium {
+        <#
 	.SYNOPSIS
 		Checks targeted exchange servers for signs of Hafnium vulnerability compromise.
 
@@ -28,135 +40,135 @@ function Test-ExchangeHafnium {
 
 		Scans all exchange servers in the organization for Hafnium vulnerability compromises
 #>
-    [CmdletBinding()]
-    param (
-        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [string[]]
-        $ComputerName = $env:COMPUTERNAME,
+        [CmdletBinding()]
+        param (
+            [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [string[]]
+            $ComputerName = $env:COMPUTERNAME,
 
-        [pscredential]
-        $Credential
-    )
-    begin {
-        #region Remoting Scriptblock
-        $scriptBlock = {
-            #region Functions
-            function Get-Cve26855 {
-                [CmdletBinding()]
-                param ()
+            [pscredential]
+            $Credential
+        )
+        begin {
+            #region Remoting Scriptblock
+            $scriptBlock = {
+                #region Functions
+                function Get-Cve26855 {
+                    [CmdletBinding()]
+                    param ()
 
-                Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs"
+                    Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs"
 
-                $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
+                    $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
 
-                $files = (Get-ChildItem -Recurse -Path "$exchangePath\Logging\HttpProxy" -Filter '*.log').FullName
-                $count = 0
-                $allResults = @()
-                $sw = New-Object System.Diagnostics.Stopwatch
-                $sw.Start()
-                $files | ForEach-Object {
-                    $count++
+                    $files = (Get-ChildItem -Recurse -Path "$exchangePath\Logging\HttpProxy" -Filter '*.log').FullName
+                    $count = 0
+                    $allResults = @()
+                    $sw = New-Object System.Diagnostics.Stopwatch
+                    $sw.Start()
+                    $files | ForEach-Object {
+                        $count++
 
-                    if ($sw.ElapsedMilliseconds -gt 500) {
-                        Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs" -Status "$count / $($files.Count)" -PercentComplete ($count * 100 / $files.Count)
-                        $sw.Restart()
+                        if ($sw.ElapsedMilliseconds -gt 500) {
+                            Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs" -Status "$count / $($files.Count)" -PercentComplete ($count * 100 / $files.Count)
+                            $sw.Restart()
+                        }
+
+                        if ((Get-ChildItem $_ -ErrorAction SilentlyContinue | Select-String "ServerInfo~").Count -gt 0) {
+                            $fileResults = @(Import-Csv -Path $_ -ErrorAction SilentlyContinue | Where-Object AnchorMailbox -Like 'ServerInfo~*/*' | Select-Object -Property DateTime, RequestId, ClientIPAddress, UrlHost, UrlStem, RoutingHint, UserAgent, AnchorMailbox, HttpStatus)
+                            $fileResults | ForEach-Object {
+                                $allResults += $_
+                            }
+                        }
                     }
 
-                    if ((Get-ChildItem $_ -ErrorAction SilentlyContinue | Select-String "ServerInfo~").Count -gt 0) {
-                        $fileResults = @(Import-Csv -Path $_ -ErrorAction SilentlyContinue | Where-Object AnchorMailbox -Like 'ServerInfo~*/*' | Select-Object -Property DateTime, RequestId, ClientIPAddress, UrlHost, UrlStem, RoutingHint, UserAgent, AnchorMailbox, HttpStatus)
-                        $fileResults | ForEach-Object {
-                            $allResults += $_
+                    Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs" -Completed
+
+                    return $allResults
+                }
+
+                function Get-Cve26857 {
+                    [CmdletBinding()]
+                    param ()
+
+                    Get-WinEvent -FilterHashtable @{
+                        LogName      = 'Application'
+                        ProviderName = 'MSExchange Unified Messaging'
+                        Level        = '2'
+                    } -ErrorAction SilentlyContinue | Where-Object Message -Like "*System.InvalidCastException*"
+                }
+
+                function Get-Cve26858 {
+                    [CmdletBinding()]
+                    param ()
+
+                    $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
+
+                    Get-ChildItem -Recurse -Path "$exchangePath\Logging\OABGeneratorLog" | Select-String "Download failed and temporary file" -List | Select-Object -ExpandProperty Path
+                }
+
+                function Get-Cve27065 {
+                    [CmdletBinding()]
+                    param ()
+
+                    $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
+
+                    Get-ChildItem -Recurse -Path "$exchangePath\Logging\ECP\Server\*.log" | Select-String "Set-.*VirtualDirectory" -List | Select-Object -ExpandProperty Path
+                }
+
+                function Get-SuspiciousFile {
+                    [CmdletBinding()]
+                    param ()
+
+                    foreach ($file in Get-ChildItem -Recurse -Path "$env:WINDIR\temp\lsass.*dmp") {
+                        [PSCustomObject]@{
+                            ComputerName = $env:COMPUTERNAME
+                            Type         = 'LsassDump'
+                            Path         = $file.FullName
+                            Name         = $file.Name
+                        }
+                    }
+                    foreach ($file in Get-ChildItem -Recurse -Path "c:\root\lsass.*dmp") {
+                        [PSCustomObject]@{
+                            ComputerName = $env:COMPUTERNAME
+                            Type         = 'LsassDump'
+                            Path         = $file.FullName
+                            Name         = $file.Name
+                        }
+                    }
+                    foreach ($file in Get-ChildItem -Recurse -Path $env:ProgramData -ErrorAction SilentlyContinue | Where-Object Extension -Match ".7z|.zip|.rar") {
+                        [PSCustomObject]@{
+                            ComputerName = $env:COMPUTERNAME
+                            Type         = 'SuspiciousArchive'
+                            Path         = $file.FullName
+                            Name         = $file.Name
                         }
                     }
                 }
+                #endregion Functions
 
-                Write-Progress -Activity "Checking for CVE-2021-26855 in the HttpProxy logs" -Completed
-
-                return $allResults
-            }
-
-            function Get-Cve26857 {
-                [CmdletBinding()]
-                param ()
-
-                Get-WinEvent -FilterHashtable @{
-                    LogName      = 'Application'
-                    ProviderName = 'MSExchange Unified Messaging'
-                    Level        = '2'
-                } -ErrorAction SilentlyContinue | Where-Object Message -Like "*System.InvalidCastException*"
-            }
-
-            function Get-Cve26858 {
-                [CmdletBinding()]
-                param ()
-
-                $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
-
-                Get-ChildItem -Recurse -Path "$exchangePath\Logging\OABGeneratorLog" | Select-String "Download failed and temporary file" -List | Select-Object -ExpandProperty Path
-            }
-
-            function Get-Cve27065 {
-                [CmdletBinding()]
-                param ()
-
-                $exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
-
-                Get-ChildItem -Recurse -Path "$exchangePath\Logging\ECP\Server\*.log" | Select-String "Set-.*VirtualDirectory" -List | Select-Object -ExpandProperty Path
-            }
-
-            function Get-SuspiciousFile {
-                [CmdletBinding()]
-                param ()
-
-                foreach ($file in Get-ChildItem -Recurse -Path "$env:WINDIR\temp\lsass.*dmp") {
-                    [PSCustomObject]@{
-                        ComputerName = $env:COMPUTERNAME
-                        Type         = 'LsassDump'
-                        Path         = $file.FullName
-                        Name         = $file.Name
-                    }
-                }
-                foreach ($file in Get-ChildItem -Recurse -Path "c:\root\lsass.*dmp") {
-                    [PSCustomObject]@{
-                        ComputerName = $env:COMPUTERNAME
-                        Type         = 'LsassDump'
-                        Path         = $file.FullName
-                        Name         = $file.Name
-                    }
-                }
-                foreach ($file in Get-ChildItem -Recurse -Path $env:ProgramData -ErrorAction SilentlyContinue | Where-Object Extension -Match ".7z|.zip|.rar") {
-                    [PSCustomObject]@{
-                        ComputerName = $env:COMPUTERNAME
-                        Type         = 'SuspiciousArchive'
-                        Path         = $file.FullName
-                        Name         = $file.Name
-                    }
+                [PSCustomObject]@{
+                    ComputerName = $env:COMPUTERNAME
+                    Cve26855     = @(Get-Cve26855)
+                    Cve26857     = @(Get-Cve26857)
+                    Cve26858     = @(Get-Cve26858)
+                    Cve27065     = @(Get-Cve27065)
+                    Suspicious   = @(Get-SuspiciousFile)
                 }
             }
-            #endregion Functions
-
-            [PSCustomObject]@{
-                ComputerName = $env:COMPUTERNAME
-                Cve26855     = @(Get-Cve26855)
-                Cve26857     = @(Get-Cve26857)
-                Cve26858     = @(Get-Cve26858)
-                Cve27065     = @(Get-Cve27065)
-                Suspicious   = @(Get-SuspiciousFile)
+            #endregion Remoting Scriptblock
+            $parameters = @{
+                ScriptBlock = $scriptBlock
             }
+            if ($Credential) { $parameters.Credential = $Credential }
         }
-        #endregion Remoting Scriptblock
-        $parameters = @{
-            ScriptBlock = $scriptBlock
+        process {
+            Invoke-Command @parameters -ComputerName $ComputerName
         }
-        if ($Credential) { $parameters.Credential = $Credential }
     }
-    process {
-        Invoke-Command @parameters -ComputerName $ComputerName
-    }
-}
 
-function Write-HafniumReport {
-    <#
+    function Write-HafniumReport {
+        <#
 	.SYNOPSIS
 		Processes output of Test-ExchangeHafnium for reporting on the console screen.
 
@@ -175,72 +187,73 @@ function Write-HafniumReport {
 
 		Gather data from all exchange servers in the organization and write a report to C:\logs
 #>
-    [CmdletBinding()]
-    param (
-        [parameter(ValueFromPipeline = $true)]
-        $InputObject,
+        [CmdletBinding()]
+        param (
+            [parameter(ValueFromPipeline = $true)]
+            $InputObject,
 
-        [string]
-        $OutPath
-    )
+            [string]
+            $OutPath
+        )
 
-    process {
-        foreach ($report in $InputObject) {
-            Write-Host "Hafnium Status: Exchange Server $($report.ComputerName)"
-            if (-not ($report.Cve26855.Count -or $report.Cve26857.Count -or $report.Cve26858.Count -or $report.Cve27065.Count -or $report.Suspicious.Count)) {
-                Write-Host "  Nothing suspicious detected" -ForegroundColor Green
-                Write-Host ""
-                continue
-            }
+        process {
+            foreach ($report in $InputObject) {
+                Write-Host "Hafnium Status: Exchange Server $($report.ComputerName)"
+                if (-not ($report.Cve26855.Count -or $report.Cve26857.Count -or $report.Cve26858.Count -or $report.Cve27065.Count -or $report.Suspicious.Count)) {
+                    Write-Host "  Nothing suspicious detected" -ForegroundColor Green
+                    Write-Host ""
+                    continue
+                }
 
-            if ($report.Cve26855.Count -gt 0) {
-                Write-Host "  [CVE-2021-26855] Suspicious activity found in Http Proxy log!" -ForegroundColor Red
-                if ($OutPath) {
-                    $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26855.csv"
-                    $report.Cve26855 | Export-Csv -Path $newFile
-                    Write-Host "  Report exported to: $newFile"
-                } else {
-                    $report.Cve26855 | Format-Table -AutoSize | Out-Host
+                if ($report.Cve26855.Count -gt 0) {
+                    Write-Host "  [CVE-2021-26855] Suspicious activity found in Http Proxy log!" -ForegroundColor Red
+                    if ($OutPath) {
+                        $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26855.csv"
+                        $report.Cve26855 | Export-Csv -Path $newFile
+                        Write-Host "  Report exported to: $newFile"
+                    } else {
+                        $report.Cve26855 | Format-Table -AutoSize | Out-Host
+                    }
+                    Write-Host ""
                 }
-                Write-Host ""
-            }
-            if ($report.Cve26857.Count -gt 0) {
-                Write-Host "  [CVE-2021-26857] Suspicious activity found in Eventlog!" -ForegroundColor Red
-                Write-Host "  $(@($report.Cve26857).Count) events found"
-                if ($OutPath) {
-                    $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26857.csv"
-                    $report.Cve26857 | Select-Object TimeCreated, MachineName, Message | Export-Csv -Path $newFile
-                    Write-Host "  Report exported to: $newFile"
+                if ($report.Cve26857.Count -gt 0) {
+                    Write-Host "  [CVE-2021-26857] Suspicious activity found in Eventlog!" -ForegroundColor Red
+                    Write-Host "  $(@($report.Cve26857).Count) events found"
+                    if ($OutPath) {
+                        $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26857.csv"
+                        $report.Cve26857 | Select-Object TimeCreated, MachineName, Message | Export-Csv -Path $newFile
+                        Write-Host "  Report exported to: $newFile"
+                    }
+                    Write-Host ""
                 }
-                Write-Host ""
-            }
-            if ($report.Cve26858.Count -gt 0) {
-                Write-Host "  [CVE-2021-26858] Suspicious activity found in OAB generator logs!" -ForegroundColor Red
-                Write-Host "  Please review the following files for 'Download failed and temporary file' entries:"
-                foreach ($entry in $report.Cve26858) {
-                    Write-Host "   $entry"
+                if ($report.Cve26858.Count -gt 0) {
+                    Write-Host "  [CVE-2021-26858] Suspicious activity found in OAB generator logs!" -ForegroundColor Red
+                    Write-Host "  Please review the following files for 'Download failed and temporary file' entries:"
+                    foreach ($entry in $report.Cve26858) {
+                        Write-Host "   $entry"
+                    }
+                    if ($OutPath) {
+                        $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26858.log"
+                        $report.Cve26858 | Set-Content -Path $newFile
+                        Write-Host "  Report exported to: $newFile"
+                    }
+                    Write-Host ""
                 }
-                if ($OutPath) {
-                    $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26858.log"
-                    $report.Cve26858 | Set-Content -Path $newFile
-                    Write-Host "  Report exported to: $newFile"
-                }
-                Write-Host ""
-            }
-            if ($report.Suspicious.Count -gt 0) {
-                Write-Host "  Other suspicious files found: $(@($report.Suspicious).Count)"
-                if ($OutPath) {
-                    $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-other.csv"
-                    $report.Suspicious | Export-Csv -Path $newFile
-                    Write-Host "  Report exported to: $newFile"
-                } else {
-                    foreach ($entry in $report.Suspicious) {
-                        Write-Host "   $($entry.Type) : $($entry.Path)"
+                if ($report.Suspicious.Count -gt 0) {
+                    Write-Host "  Other suspicious files found: $(@($report.Suspicious).Count)"
+                    if ($OutPath) {
+                        $newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-other.csv"
+                        $report.Suspicious | Export-Csv -Path $newFile
+                        Write-Host "  Report exported to: $newFile"
+                    } else {
+                        foreach ($entry in $report.Suspicious) {
+                            Write-Host "   $($entry.Type) : $($entry.Path)"
+                        }
                     }
                 }
             }
         }
     }
-}
 
-$input | Test-ExchangeHafnium | Write-HafniumReport
+    $ComputerName | Test-ExchangeHafnium | Write-HafniumReport -OutPath $OutPath
+}
