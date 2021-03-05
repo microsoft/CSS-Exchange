f4d1a09349081314b0f3e6120d201c29975dd1f8
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2021-03-05 16:20:12 +0100

    Adopted questionable formatting convention

--- Security/Test-Hafnium.ps1
+++ Security/Test-Hafnium.ps1
@@ -1,31 +1,31 @@
-﻿#Checks for signs of exploit from CVE-2021-26855, 26858, 26857, and 27065.
+#Checks for signs of exploit from CVE-2021-26855, 26858, 26857, and 27065.
 
 function Test-ExchangeHafnium {
-<#
+	<#
 	.SYNOPSIS
 		Checks targeted exchange servers for signs of Hafnium vulnerability compromise.
-	
+
 	.DESCRIPTION
 		Checks targeted exchange servers for signs of Hafnium vulnerability compromise.
 		Will do so in parallel if more than one server is specified, so long as names aren't provided by pipeline.
-	
+
 		The vulnerabilities are described in CVE-2021-26855, 26858, 26857, and 27065
-	
+
 	.PARAMETER ComputerName
 		The list of server names to scan for signs of compromise.
 		Do not provide these by pipeline if you want parallel processing.
-	
+
 	.PARAMETER Credential
 		Credentials to use for remote connections.
-	
+
 	.EXAMPLE
 		PS C:\> Test-ExchangeHafnium
-	
+
 		Scans the current computer for signs of Hafnium vulnerability compromise.
-	
+
 	.EXAMPLE
 		PS C:\> Test-ExchangeHafnium -ComputerName (Get-ExchangeServer).Fqdn
-	
+
 		Scans all exchange servers in the organization for Hafnium vulnerability compromises
 #>
 	[CmdletBinding()]
@@ -33,7 +33,7 @@ function Test-ExchangeHafnium {
 		[Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
 		[string[]]
 		$ComputerName = $env:COMPUTERNAME,
-		
+
 		[pscredential]
 		$Credential
 	)
@@ -44,80 +44,80 @@ function Test-ExchangeHafnium {
 			function Get-Cve26855 {
 				[CmdletBinding()]
 				param ()
-				
+
 				$exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
-				
+
 				foreach ($httpProxyLogfile in Get-ChildItem -Recurse -Path "$exchangePath\Logging\HttpProxy" -Filter '*.log') {
 					Import-Csv -Path $httpProxyLogfile.FullName | Where-Object AnchorMailbox -Like 'ServerInfo~*/*' | Select-Object -Property DateTime, RequestId, ClientIPAddress, UrlHost, UrlStem, RoutingHint, UserAgent, AnchorMailbox
 				}
 			}
-			
+
 			function Get-Cve26857 {
 				[CmdletBinding()]
 				param ()
-				
+
 				Get-WinEvent -FilterHashtable @{
-					LogName	     = 'Application'
+					LogName      = 'Application'
 					ProviderName = 'MSExchange Unified Messaging'
-					Level	     = '2'
-				} -ErrorAction SilentlyContinue | Where-Object Message -like "*System.InvalidCastException*"
+					Level        = '2'
+				} -ErrorAction SilentlyContinue | Where-Object Message -Like "*System.InvalidCastException*"
 			}
-			
+
 			function Get-Cve26858 {
 				[CmdletBinding()]
 				param ()
-				
+
 				$exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
-				
+
 				Get-ChildItem -Recurse -Path "$exchangePath\Logging\OABGeneratorLog" | Select-String "Download failed and temporary file" -List | Select-Object -ExpandProperty Path
 			}
-			
+
 			function Get-Cve27065 {
 				[CmdletBinding()]
 				param ()
-				
+
 				$exchangePath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup).MsiInstallPath
-				
+
 				Get-ChildItem -Recurse -Path "$exchangePath\Logging\ECP\Server\*.log" | Select-String "Set-.*VirtualDirectory" -List | Select-Object -ExpandProperty Path
 			}
-			
+
 			function Get-SuspiciousFile {
 				[CmdletBinding()]
 				param ()
-				
+
 				foreach ($file in Get-ChildItem -Recurse -Path "$env:WINDIR\temp\lsass.*dmp") {
 					[PSCustomObject]@{
 						ComputerName = $env:COMPUTERNAME
-						Type		 = 'LsassDump'
-						Path		 = $file.FullName
-						Name		 = $file.Name
+						Type         = 'LsassDump'
+						Path         = $file.FullName
+						Name         = $file.Name
 					}
 				}
 				foreach ($file in Get-ChildItem -Recurse -Path "c:\root\lsass.*dmp") {
 					[PSCustomObject]@{
 						ComputerName = $env:COMPUTERNAME
-						Type		 = 'LsassDump'
-						Path		 = $file.FullName
-						Name		 = $file.Name
+						Type         = 'LsassDump'
+						Path         = $file.FullName
+						Name         = $file.Name
 					}
 				}
-				foreach ($file in Get-ChildItem -Recurse -Path $env:ProgramData -ErrorAction SilentlyContinue | Where-Object Extension -match ".7z|.zip|.rar") {
+				foreach ($file in Get-ChildItem -Recurse -Path $env:ProgramData -ErrorAction SilentlyContinue | Where-Object Extension -Match ".7z|.zip|.rar") {
 					[PSCustomObject]@{
 						ComputerName = $env:COMPUTERNAME
-						Type		 = 'SuspiciousArchive'
-						Path		 = $file.FullName
-						Name		 = $file.Name
+						Type         = 'SuspiciousArchive'
+						Path         = $file.FullName
+						Name         = $file.Name
 					}
 				}
 			}
 			#endregion Functions
-			
+
 			[PSCustomObject]@{
 				ComputerName = $env:COMPUTERNAME
-				Cve26855	 = Get-Cve26855
-				Cve26857	 = Get-Cve26857
-				Cve26858	 = Get-Cve26858
-				Cve27065	 = Get-Cve27065
+				Cve26855     = Get-Cve26855
+				Cve26857     = Get-Cve26857
+				Cve26858     = Get-Cve26858
+				Cve27065     = Get-Cve27065
 				Suspicious   = Get-SuspiciousFile
 			}
 		}
@@ -133,34 +133,34 @@ function Test-ExchangeHafnium {
 }
 
 function Write-HafniumReport {
-<#
+	<#
 	.SYNOPSIS
 		Processes output of Test-ExchangeHafnium for reporting on the console screen.
-	
+
 	.DESCRIPTION
 		Processes output of Test-ExchangeHafnium for reporting on the console screen.
-	
+
 	.PARAMETER InputObject
 		The reports provided by Test-ExchangeHafnium
-	
+
 	.PARAMETER OutPath
 		Path to a FOLDER in which to generate output logfiles.
 		This command will only write to the console screen if no path is provided.
-	
+
 	.EXAMPLE
 		PS C:\> Test-ExchangeHafnium -ComputerName (Get-ExchangeServer).Fqdn | Write-HafniumReport -OutPath C:\logs
-	
+
 		Gather data from all exchange servers in the organization and write a report to C:\logs
 #>
 	[CmdletBinding()]
 	param (
 		[parameter(ValueFromPipeline = $true)]
 		$InputObject,
-		
+
 		[string]
 		$OutPath
 	)
-	
+
 	process {
 		foreach ($report in $InputObject) {
 			Write-Host "Hafnium Status: Exchange Server $($report.ComputerName)"
@@ -169,15 +169,14 @@ function Write-HafniumReport {
 				Write-Host ""
 				continue
 			}
-			
+
 			if ($report.Cve26855) {
 				Write-Host "  [CVE-2021-26855] Suspicious activity found in Http Proxy log!" -ForegroundColor Red
 				if ($OutPath) {
 					$newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-Cve-2021-26855.csv"
 					$report.Cve26855 | Export-Csv -Path $newFile
 					Write-Host "  Report exported to: $newFile"
-				}
-				else {
+				} else {
 					$report.Cve26855 | Format-Table -AutoSize | Out-Host
 				}
 				Write-Host ""
@@ -211,8 +210,7 @@ function Write-HafniumReport {
 					$newFile = Join-Path -Path $OutPath -ChildPath "$($report.ComputerName)-other.csv"
 					$report.Suspicious | Export-Csv -Path $newFile
 					Write-Host "  Report exported to: $newFile"
-				}
-				else {
+				} else {
 					foreach ($entry in $report.Suspicious) {
 						Write-Host "   $($entry.Type) : $($entry.Path)"
 					}
