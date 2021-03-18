68d2d867ae91ad424374578ea989bef9c8109ddb
David Paulson <dpaul@microsoft.com>
2021-03-18 13:34:57 -0500

    Store MSI Copy Script

---
+++ Setup/Utils/CopyMsiOnly.NotPublished.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param(
+    [string]$CuRoot,
+    [string]$CopyToRoot
+)
+
+
+$msiFromCU = Get-ChildItem $CuRoot -Recurse |
+    Where-Object {$_.Name.ToLower().EndsWith(".msi")}
+
+
+foreach($msi in $msiFromCU) {
+
+    $copyTo = $msi.Directory.FullName.Replace($CuRoot, $CopyToRoot)
+
+    if (!(Test-Path $copyTo)) {
+        New-Item $copyTo -ItemType Directory | Out-Null
+    }
+
+    Copy-Item $msi.FullName $copyTo
+}
