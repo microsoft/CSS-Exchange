4f6d50a185c5321007b75af0a1766188fc706935
David Paulson <dpaul@microsoft.com>
2021-03-19 12:36:27 -0500

    Proper Reboot Pending check and how to fix
    Fixed #256

--- Setup/SetupAssist.ps1
+++ Setup/SetupAssist.ps1
@@ -38,8 +38,13 @@ Function GetSidFromLine ([string]$Line) {
 
 # From https://stackoverflow.com/questions/47867949/how-can-i-check-for-a-pending-reboot
 function Test-PendingReboot {
-    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) {
+    if (Get-Item "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) {
         Write-Verbose "Key set in: HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending. Remove it if reboot doesn't work"
+        Write-Verbose ("To Fix, only after reboot does work: `r`n`t" + `
+                "Open regedit, find HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing.`r`n`t" + `
+                "1. If you see PackagesPending, right click it, open Permissions, click on Advanced, change owner to your account. Close Advanced window.`r`n`t`t" + `
+                "Give your account Full Control in Permissions window. Delete the key.`r`n`t" + `
+                "2. Repeat step 1. with Reboot Pending")
         return $true
     }
     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) {
