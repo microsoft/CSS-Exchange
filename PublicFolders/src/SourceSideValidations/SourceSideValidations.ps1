e779ec163aad18cc724865fb7df70675dc16196c
Bill Long <bilong@microsoft.com>
2021-04-13 13:20:49 -0700

    Highlight the action

--- PublicFolders/src/SourceSideValidations/SourceSideValidations.ps1
+++ PublicFolders/src/SourceSideValidations/SourceSideValidations.ps1
@@ -213,7 +213,7 @@ if ($badPermissions.Count -gt 0) {
     Write-Host $badPermissions.Count "invalid permissions were found. These are listed in the following CSV file:"
     Write-Host $badPermissionsFile -ForegroundColor Green
     Write-Host "The invalid permissions can be removed using the RemoveInvalidPermissions switch as follows:"
-    Write-Host ".\SourceSideValidations.ps1 -RemoveInvalidPermissions"
+    Write-Host ".\SourceSideValidations.ps1 -RemoveInvalidPermissions" -ForegroundColor Green
 }
 
 $folderCountMigrationLimit = 250000
