0274fd1e308323eb66ec2e45e48df9ba80f1306c
David Paulson <dpaul@microsoft.com>
2021-04-15 19:46:43 -0500

    Broke out SetupAssist script

---
+++ Setup/src/SetupAssist/Checks/Test-CriticalService.ps1
@@ -0,0 +1,16 @@
+Function Test-CriticalService {
+    $critical = @("MpsSvc")
+    $services = Get-Service -ErrorAction SilentlyContinue
+
+    foreach ($name in $critical) {
+        $service = $services | Where-Object { $_.Name -eq $name }
+
+        if ($null -ne $service) {
+
+            if ($service.Status.ToString() -ne "Running" -or
+                $service.StartType.ToString() -eq "Disabled") {
+                Write-Warning "Critical Service '$name' Status: $($service.Status) StartType: $($service.StartType). Must be running and not disabled."
+            }
+        }
+    }
+}
