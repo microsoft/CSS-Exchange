1d0f2eb1891d182cb572294df5a1e857a1463557
David Paulson <dpaul@microsoft.com>
2021-04-20 20:49:01 -0500

    Added SuppressMessageAttribute for Code formatting
    Need to be able to override to log the functions from Shared.

--- Diagnostics/src/HealthChecker/Writers/Write-Verbose.ps1
+++ Diagnostics/src/HealthChecker/Writers/Write-Verbose.ps1
@@ -1,4 +1,5 @@
 Function Write-Verbose {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidOverwritingBuiltInCmdlets', '', Justification = 'In order to log Write-Verbose from Shared functions')]
     [CmdletBinding()]
     param(
         [Parameter(Position = 1, ValueFromPipeline)]
