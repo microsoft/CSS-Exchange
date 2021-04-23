bfd46f77294eb628824be10f40e33b30a114491c
David Paulson <dpaul@microsoft.com>
2021-04-22 19:58:42 -0500

    Fixed Pester to find any Tests files we create

--- .build/Pester.ps1
+++ .build/Pester.ps1
@@ -1,6 +1,6 @@
-$scripts = @(
-    "$PSScriptRoot\..\Setup\src\Tests\SetupLogReviewer.Tests.ps1"
-)
+$root = Get-Item "$PSScriptRoot\.."
+$scripts = @(Get-ChildItem -Recurse $root |
+    Where-Object { $_.Name -like "*.Tests.ps1" }).FullName
 
 foreach ($script in $scripts) {
     Invoke-Pester -Path $script
