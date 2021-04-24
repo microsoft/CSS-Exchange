f44d64b83572d0ed9a7bc8e11e434671002b085c
Bill Long <bilong@microsoft.com>
2021-04-24 10:41:55 -0500

    Refactor build process to more consistently handle embedded scripts

--- .build/Build.ps1
+++ .build/Build.ps1
@@ -3,6 +3,209 @@ param (
 
 )
 
+<#
+.SYNOPSIS
+    Returns the dot-sourced script path from this line of PowerShell
+    script, if any.
+#>
+function Get-DotSourcedScriptName {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $Line
+    )
+
+    process {
+        $m = $Line | Select-String "\. (?:\.|\`$PSScriptRoot)\\(.*).ps1"
+        if ($m.Matches.Count -gt 0) {
+            $dotloadedScriptPath = $m.Matches[0].Groups[1].Value + ".ps1"
+            $dotloadedScriptPath
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Returns the embedded data file info from this line of PowerShell
+    script, including the file path and variable name it is assigned to.
+#>
+function Get-EmbeddedFileInfo {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $Line
+    )
+
+    process {
+        $m = $Line | Select-String "\`$(.+) = Get-Content `"?(\`$PSScriptRoot|\.)\\([\w|\d|\.|\\]+)`"? -AsByteStream -Raw"
+        if ($m.Matches.Count -gt 0) {
+            [PSCustomObject]@{
+                FilePath     = $m.Matches[0].Groups[3].Value
+                VariableName = $m.Matches[0].Groups[1].Value
+            }
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Returns the embedded file path from this line of PowerShell
+    script, if any. This could be a script or a data file.
+#>
+function Get-EmbeddedFile {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [string]
+        $Line
+    )
+
+    process {
+        $dotSourcedScript = Get-DotSourcedScriptName $Line
+        if ($null -ne $dotSourcedScript) {
+            $dotSourcedScript
+            return
+        }
+
+        $dotSourcedData = Get-EmbeddedFileInfo $Line
+        if ($null -ne $dotSourcedData) {
+            $dotSourcedData.FilePath
+            return
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Gets the list of all embedded scripts and data files in this
+    script.
+#>
+function Get-EmbeddedFileList {
+    [CmdletBinding()]
+    [OutputType([string[]])]
+    param (
+        [Parameter()]
+        [string]
+        $File,
+
+        [Parameter()]
+        [System.Collections.Generic.HashSet[string]]
+        $FilesAlreadyAdded = (New-Object 'System.Collections.Generic.HashSet[string]')
+    )
+
+    process {
+        $files = New-Object 'System.Collections.Generic.List[string]'
+        $directoryOfCurrentFile = (Get-Item $File).Directory
+        Get-Content $File | Get-EmbeddedFile | ForEach-Object {
+            $absolutePath = (Get-Item (Join-Path $directoryOfCurrentFile $_)).FullName
+            if ($FilesAlreadyAdded.Add($absolutePath)) {
+                $files.Add($absolutePath)
+                if ($_ -like "*.ps1") {
+                    Get-EmbeddedFileList $absolutePath $FilesAlreadyAdded | ForEach-Object {
+                        $files.Add($_)
+                    }
+                }
+            }
+        }
+
+        if ($files.Count -gt 0) {
+            $files
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Gets the most recent commit, considering all files related to the specified script,
+    including any embedded dot-sourced scripts or other files.
+#>
+function Get-ScriptProjectMostRecentCommit {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $File
+    )
+
+    process {
+        Write-Verbose "Get-ScriptProjectMostRecentCommit called for file $File"
+        $mostRecentCommit = [DateTime]::MinValue
+        if ([DateTime]::TryParse((git log -n 1 --format="%ad" --date=rfc $File), [ref] $mostRecentCommit)) {
+            Get-EmbeddedFileList $File | ForEach-Object {
+                Write-Verbose "Getting commit time for $_"
+                $commitTime = [DateTime]::Parse((git log -n 1 --format="%ad" --date=rfc $_))
+                if ($commitTime -gt $mostRecentCommit) {
+                    $mostRecentCommit = $commitTime
+                    Write-Host ("Changing commit time to: $($commitTime.ToString("yy.MM.dd.HHmm"))")
+                }
+            }
+        }
+
+        $mostRecentCommit
+    }
+}
+
+<#
+.SYNOPSIS
+    Gets the content of the designated file, embedding the content of
+    any dot-sourced scripts or embedded data files.
+#>
+function Get-ExpandedScriptContent {
+    [CmdletBinding()]
+    [OutputType([System.Collections.Generic.List[string]])]
+    param (
+        [Parameter()]
+        [string]
+        $File,
+
+        [Parameter()]
+        [System.Collections.Generic.HashSet[string]]
+        $ScriptsAlreadyEmbedded = (New-Object 'System.Collections.Generic.HashSet[string]')
+    )
+
+    begin {
+        $currentFolder = (Get-Item $File).DirectoryName
+        Write-Verbose "Get-ExpandedScriptContent called for file $File"
+    }
+
+    process {
+        $scriptContent = New-Object 'System.Collections.Generic.List[string]'
+        $scriptContent.AddRange([IO.File]::ReadAllLines($File))
+        for ($i = 0; $i -lt $scriptContent.Count; $i++) {
+            $line = $scriptContent[$i]
+            $dotSourcedFile = $line | Get-DotSourcedScriptName
+            if ($null -ne $dotSourcedFile) {
+                $scriptContent.RemoveAt($i)
+                $dotSourcedFile = (Get-Item (Join-Path $currentFolder $dotSourcedFile))
+                # Scripts must only be embedded once
+                if ($ScriptsAlreadyEmbedded.Add($dotSourcedFile)) {
+                    $dotSourcedFileContent = Get-ExpandedScriptContent $dotSourcedFile $ScriptsAlreadyEmbedded
+                    $scriptContent.InsertRange($i, $dotSourcedFileContent)
+                }
+            } else {
+                $embeddedFile = $line | Get-EmbeddedFileInfo
+                if ($null -ne $embeddedFile) {
+                    $absolutePath = (Get-Item (Join-Path $currentFolder $embeddedFile.FilePath)).FullName
+                    $fileAsBase64 = [Convert]::ToBase64String(([IO.File]::ReadAllBytes($absolutePath)), "InsertLineBreaks")
+                    $scriptContent.RemoveAt($i)
+                    [string[]]$linesToInsert = @()
+                    $linesToInsert += "`$$($embeddedFile.VariableName)Base64 = @'"
+                    $linesToInsert += $fileAsBase64
+                    $linesToInsert += "'@"
+                    $linesToInsert += ""
+                    $linesToInsert += "`$$($embeddedFile.VariableName) = [Convert]::FromBase64String(`$$($embeddedFile.VariableName)Base64)"
+                    $scriptContent.InsertRange($i, $linesToInsert)
+                }
+            }
+        }
+
+        Write-Output -NoEnumerate $scriptContent
+    }
+}
+
 $repoRoot = Get-Item "$PSScriptRoot\.."
 
 <#
@@ -59,94 +262,47 @@ $scriptFiles = $scriptFiles | Where-Object {
     $m.Count -lt 1
 }
 
-<#
-    Copy the remaining files to dist, expand dot-sourced files,
-    add disclaimer and version.
-#>
-
-$disclaimer = [IO.File]::ReadAllLines("$repoRoot\.build\disclaimer.txt")
+# Build the files
 
 $scriptVersions = @()
 
+$disclaimer = [IO.File]::ReadAllLines("$PSScriptRoot\disclaimer.txt")
+
 $scriptFiles | ForEach-Object {
-    $scriptContent = New-Object 'System.Collections.Generic.List[string]'
-    $scriptContent.AddRange([IO.File]::ReadAllLines($_))
+    $scriptName = [IO.Path]::GetFileName($_)
 
-    $commitTime = [DateTime]::Parse((git log -n 1 --format="%ad" --date=rfc $_))
+    # Expand the embedded files
+    $expandedScript = Get-ExpandedScriptContent $_
 
-    # Expand dot-sourced files
-    for ($i = 0; $i -lt $scriptContent.Count; $i++) {
-        $line = $scriptContent[$i].Trim()
-        $m = $line | Select-String "\. (?:\.|\`$PSScriptRoot)\\(.*).ps1"
-
-        if ($m.Matches.Count -gt 0) {
-            $parentPath = [IO.Path]::GetDirectoryName($_)
-            $dotloadedScriptPath = [IO.Path]::Combine($parentPath, $m.Matches[0].Groups[1].Value + ".ps1")
-            $dotloadedScriptContent = [IO.File]::ReadAllLines($dotloadedScriptPath)
-            $scriptContent.RemoveAt($i)
-            $scriptContent.InsertRange($i, $dotloadedScriptContent)
-            $commitTimeTest = [DateTime]::Parse((git log -n 1 --format="%ad" --date=rfc $dotloadedScriptPath))
-
-            if ($commitTimeTest -gt $commitTime) {
-                $commitTime = $commitTimeTest
-                Write-Host ("Changing commit time to: $($commitTime.ToString("yy.MM.dd.HHmm"))")
-            }
-        }
-    }
-
-    # Expand Get-Content calls for local files that are marked -AsByteStream -Raw
-    for ($i = 0; $i -lt $scriptContent.Count; $i++) {
-        $line = $scriptContent[$i].Trim()
-        $m = $line | Select-String "\`$(.+) = Get-Content `"?(\`$PSScriptRoot|\.)\\([\w|\d|\.|\\]+)`"? -AsByteStream -Raw"
-        if ($m.Matches.Count -gt 0) {
-            $parentPath = [IO.Path]::GetDirectoryName($_)
-            $filePath = [IO.Path]::Combine($parentPath, $m.Matches[0].Groups[3].Value)
-            $fileAsBase64 = [Convert]::ToBase64String(([IO.File]::ReadAllBytes($filePath)), "InsertLineBreaks")
-            $scriptContent.RemoveAt($i)
-            [string[]]$linesToInsert = @()
-            $linesToInsert += "`$$($m.Matches[0].Groups[1].Value)Base64 = @'"
-            $linesToInsert += $fileAsBase64
-            $linesToInsert += "'@"
-            $linesToInsert += ""
-            $linesToInsert += "`$$($m.Matches[0].Groups[1].Value) = [Convert]::FromBase64String(`$$($m.Matches[0].Groups[1].Value)Base64)"
-            $scriptContent.InsertRange($i, $linesToInsert)
-            $commitTimeTest = [DateTime]::Parse((git log -n 1 --format="%ad" --date=rfc $filePath))
-
-            if ($commitTimeTest -gt $commitTime) {
-                $commitTime = $commitTimeTest
-                Write-Host ("Changing commit time to: $($commitTime.ToString("yy.MM.dd.HHmm"))")
-            }
-        }
-    }
-
+    # Add the version information
+    $commitTime = Get-ScriptProjectMostRecentCommit $_
     $buildVersionString = $commitTime.ToString("yy.MM.dd.HHmm")
     Write-Host ("Setting version for script '$_' to $buildVersionString")
 
     # Set version variable if present
-    for ($i = 0; $i -lt $scriptContent.Count; $i++) {
-        $line = $scriptContent[$i]
+    for ($i = 0; $i -lt $expandedScript.Count; $i++) {
+        $line = $expandedScript[$i]
         if ($line.Contains("`$BuildVersion = `"`"")) {
             $newLine = $line.Replace("`$BuildVersion = `"`"", "`$BuildVersion = `"$buildVersionString`"")
             Write-Host $newLine
-            $scriptContent.RemoveAt($i)
-            $scriptContent.Insert($i, $newLine)
+            $expandedScript.RemoveAt($i)
+            $expandedScript.Insert($i, $newLine)
         }
     }
 
     # Stamp version in comments
-    $scriptContent.Insert(0, "")
-    $scriptContent.Insert(0, "# Version $buildVersionString")
+    $expandedScript.Insert(0, "")
+    $expandedScript.Insert(0, "# Version $buildVersionString")
 
     # Add disclaimer
-    $scriptContent.Insert(0, "")
-    $scriptContent.InsertRange(0, $disclaimer)
+    $expandedScript.Insert(0, "")
+    $expandedScript.InsertRange(0, $disclaimer)
 
+    Set-Content -Path (Join-Path $distFolder $scriptName) -Value $expandedScript
     $scriptVersions += [PSCustomObject]@{
-        File    = [IO.Path]::GetFileName($_)
-        Version = $commitTime.ToString("yy.MM.dd.HHmm")
+        File    = $scriptName
+        Version = $buildVersionString
     }
-
-    Set-Content -Path ([IO.Path]::Combine($distFolder, [IO.Path]::GetFileName($_))) -Value $scriptContent
 }
 
 # Generate version text for release description
