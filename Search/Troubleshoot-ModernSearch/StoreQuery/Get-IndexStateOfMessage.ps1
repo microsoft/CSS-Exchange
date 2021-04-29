b6a36fa18921acb4d2bb3bd12221c80c9a05286b
David Paulson <dpaul@microsoft.com>
2021-04-29 14:42:10 -0500

    Fixed Index State of Message determination
    Fixed a few issues with determining the correct index state.
    
    Resolved #569

--- Search/Troubleshoot-ModernSearch/StoreQuery/Get-IndexStateOfMessage.ps1
+++ Search/Troubleshoot-ModernSearch/StoreQuery/Get-IndexStateOfMessage.ps1
@@ -16,17 +16,19 @@
         if ($Message.p365A0003 -gt 0) {
             $status = "ShouldNotBeIndexed"
         } elseif ($Message.BigFunnelPOISize -gt 0 -and
+            $Message.BigFunnelPOISize -ne "NULL" -and
             $Message.p3655000B -eq $true -and
             ($Message.($BigFunnelPropNameMapping.IsPartiallyIndexed) -eq "NULL" -or
                 $Message.($BigFunnelPropNameMapping.IsPartiallyIndexed) -eq $false)) {
             $status = "Indexed"
         } elseif ($Message.BigFunnelPOISize -gt 0 -and
+            $Message.BigFunnelPOISize -ne "NULL" -and
             $Message.p3655000B -eq $true -and
             $Message.($BigFunnelPropNameMapping.IsPartiallyIndexed) -eq $true) {
             $status = "PartiallyIndexed"
         } elseif (($Message.BigFunnelPOISize -eq "NULL" -or
                 $Message.BigFunnelPOISize -le 0) -and
-            ($Message.p3655000B -eq "NULL" -or
+            ($Message.p3655000B.ToString() -eq "NULL" -or
                 $Message.p3655000B -eq $false)) {
             $status = "NotIndexed"
         } elseif (($Message.BigFunnelPOISize -eq "NULL" -or
@@ -34,7 +36,7 @@
             $Message.p3655000B -eq $true) {
             $status = "Corrupted"
         } elseif ($Message.BigFunnelPOISize -gt 0 -and
-            ($Message.p3655000B -eq "NULL" -or
+            ($Message.p3655000B.ToString() -eq "NULL" -or
                 $Message.p3655000B -eq $false)) {
             $status = "Stale"
         }
