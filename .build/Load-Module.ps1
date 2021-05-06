09b34227e6eba97dff33ca361d4dfc5b3c92c003
Bill Long <bilong@microsoft.com>
2021-05-06 13:50:50 -0500

    Add a Load-Module function for build script use

---
+++ .build/Load-Module.ps1
@@ -0,0 +1,52 @@
+﻿function Load-Module {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory = $false)]
+        [string]
+        $MinimumVersion
+    )
+
+    $moduleAlreadyLoaded = Get-Module -Name $Name
+    if ($null -ne $moduleAlreadyLoaded) {
+        if ([string]::IsNullOrEmpty($MinimumVersion) -or $moduleAlreadyLoaded.Version -ge $MinimumVersion) {
+            return $true
+        } else {
+            Remove-Module -Name $Name
+        }
+    }
+
+    $modulesOnDisk = @(Get-Module -Name $Name -ListAvailable | Sort-Object Version -Descending)
+    $moduleToLoad = $null
+    foreach ($module in $modulesOnDisk) {
+        if ([string]::IsNullOrEmpty($MinimumVersion) -or $module.Version -ge $MinimumVersion) {
+            $moduleToLoad = $module
+            break
+        }
+    }
+
+    if ($null -ne $moduleToLoad) {
+        Import-Module $module
+        return $true
+    }
+
+    $params = @{
+        Name = $Name
+    }
+
+    if (-not [string]::IsNullOrEmpty($MinimumVersion)) {
+        $params.MinimumVersion = $MinimumVersion
+    }
+
+    $errorCount = $Error.Count
+    Install-Module @params -Force
+    if ($Error.Count -gt $errorCount) {
+        return $false
+    }
+
+    return $true
+}
--- .build/Pester.ps1
+++ .build/Pester.ps1
@@ -1,4 +1,10 @@
-﻿$root = Get-Item "$PSScriptRoot\.."
+. $PSScriptRoot\Load-Module.ps1
+
+if (-not (Load-Module -Name Pester -MinimumVersion 5.2.0)) {
+    throw "Pester module could not be loaded"
+}
+
+$root = Get-Item "$PSScriptRoot\.."
 $scripts = @(Get-ChildItem -Recurse $root |
         Where-Object { $_.Name -like "*.Tests.ps1" }).FullName
 
