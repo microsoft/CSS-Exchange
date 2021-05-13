866935cb0e5f1cf65c709098647540c9bbe55bdb
Ahmed Ashour <ahmed_ashour2002@hotmail.com>
2021-05-13 11:19:27 +0000

    rearrange sorting

--- Transport/Compute-TopExoRecipientsFromMessageTrace.ps1
+++ Transport/Compute-TopExoRecipientsFromMessageTrace.ps1
@@ -44,7 +44,7 @@ param
 $CreateHourlyReport =
 {
     param($eventList, $Threshold)
-    $eventList = $eventList | Sort RecipientAddress, Received
+    $eventList = $eventList | Sort Received, RecipientAddress
     $eventList.foreach(
         {
             $event = $_
