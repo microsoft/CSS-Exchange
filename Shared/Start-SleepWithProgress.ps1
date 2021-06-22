f5a8ff564df7286bfa01588ab69c83ffb14c5942
Canthv0 <matbyrd@microsoft.com>
2021-06-22 14:00:14 -0400

    Added help

--- Shared/Start-SleepWithProgress.ps1
+++ Shared/Start-SleepWithProgress.ps1
@@ -1,4 +1,32 @@
-﻿# Sleeps X seconds and displays a progress bar
+﻿<#
+.NOTES
+	Name: Start-SleepWithProgress.ps1
+	Requires: NA
+    Major Release History:
+        06/22/2021 - Initial Release
+
+.SYNOPSIS
+Sleep with a progress bar managing the bar and the countdown.
+
+.DESCRIPTION
+Sleeps X amount of time showing a progress bar.
+
+.PARAMETER SleepTime
+Amount of time to sleep.
+
+.PARAMETER Message
+Message to display on the progress bar.
+
+.OUTPUTS
+Progress bar to screen
+
+.EXAMPLE
+Start-SleepWithProgress -SleepTime 60 -Message "Waiting on Process to complete"
+
+Creates a Progress bar with the message "Waiting on Process to complete"
+Counts down 60 seconds and updates the Progress bar during the proess.
+
+#>
 Function Start-SleepWithProgress {
     Param(
         [Parameter(Mandatory = $true)]
@@ -19,5 +47,5 @@ Function Start-SleepWithProgress {
         Start-Sleep 1
     }
 
-    Write-Progress -Completed -Activity "Sleeping" -Status " "
+    Write-Progress -Completed -Activity $Message -Status " "
 }
