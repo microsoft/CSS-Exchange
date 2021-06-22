d646cc4d96e24db983ac4f9bdb79819ab9fb63d7
Canthv0 <matbyrd@microsoft.com>
2021-06-22 13:52:24 -0400

    Added help

--- Shared/Write-SimpleLogFile.ps1
+++ Shared/Write-SimpleLogFile.ps1
@@ -1,4 +1,46 @@
-﻿# Writes output to a log file with a time date stamp
+﻿<#
+.NOTES
+	Name: Write-SimpleLogFile.ps1
+	Requires: NA
+    Major Release History:
+        06/22/2021 - Initial Release
+
+.SYNOPSIS
+Supports writing a basic log file to LocalAppData
+
+.DESCRIPTION
+Supports basic log file generation for other scripts.
+Places the log file into the $env:LocalAppData Folder.
+
+Supports outputing to the host as well as the log files.
+
+.PARAMETER String
+String to be written into the log file.
+
+.PARAMETER Name
+Name of the log file.
+
+.PARAMETER OutHost
+Switch that will write the output to the host as well as the log file.
+
+.PARAMETER OpenLog
+Opens the log file in notepad.
+
+.OUTPUTS
+Log file specified in the -Name parameter.
+Writes the file intothe $Env:LocalAppData
+
+.EXAMPLE
+Write-SimpleLogFile -String "Start ProcessA" -Name mylogfile.log
+
+Writes "[Date] - Start ProcessA" to $env:LocalAppData\mylogfile.log
+
+.EXAMPLE
+Write-SimpleLogFile -String "Start ProcessB" -Name mylogfile.log -OutHost
+
+Writes "[Date] - Start ProcessB" to $env:LocalAppData\mylogfile and to the Host
+
+#>
 Function Write-SimpleLogfile {
     [cmdletbinding()]
     Param
