ad98d533171afb1a91692ee194e66e4fe0b620e7
Bill Long <bilong@microsoft.com>
2021-07-19 10:18:24 -0500

    SetupAssist check for Computers container

---
+++ Setup/SetupAssist/Checks/Test-ComputersContainerExists.ps1
@@ -0,0 +1,13 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Function Test-ComputersContainerExists {
+    $forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+    foreach ($domain in $forest.Domains) {
+        $domainDN = $domain.GetDirectoryEntry().distinguishedName
+        $computersPath = ("LDAP://CN=Computers," + $domainDN)
+        if (-not [System.DirectoryServices.DirectoryEntry]::Exists($computersPath)) {
+            "The Computers container in domain $domainDN has been renamed or deleted. This will cause /PrepareAd to fail in some scenarios." | Receive-Output -IsWarning
+        }
+    }
+}
