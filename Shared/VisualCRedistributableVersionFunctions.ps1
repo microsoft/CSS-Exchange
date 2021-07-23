315bca99e873d6269f5e678bf0405c10a4741686
David Paulson <dpaul@microsoft.com>
2021-07-23 12:15:11 -0500

    Created a reusable Visual C Redistributable Version Status Function
    This function allows you to determine if the version is installed and if it is up to date or not. Makes it easy to use it within other scripts.

--- Shared/VisualCRedistributableVersionFunctions.ps1
+++ Shared/VisualCRedistributableVersionFunctions.ps1
@@ -37,3 +37,53 @@ Function Get-VisualCRedistributableVersion {
         return $softwareList
     }
 }
+
+Function Get-VisualCRedistributable2012Information {
+    return [PSCustomObject]@{
+        VersionNumber = 184610406
+        DownloadUrl   = "https://www.microsoft.com/en-us/download/details.aspx?id=30679"
+        DisplayName   = "Microsoft Visual C++ 2012*"
+    }
+}
+
+Function Get-VisualCRedistributable2013Information {
+    return [PSCustomObject]@{
+        VersionNumber = 201367256
+        DownloadUrl   = "https://support.microsoft.com/en-us/topic/update-for-visual-c-2013-redistributable-package-d8ccd6a5-4e26-c290-517b-8da6cfdf4f10"
+        DisplayName   = "Microsoft Visual C++ 2013*"
+    }
+}
+
+#Returns a 0 if no version is detected
+#Returns a 1 if version is detected, but the the version we wanted.
+#Returns a 2 if version is up to date.
+Function Get-VcRedistributableVersionStatus {
+    [CmdletBinding()]
+    param(
+        [object]$VisualCRedistributableVersion,
+        [Parameter(Mandatory = $true)]
+        [object]$VersionInformation
+    )
+    begin {
+        $versionDetected = $true
+        $value = 0
+    }
+    process {
+        foreach ($detectVersion in $VisualCRedistributableVersion) {
+            if ($detectVersion.DisplayName -like $VersionInformation.DisplayName) {
+                $versionDetected = $true
+
+                if ($detectVersion.VersionIdentifier -eq $VersionInformation.VersionNumber) {
+                    $value += 1
+                    return
+                }
+            }
+        }
+    }
+    end {
+        if ($versionDetected) {
+            $value += 1
+        }
+        return $value
+    }
+}
