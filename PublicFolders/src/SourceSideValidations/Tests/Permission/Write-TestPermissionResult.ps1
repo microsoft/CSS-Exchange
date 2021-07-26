f80773dba8e99b6908741ff2b856793e28c99d62
Bill Long <bilong@microsoft.com>
2021-07-26 06:44:16 -0700

    Adjust BadPermission result summary

--- PublicFolders/src/SourceSideValidations/Tests/Permission/Write-TestPermissionResult.ps1
+++ PublicFolders/src/SourceSideValidations/Tests/Permission/Write-TestPermissionResult.ps1
@@ -20,6 +20,7 @@
         if ($badPermissionCount -gt 0) {
             Write-Host
             Write-Host $badPermissionCount "invalid permissions were found."
+            Write-Host "These are shown in the results CSV with a result type of BadPermission."
             Write-Host "The invalid permissions can be removed using the RemoveInvalidPermissions switch as follows:"
             Write-Host ".\SourceSideValidations.ps1 -RemoveInvalidPermissions" -ForegroundColor Green
         }
