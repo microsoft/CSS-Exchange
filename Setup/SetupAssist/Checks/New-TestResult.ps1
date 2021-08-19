f2221a22da75c5f01d4769787c3417644d3a93ba
Bill Long <bilong@microsoft.com>
2021-08-19 18:10:06 -0500

    Strictly enforce input to New-TestResult

--- Setup/SetupAssist/Checks/New-TestResult.ps1
+++ Setup/SetupAssist/Checks/New-TestResult.ps1
@@ -2,11 +2,20 @@
 # Licensed under the MIT License.
 
 Function New-TestResult {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'Does not change state')]
     [CmdletBinding()]
     param(
+        [Parameter(Mandatory = $true)]
         [string]$TestName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet("Passed", "Failed", "Warning")]
         [string]$Result,
+
+        [Parameter(Mandatory = $false)]
         [string]$AdditionalContext,
+
+        [Parameter(Mandatory = $false)]
         [object]$CustomData
     )
 
