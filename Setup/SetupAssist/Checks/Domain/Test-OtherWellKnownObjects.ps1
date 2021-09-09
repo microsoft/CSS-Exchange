3afe724da5a654558341bfff882e934a53ca2db9
Bill Long <bilong@microsoft.com>
2021-09-09 15:04:44 -0700

    Remove ldifde dependency at test time - fixes #696

--- Setup/SetupAssist/Checks/Domain/Test-OtherWellKnownObjects.ps1
+++ Setup/SetupAssist/Checks/Domain/Test-OtherWellKnownObjects.ps1
@@ -6,29 +6,17 @@
 Function Test-OtherWellKnownObjects {
     $rootDSE = [ADSI]("LDAP://RootDSE")
     $exchangeContainerPath = ("CN=Microsoft Exchange,CN=Services," + $rootDSE.configurationNamingContext)
-    $filePath = "$PSScriptRoot\ExchangeContainerOriginal.txt"
-    $importFilePath = "ExchangeContainerImport.txt"
+    $exchangeContainer = [ADSI]("LDAP://" + $exchangeContainerPath)
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($exchangeContainer, "(objectClass=*)", @("otherWellKnownObjects", "distinguishedName"))
+    $result = $searcher.FindOne()
 
-    ldifde -d $exchangeContainerPath -p Base -l otherWellKnownObjects -f $filePath | Out-Null
-
-    $ldifObjects = @(Get-Content $filePath | ConvertFrom-Ldif)
-
-    if ($ldifObjects.Length -lt 1) {
-        throw "Failed to export $([IO.Path]::GetFileName($filePath)) file"
-    }
-
-    if ($ldifObjects.Length -gt 1) {
-        throw "Unexpected LDIF data."
-    }
-
-    $exchangeContainer = $ldifObjects[0]
-    $otherWellKnownObjects = @($exchangeContainer.otherWellKnownObjects)
+    $importFilePath = "$PSScriptRoot\ExchangeContainerImport.txt"
     $outputLines = New-Object 'System.Collections.Generic.List[string]'
-    $outputLines.Add("dn: $($exchangeContainer.dn[0])")
+    $outputLines.Add("dn: $($result.Properties["distinguishedName"][0].ToString())")
     $outputLines.Add("changeType: modify")
     $outputLines.Add("replace: otherWellKnownObjects")
     $badItemsFound = $false
-    foreach ($value in $otherWellKnownObjects) {
+    foreach ($value in $result.Properties["otherWellKnownObjects"]) {
 
         $params = @{
             TestName = "Other Well Known Objects"
@@ -38,8 +26,8 @@ Function Test-OtherWellKnownObjects {
         if ($value -like "*CN=Deleted Objects*") {
             $badItemsFound = $true
             New-TestResult @params -Result "Failed" -ReferenceInfo (
-                "Verify the results in $importFilePath. Then run the following command:`n" +
-                "     ldifde -i -f $importFilePath`n" +
+                "Verify the results in $importFilePath. Then run the following command:`n`n" +
+                "     ldifde -i -f $importFilePath`n`n" +
                 "Then, run Setup.exe /PrepareAD to recreate the deleted groups.")
         } else {
             $outputLines.Add("otherWellKnownObjects: $value")
@@ -51,7 +39,5 @@ Function Test-OtherWellKnownObjects {
         $outputLines.Add("-")
         $outputLines.Add("")
         $outputLines | Out-File -FilePath $importFilePath
-    } else {
-        Remove-Item $filePath -Force
     }
 }
