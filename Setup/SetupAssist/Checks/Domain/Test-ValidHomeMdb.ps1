051ebc25cd9600ecdf6c36197d30d2c6ecdb1a5f
David Paulson <dpaul@microsoft.com>
2021-09-10 11:24:38 -0500

    Removed ldifde dependency

--- Setup/SetupAssist/Checks/Domain/Test-ValidHomeMdb.ps1
+++ Setup/SetupAssist/Checks/Domain/Test-ValidHomeMdb.ps1
@@ -1,46 +1,43 @@
 ﻿# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+. $PSScriptRoot\..\New-TestResult.ps1
 Function Test-ValidHomeMDB {
-    $filePath = "$PSScriptRoot\validHomeMdb.txt"
+    $testName = "Valid Home MDB"
     $rootDSE = [ADSI]("LDAP://RootDSE")
+    $container = [ADSI]("LDAP://$($rootDSE.rootDomainNamingContext)")
     $arbitration = 0x800000
     $discovery = 0x20000000
     $publicFolder = 0x1000000000
     $recipientTypes = $arbitration -bor $discovery -bor $publicFolder
-    ldifde -t 3268 -r "(&(objectClass=user)(mailnickname=*)(msExchRecipientTypeDetails:1.2.840.113556.1.4.804:=$recipientTypes))" `
-        -l "distinguishedName,homeMDB" -f $filePath -d $rootDSE.rootDomainNamingContext | Out-Null
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($container,
+        "(&(objectClass=user)(mailnickname=*)(msExchRecipientTypeDetails:1.2.840.113556.1.4.804:=$recipientTypes))",
+        @("distinguishedName", "homeMDB"))
 
-    $ldifeObject = @(Get-Content $filePath | ConvertFrom-Ldif)
-    $testName = "Valid Home MDB"
+    $results = $searcher.FindAll()
 
-    if ($ldifeObject.Count -gt 0) {
+    if ($null -ne $results -and
+        $results.Count -gt 0) {
 
-        foreach ($result in $ldifeObject) {
-            $dbName = $result.homeMDB
+        foreach ($result in $results) {
+            $dbName = $result.Properties["homeMDB"]
             $params = @{
                 TestName      = $testName
-                Details       = @("Mailbox DN: $($result.dn)",
+                Details       = ("Mailbox DN: $($result.Properties["distinguishedName"])`n" +
                     "Database DN: $dbName")
-                ReferenceInfo = @("Run the following command in EMS.",
-                    "If EMS is down, launch PowerShell and run `"Add-PSSnapin *Exchange*`"",
+                ReferenceInfo = ("Run the following command in EMS.`n" +
+                    "If EMS is down, launch PowerShell and run `"Add-PSSnapin *Exchange*`"`n" +
                     "    Set-Mailbox 'DN' -Database 'DB_Name'")
             }
 
-            if (![string]::IsNullOrEmpty($dbName)) {
-
-                if (!([ADSI]::Exists("LDAP://$dbName"))) {
-                    New-TestResult @params -Result "Failed"
-                } else {
-                    New-TestResult @params -Result "Passed"
-                }
+            if ((-not([string]::IsNullOrEmpty($dbName))) -and
+                ([ADSI]::Exists("LDAP://$dbName"))) {
+                New-TestResult @params -Result "Passed"
             } else {
                 New-TestResult @params -Result "Failed"
             }
         }
-
-        Remove-Item $filePath -Force
     } else {
-        New-TestResult -TestName $testName -Result "Failed" -Details "Unexpected LDIF Data"
+        New-TestResult -TestName $testName -Result "Failed" -Details "Failed to find any critical mailboxes"
     }
 }
