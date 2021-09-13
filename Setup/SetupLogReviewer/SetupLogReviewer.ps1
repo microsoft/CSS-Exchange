5c686e71d30c289b73ee8784f727dd2ecc79a3d9
David Paulson <dpaul@microsoft.com>
2021-09-13 09:40:25 -0500

    Broke out logic into Shared location
    This allows both scripts to be fully utilized with both capabilities, once added to the SetupAssist that is.
    
    While at this point you could remove SetupLogReviewer, to make things easier could have SetupLogReviewer recommended for engineer reviewing while the SetupAssist is designed for on the server.

--- Setup/SetupLogReviewer/SetupLogReviewer.ps1
+++ Setup/SetupLogReviewer/SetupLogReviewer.ps1
@@ -14,81 +14,10 @@ param(
     [switch]$DelegatedSetup
 )
 
-. $PSScriptRoot\..\Shared\SetupLogReviewerFunctions.ps1
-. $PSScriptRoot\Checks\Test-KnownIssuesByErrors.ps1
-. $PSScriptRoot\Checks\Test-KnownLdifErrors.ps1
-. $PSScriptRoot\Checks\Test-KnownMsiIssuesCheck.ps1
-. $PSScriptRoot\Checks\Test-KnownOrganizationPreparationErrors.ps1
-. $PSScriptRoot\Checks\Test-PrerequisiteCheck.ps1
-. $PSScriptRoot\Checks\Write-LastErrorInformation.ps1
-. $PSScriptRoot\Checks\Write-Result.ps1
-
-Function InvokeTests {
-    [CmdletBinding()]
-    param(
-        [object]$SetupLogReviewer,
-        [string[]]$Tests
-    )
-
-    foreach ($test in $Tests) {
-        $result = $SetupLogReviewer | & $test
-
-        if ($null -ne $result) {
-            $result | Write-Result
-            break
-        }
-    }
-}
-
-Function Main {
-
-    if (-not ([IO.File]::Exists($SetupLog))) {
-        Write-Error "Could not find file: $SetupLog"
-        return
-    }
-
-    $setupLogReviewer = Get-SetupLogReviewer -SetupLog $SetupLog
-    $color = "Gray"
-    $ranDate = $setupLogReviewer.SetupRunDate
-
-    if ($ranDate -lt ([DateTime]::Now.AddDays(-14))) { $color = "Yellow" }
-    Write-Host "Setup.exe Run Date: $ranDate" -ForegroundColor $color
-    Write-Host "Setup.exe Build Number: $($setupLogReviewer.SetupBuildNumber)"
-
-    if (-not ([string]::IsNullOrEmpty($setupLogReviewer.LocalBuildNumber))) {
-        Write-Host "Current Exchange Build: $($setupLogReviewer.LocalBuildNumber)"
-
-        if ($setupLogReviewer.LocalBuildNumber -eq $setupLogReviewer.SetupBuildNumber) {
-            Write-Host "Same build number detected..... if using powershell.exe to start setup. Make sure you do '.\setup.exe'" -ForegroundColor "Red"
-        }
-    }
-
-    $successFullInstall = $setupLogReviewer | SelectStringLastRunOfExchangeSetup -Pattern "The Exchange Server setup operation completed successfully\."
-
-    if ($null -ne $successFullInstall) {
-        Write-Host "The most recent setup attempt completed successfully based off this line:"
-        Write-Host $successFullInstall.Line
-        Write-Host "`r`nNo Action is required."
-        return
-    }
-
-    if ($DelegatedSetup) {
-        $setupLogReviewer | Test-DelegatedInstallerHasProperRights
-        return
-    }
-
-    InvokeTests -SetupLogReviewer $setupLogReviewer -Tests @(
-        "Test-PrerequisiteCheck",
-        "Test-KnownLdifErrors",
-        "Test-KnownOrganizationPreparationErrors",
-        "Test-KnownMsiIssuesCheck",
-        "Test-KnownIssuesByErrors",
-        "Write-LastErrorInformation"
-    )
-}
+. $PSScriptRoot\..\Shared\SetupLogReviewerLogic.ps1
 
 try {
-    Main
+    Invoke-SetupLogReviewer -SetupLog $SetupLog -DelegatedSetup:$DelegatedSetup
 } catch {
     "$($Error[0].Exception)" | Write-Output
     "$($Error[0].ScriptStackTrace)" | Write-Output
---
+++ Setup/Shared/SetupLogReviewerLogic.ps1
@@ -0,0 +1,80 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\SetupLogReviewerFunctions.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Test-KnownIssuesByErrors.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Test-KnownLdifErrors.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Test-KnownMsiIssuesCheck.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Test-KnownOrganizationPreparationErrors.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Test-PrerequisiteCheck.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Write-LastErrorInformation.ps1
+. $PSScriptRoot\..\SetupLogReviewer\Checks\Write-Result.ps1
+Function Invoke-SetupLogReviewer {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true,
+            Position = 0)]
+        [System.IO.FileInfo]$SetupLog,
+        [switch]$DelegatedSetup
+    )
+    Function InvokeTests {
+        [CmdletBinding()]
+        param(
+            [object]$SetupLogReviewer,
+            [string[]]$Tests
+        )
+
+        foreach ($test in $Tests) {
+            $result = $SetupLogReviewer | & $test
+
+            if ($null -ne $result) {
+                $result | Write-Result
+                break
+            }
+        }
+    }
+
+    if (-not ([IO.File]::Exists($SetupLog))) {
+        Write-Error "Could not find file: $SetupLog"
+        return
+    }
+
+    $setupLogReviewer = Get-SetupLogReviewer -SetupLog $SetupLog
+    $color = "Gray"
+    $ranDate = $setupLogReviewer.SetupRunDate
+
+    if ($ranDate -lt ([DateTime]::Now.AddDays(-14))) { $color = "Yellow" }
+    Write-Host "Setup.exe Run Date: $ranDate" -ForegroundColor $color
+    Write-Host "Setup.exe Build Number: $($setupLogReviewer.SetupBuildNumber)"
+
+    if (-not ([string]::IsNullOrEmpty($setupLogReviewer.LocalBuildNumber))) {
+        Write-Host "Current Exchange Build: $($setupLogReviewer.LocalBuildNumber)"
+
+        if ($setupLogReviewer.LocalBuildNumber -eq $setupLogReviewer.SetupBuildNumber) {
+            Write-Host "Same build number detected..... if using powershell.exe to start setup. Make sure you do '.\setup.exe'" -ForegroundColor "Red"
+        }
+    }
+
+    $successFullInstall = $setupLogReviewer | SelectStringLastRunOfExchangeSetup -Pattern "The Exchange Server setup operation completed successfully\."
+
+    if ($null -ne $successFullInstall) {
+        Write-Host "The most recent setup attempt completed successfully based off this line:"
+        Write-Host $successFullInstall.Line
+        Write-Host "`r`nNo Action is required."
+        return
+    }
+
+    if ($DelegatedSetup) {
+        $setupLogReviewer | Test-DelegatedInstallerHasProperRights
+        return
+    }
+
+    InvokeTests -SetupLogReviewer $setupLogReviewer -Tests @(
+        "Test-PrerequisiteCheck",
+        "Test-KnownLdifErrors",
+        "Test-KnownOrganizationPreparationErrors",
+        "Test-KnownMsiIssuesCheck",
+        "Test-KnownIssuesByErrors",
+        "Write-LastErrorInformation"
+    )
+}
