8782c7146b722e91f6626ae16d759c3591acffe0
David Paulson <dpaul@microsoft.com>
2021-09-14 08:41:50 -0500

    Added ErrorContext

--- Setup/SetupLogReviewer/Checks/Test-KnownLdifErrors.ps1
+++ Setup/SetupLogReviewer/Checks/Test-KnownLdifErrors.ps1
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 . $PSScriptRoot\New-ActionPlan.ps1
+. $PSScriptRoot\New-ErrorContext.ps1
 Function Test-KnownLdifErrors {
     [CmdletBinding()]
     param(
@@ -15,6 +16,7 @@ Function Test-KnownLdifErrors {
             SelectStringLastRunOfExchangeSetup "\[ERROR\] There was an error while running 'ldifde.exe' to import the schema file '(.*)'. The error code is: (\d+). More details can be found in the error file: '(.*)'"
 
         if ($null -ne $schemaImportProcessFailure) {
+            $schemaImportProcessFailure.Line | New-ErrorContext
             New-ActionPlan -ActionList @(
                 "Failed to import schema setting from file '$($schemaImportProcessFailure.Matches.Groups[1].Value)'",
                 "Review ldif.err file '$($schemaImportProcessFailure.Matches.Groups[3].Value)' to help determine which object in the file '$($schemaImportProcessFailure.Matches.Groups[1].Value)' was trying to be imported that was causing problems.",
