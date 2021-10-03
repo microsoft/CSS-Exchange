9e8c744825c53253e5dafbbe9cd41fd6e46f6e11
David Paulson <dpaul@microsoft.com>
2021-10-03 13:47:15 -0500

    Fixed issue with null results from getting page file

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-PageFileInformation.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-PageFileInformation.ps1
@@ -6,13 +6,18 @@ Function Get-PageFileInformation {
 
     Write-Verbose "Calling: $($MyInvocation.MyCommand)"
     $pageFiles = @(Get-WmiObjectHandler -ComputerName $Script:Server -Class "Win32_PageFileSetting" -CatchActionFunction ${Function:Invoke-CatchActions})
-    $pageFileList = $null
+    $pageFileList = New-Object 'System.Collections.Generic.List[object]'
 
-    if ($null -eq $pageFiles) {
+    if ($null -eq $pageFiles -or
+        $pageFiles.Count -eq 0) {
         Write-Verbose "Found No Page File Settings"
+        $pageFileList.Add([PSCustomObject]@{
+                Name        = [string]::Empty
+                InitialSize = 0
+                MaximumSize = 0
+            })
     } else {
         Write-Verbose "Found $($pageFiles.Count) different page files"
-        $pageFileList = New-Object 'System.Collections.Generic.List[object]'
     }
 
     foreach ($pageFile in $pageFiles) {
