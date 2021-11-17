a2875a560ff388b03322ce5388288866800c8a67
Lukas Sassl <lusassl@microsoft.com>
2021-11-17 18:36:53 +0100

    First integration of an AMSI check

---
+++ Diagnostics/HealthChecker/Analyzer/Security/Invoke-AnalyzerSecurityAMSIConfigState.ps1
@@ -0,0 +1,71 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Function Invoke-AnalyzerSecurityAMSIConfigState {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ref]$AnalyzeResults,
+
+        [Parameter(Mandatory = $true)]
+        [object]$HealthServerObject,
+
+        [Parameter(Mandatory = $true)]
+        [object]$DisplayGroupingKey
+    )
+
+    Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+    $amsiInformation = $HealthServerObject.ExchangeInformation.AMSIConfiguration
+    $exchangeInformation = $HealthServerObject.ExchangeInformation
+    $exchangeCU = $exchangeInformation.BuildInformation.CU
+    $osInformation = $HealthServerObject.OSInformation
+
+    # AMSI integration is only available on Windows Server 2016 or higher and only on
+    # Exchange Server 2016 CU21+ or Exchange Server 2019 CU10+.
+    # AMSI is also not available on Edge Transport Servers.
+    if ((($osInformation.BuildInformation.MajorVersion -ge [HealthChecker.OSServerVersion]::Windows2016) -and
+        (($exchangeInformation.BuildInformation.MajorVersion -eq [HealthChecker.ExchangeMajorVersion]::Exchange2016) -and
+            ($exchangeCU -ge [HealthChecker.ExchangeCULevel]::CU21)) -or
+        (($exchangeInformation.BuildInformation.MajorVersion -eq [HealthChecker.ExchangeMajorVersion]::Exchange2019) -and
+            ($exchangeCU -ge [HealthChecker.ExchangeCULevel]::CU10))) -and
+        ($exchangeInformation.BuildInformation.ServerRole -ne [HealthChecker.ExchangeServerRole]::Edge)) {
+
+        if ($amsiInformation.QuerySuccessful -eq $true) {
+            if ($amsiInformation.Enabled -eq $true) {
+                $amsiWriteType = "Green"
+            } elseif ($amsiInformation.Enabled -eq $false) {
+                $amsiWriteType = "Yellow"
+                Switch ($amsiInformation.OrgWideSetting) {
+                    ($true) { $additionalAMSIDisplayValue = "Setting applies to all servers of the organization" }
+                    ($false) {
+                        $additionalAMSIDisplayValue = "Setting applies to the following server(s) of the organization:"
+                        foreach ($server in $amsiInformation.Server) {
+                            $additionalAMSIDisplayValue += "`r`n`t`t{0}" -f $server
+                        }
+                    }
+                }
+                $additionalAMSIDisplayValue += "`r`n`t`tThis may pose a security risk to your servers"
+                $additionalAMSIDisplayValue += "`r`n`t`tMore Information: https://aka.ms/AMSIExchange"
+            } else {
+                $amsiWriteType = "Yellow"
+                $additionalAMSIDisplayValue = "Exchange AMSI integration state is unknown"
+            }
+        } else {
+            $amsiWriteType = "Yellow"
+            $additionalAMSIDisplayValue = "Unable to query Exchange AMSI integration state"
+        }
+
+        $AnalyzeResults | Add-AnalyzedResultInformation -Name "AMSI Enabled" -Details $amsiInformation.Enabled `
+            -DisplayGroupingKey $DisplayGroupingKey `
+            -DisplayWriteType $amsiWriteType
+
+        if ($null -ne $additionalAMSIDisplayValue) {
+            $AnalyzeResults | Add-AnalyzedResultInformation -Details $additionalAMSIDisplayValue `
+                -DisplayGroupingKey $DisplayGroupingKey `
+                -DisplayWriteType $amsiWriteType `
+                -DisplayCustomTabNumber 2
+        }
+    } else {
+        Write-Verbose "AMSI integration is not available because we are on: $($exchangeInformation.BuildInformation.MajorVersion) $exchangeCU"
+    }
+}
---
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Get-ExchangeAMSIConfigurationState.ps1
@@ -0,0 +1,57 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\..\..\Helpers\Invoke-CatchActions.ps1
+Function Get-ExchangeAMSIConfigurationState {
+
+    begin {
+        Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+    } process {
+        try {
+            Write-Verbose "Trying to query AMSI configuration state"
+            $amsiConfiguration = Get-SettingOverride | Where-Object { ($_.ComponentName -eq "Cafe") -and ($_.SectionName -eq "HttpRequestFiltering") } -ErrorAction Stop
+
+            if (($null -ne $amsiConfiguration) -and
+                ($amsiConfiguration.Count -eq 1)) {
+                Write-Verbose "Setting override detected for AMSI configuration"
+                $amsiConfigurationQuerySuccessful = $true
+                Switch ($amsiConfiguration.Parameters.Split("=")[1]) {
+                    ("False") { $amsiState = $false }
+                    ("True") { $amsiState = $true }
+                    Default { $amsiState = "Unknown" }
+                }
+
+                if ($null -eq $amsiConfiguration.Server) {
+                    $amsiOrgWideSetting = $true
+                } else {
+                    $amsiOrgWideSetting = $false
+                }
+            } elseif (($null -ne $amsiConfiguration) -and
+                ($amsiConfiguration.Count -gt 1)) {
+                Write-Verbose "Multiple overrides for the same component and section detected"
+                $amsiState = "Multiple overrides detected"
+                $amsiConfigurationQuerySuccessful = $true
+            } else {
+                Write-Verbose "No setting override found that overrides AMSI configuration"
+                $amsiState = $true
+                $amsiConfigurationQuerySuccessful = $true
+            }
+        } catch {
+            Write-Verbose "Unable to query AMSI configuration state"
+            $amsiState = "Unknown"
+            $amsiConfigurationQuerySuccessful = $false
+            Invoke-CatchActions
+        }
+    } end {
+        return [PSCustomObject]@{
+            Id              = $amsiConfiguration.Id
+            Name            = $amsiConfiguration.Name
+            Reason          = $amsiConfiguration.Reason
+            Server          = $amsiConfiguration.Server
+            ModifiedBy      = $amsiConfiguration.ModifiedBy
+            Enabled         = $amsiState
+            OrgWideSetting  = $amsiOrgWideSetting
+            QuerySuccessful = $amsiConfigurationQuerySuccessful
+        }
+    }
+}
