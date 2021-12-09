619b029b35007baf2740626a447f20f8ad7a8d27
Lukas Sassl <lusassl@microsoft.com>
2021-12-09 15:08:44 +0100

    Encoding issue fixed

--- Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Tests/Get-ExchangeEmergencyMitigationServiceState.Tests.ps1
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Tests/Get-ExchangeEmergencyMitigationServiceState.Tests.ps1
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation.
+﻿# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Pester testing file')]
