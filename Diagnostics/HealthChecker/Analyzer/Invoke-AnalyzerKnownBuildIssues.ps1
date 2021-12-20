4d72f5a555a03948d275bc487c8e2857d5c68bbc
David Paulson <dpaul@microsoft.com>
2021-12-20 16:05:00 -0600

    Fixed logic with known issues

--- Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerKnownBuildIssues.ps1
+++ Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerKnownBuildIssues.ps1
@@ -43,10 +43,58 @@ Function Invoke-AnalyzerKnownBuildIssues {
         }
     }
 
+    Function GetKnownIssueBuildInformation {
+        param(
+            [string]$BuildNumber,
+            [string]$FixBuildNumber,
+            [bool]$BuildBound = $true
+        )
+
+        return [PSCustomObject]@{
+            BuildNumber    = $BuildNumber
+            FixBuildNumber = $FixBuildNumber
+            BuildBound     = $BuildBound
+        }
+    }
+
+    Function TestOnKnownBuildIssue {
+        param(
+            [object]$IssueBuildInformation,
+            [version]$CurrentBuild
+        )
+        $knownIssue = GetVersionFromString $issue.BuildNumber
+        Write-Verbose "Testing Known Issue Build $knownIssue"
+
+        if ($null -eq $knownIssue -or
+            $CurrentBuild.Minor -ne $knownIssue.Minor) { return $false }
+
+        $fixValueNull = [string]::IsNullOrEmpty($IssueBuildInformation.FixBuildNumber)
+        if ($fixValueNull) {
+            $resolvedBuild = GetVersionFromString "0.0.0.0"
+        } else {
+            $resolvedBuild = GetVersionFromString $IssueBuildInformation.FixBuildNumber
+        }
+
+        Write-Verbose "Testing against possible resolved build number $resolvedBuild"
+        $buildBound = $IssueBuildInformation.BuildBound
+        $withinBuildBoundRange = $CurrentBuild.Build -eq $knownIssue.Build
+        $fixNeeded = $fixValueNull -or $CurrentBuild -le $resolvedBuild
+        Write-Verbose "BuildBound: $buildBound | WithinBuildBoundRage: $withinBuildBoundRange | FixNeeded: $fixNeeded"
+        if ($CurrentBuild -ge $knownIssue) {
+            if ($buildBound) {
+                return $withinBuildBoundRange -and $fixNeeded
+            } else {
+                return $fixNeeded
+            }
+        }
+
+        return $false
+    }
+
     Function TestForKnownBuildIssues {
         param(
             [version]$CurrentVersion,
-            [hashtable]$KnownBuildIssuesToFixes,
+            [object[]]$KnownBuildIssuesToFixes,
             [object]$InformationUrl
         )
         Write-Verbose "Calling: $($MyInvocation.MyCommand)"
@@ -56,43 +104,33 @@ Function Invoke-AnalyzerKnownBuildIssues {
             $Script:CachedKnownIssues = @()
         }
 
-        foreach ($key in $KnownBuildIssuesToFixes.Keys) {
-            $knownIssue = GetVersionFromString $key
-            Write-Verbose "Testing Known Issue Build $knownIssue"
+        foreach ($issue in $KnownBuildIssuesToFixes) {
 
-            if ($null -ne $knownIssue -and
-                $CurrentVersion.Minor -eq $knownIssue.Minor) {
+            if ((TestOnKnownBuildIssue $issue $CurrentVersion) -and
+                    (-not($Script:CachedKnownIssues.Contains($InformationUrl)))) {
+                Write-Verbose "Known issue Match detected"
+                if (-not ($Script:DisplayKnownIssueHeader)) {
+                    $Script:DisplayKnownIssueHeader = $true
 
-                $resolvedBuild = GetVersionFromString $KnownBuildIssuesToFixes[$key]
-                Write-Verbose "Testing against possible resolved build number $resolvedBuild"
-                if (($null -eq $KnownBuildIssuesToFixes[$key] -and
-                    (-not($Script:CachedKnownIssues.Contains($InformationUrl)))) -or
-                    ($CurrentVersion -ge $knownIssue -and
-                    $CurrentVersion -lt $resolvedBuild)) {
-                    Write-Verbose "Known issue Match detected"
-                    if (-not ($Script:DisplayKnownIssueHeader)) {
-                        $Script:DisplayKnownIssueHeader = $true
+                    $AnalyzeResults | Add-AnalyzedResultInformation -Name "Known Issue Detected" `
+                        -Details "True" `
+                        -DisplayGroupingKey $DisplayGroupingKey `
+                        -DisplayWriteType "Yellow"
 
-                        $AnalyzeResults | Add-AnalyzedResultInformation -Name "Known Issue Detected" `
-                            -Details "True" `
-                            -DisplayGroupingKey $DisplayGroupingKey `
-                            -DisplayWriteType "Yellow"
-
-                        $AnalyzeResults | Add-AnalyzedResultInformation -Details "This build has a known issue(s) which may or may not have been addressed. See the below link(s) for more information.`r`n" `
-                            -DisplayGroupingKey $DisplayGroupingKey `
-                            -DisplayCustomTabNumber 2 `
-                            -DisplayWriteType "Yellow"
-                    }
-
-                    $AnalyzeResults | Add-AnalyzedResultInformation -Details "$($InformationUrl.Name):`r`n`t`t`t$($InformationUrl.Url)" `
+                    $AnalyzeResults | Add-AnalyzedResultInformation -Details "This build has a known issue(s) which may or may not have been addressed. See the below link(s) for more information.`r`n" `
                         -DisplayGroupingKey $DisplayGroupingKey `
                         -DisplayCustomTabNumber 2 `
                         -DisplayWriteType "Yellow"
+                }
 
-                    if (-not ($Script:CachedKnownIssues.Contains($InformationUrl))) {
-                        $Script:CachedKnownIssues += $InformationUrl
-                        Write-Verbose "Added known issue to cache"
-                    }
+                $AnalyzeResults | Add-AnalyzedResultInformation -Details "$($InformationUrl.Name):`r`n`t`t`t$($InformationUrl.Url)" `
+                    -DisplayGroupingKey $DisplayGroupingKey `
+                    -DisplayCustomTabNumber 2 `
+                    -DisplayWriteType "Yellow"
+
+                if (-not ($Script:CachedKnownIssues.Contains($InformationUrl))) {
+                    $Script:CachedKnownIssues += $InformationUrl
+                    Write-Verbose "Added known issue to cache"
                 }
             }
         }
@@ -108,13 +146,13 @@ Function Invoke-AnalyzerKnownBuildIssues {
     try {
         Write-Verbose "Working on November 2021 Security Updates - OWA redirection"
         TestForKnownBuildIssues -CurrentVersion $currentVersion `
-            -KnownBuildIssuesToFixes @{
-            "15.2.986.14"  = $null
-            "15.2.922.19"  = $null
-            "15.1.2375.17" = $null
-            "15.1.2308.20" = $null
-            "15.0.1497.26" = $null
-        } `
+            -KnownBuildIssuesToFixes @(
+            (GetKnownIssueBuildInformation "15.2.986.14" $null),
+            (GetKnownIssueBuildInformation "15.2.922.19" $null),
+            (GetKnownIssueBuildInformation "15.1.2375.17" $null),
+            (GetKnownIssueBuildInformation "15.1.2308.20" $null),
+            (GetKnownIssueBuildInformation "15.0.1497.26" $null)
+        ) `
             -InformationUrl (GetKnownIssueInformation `
                 "OWA redirection doesn't work after installing November 2021 security updates for Exchange Server 2019, 2016, or 2013" `
                 "https://support.microsoft.com/help/5008997")
