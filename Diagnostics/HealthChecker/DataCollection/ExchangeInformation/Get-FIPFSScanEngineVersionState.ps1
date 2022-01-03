30a50719c09066189a1371df3ca45bd3d3ca56bb
Lukas Sassl <lusassl@microsoft.com>
2022-01-03 17:15:28 +0100

    New check: Get-FIPFSScanEngineVersionState

---
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Get-FIPFSScanEngineVersionState.ps1
@@ -0,0 +1,147 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\..\..\..\..\Shared\Invoke-ScriptBlockHandler.ps1
+
+Function Get-FIPFSScanEngineVersionState {
+    [CmdletBinding()]
+    [OutputType("System.Bool")]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ComputerName,
+        [Parameter(Mandatory = $false)]
+        [scriptblock]
+        $CatchActionFunction
+    )
+
+    begin {
+        Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+        function GetItemFromExchangeInstallPath {
+            param(
+                [string]$ExchangeSubDir
+            )
+
+            Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+
+            $finalPathToQuery = Join-Path $env:ExchangeInstallPath $ExchangeSubDir
+            $getItem = Get-Item -Path $finalPathToQuery
+
+            $returnObject = ([PSCustomObject]@{
+                    GetItem          = $getItem
+                    LastWriteTimeUtc = $getItem.LastWriteTimeUtc
+                    VersionInfo      = ([PSCustomObject]@{
+                            ProductVersion  = $getItem.VersionInfo.ProductVersion.ToString()
+                            FileMajorPart   = $getItem.VersionInfo.FileMajorPart
+                            FileMinorPart   = $getItem.VersionInfo.FileMinorPart
+                            FileBuildPart   = $getItem.VersionInfo.FileBuildPart
+                            FilePrivatePart = $getItem.VersionInfo.FilePrivatePart
+                        })
+                })
+
+            return $returnObject
+        }
+        function TestPipeline2Version {
+            param (
+                [string]$ComputerName,
+                [scriptblock]$CatchActionFunction
+            )
+
+            Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+
+            $pipeline2FileInfo = Invoke-ScriptBlockHandler -ComputerName $ComputerName `
+                -ScriptBlock ${Function:GetItemFromExchangeInstallPath} `
+                -ArgumentList ("FIP-FS\Bin\pipeline2.dll") `
+                -CatchActionFunction $CatchActionFunction
+
+            if ($null -ne $pipeline2FileInfo) {
+                Write-Verbose "Testing pipeline2.dll version: $($pipeline2FileInfo.VersionInfo.ProductVersion)"
+
+                $pipeline2ReturnObject = ([PSCustomObject]@{
+                        isE15    = $false
+                        Affected = $false
+                    })
+
+                try {
+                    [int]$fileMajor = $pipeline2FileInfo.VersionInfo.FileMajorPart
+                    [int]$fileMinor = $pipeline2FileInfo.VersionInfo.FileMinorPart
+                    [int]$fileBuild = $pipeline2FileInfo.VersionInfo.FileBuildPart
+                    [int]$filePrivate = $pipeline2FileInfo.VersionInfo.FilePrivatePart
+
+                    if ($fileMajor -eq 15) {
+
+                        switch ($fileMinor) {
+                            0 { $pipeline2ReturnObject.isE15 = $true }
+                            1 { $pipeline2ReturnObject.Affected = (($fileBuild -le 2375) -and ($filePrivate -le 17)) }
+                            2 { $pipeline2ReturnObject.Affected = (($fileBuild -le 986) -and ($filePrivate -le 14)) }
+                            Default { Write-Verbose "Unexpected minor passed to the switch statement" }
+                        }
+                    } else {
+                        Write-Verbose "Exchange server version is not affected by the FIP-FS update issue"
+                    }
+                } catch {
+                    Write-Verbose "Error occured while detecting pipeline2.dll version"
+                    & $CatchActionFunction
+                }
+            }
+            return $pipeline2ReturnObject
+        }
+
+        function GetHighestScanEngineVersionNumber {
+            param (
+                [string]$ComputerName,
+                [scriptblock]$CatchActionFunction
+            )
+
+            Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+
+            try {
+                $scanEngineVersions = Invoke-ScriptBlockHandler -ComputerName $ComputerName `
+                    -ScriptBlock { Get-ChildItem -Path (Join-Path $env:ExchangeInstallPath "FIP-FS\Data\Engines\amd64\Microsoft\Bin") -Attributes Directory } `
+                    -CatchActionFunction $CatchActionFunction
+
+                if ($null -ne $scanEngineVersions) {
+                    [Int64]$highestScanEngineVersion = ($scanEngineVersions.Name | Measure-Object -Maximum).Maximum
+                } else {
+                    Write-Verbose "No FIP-FS scan engine version(s) detected"
+                }
+            } catch {
+                Write-Verbose "Error occured while processing FIP-FS scan engine version(s)"
+                & $CatchActionFunction
+            }
+            return $highestScanEngineVersion
+        }
+    } process {
+        $isAffectedByFIPFSUpdateIssue = $false
+        try {
+            $pipeline2 = TestPipeline2Version -ComputerName $ComputerName `
+                -CatchActionFunction $CatchActionFunction
+
+            if ($pipeline2.Affected -or
+                $pipeline2.isE15) {
+                $highesScanEngineVersionNumber = GetHighestScanEngineVersionNumber -ComputerName $ComputerName `
+                    -CatchActionFunction $CatchActionFunction
+                if (($null -ne $highesScanEngineVersionNumber) -and
+                    ($highesScanEngineVersionNumber -ge 2201010000)) {
+                    if ($pipeline2.isE15) {
+                        Write-Verbose "This Exchange 2013 server has applied FIP-FS update pattern with an invalid version number"
+                        Write-Verbose "Scan engine: $highesScanEngineVersionNumber"
+                    } else {
+                        Write-Verbose "Scan engine: $highesScanEngineVersionNumber will cause transport queue issues"
+                    }
+                    $isAffectedByFIPFSUpdateIssue = $true
+                } else {
+                    Write-Verbose "Scan engine: $highesScanEngineVersionNumber is safe to use"
+                }
+            } else {
+                Write-Verbose "The current Exchange server version is not affected by the FIP-FS update issue"
+            }
+        } catch {
+            Write-Verbose "Failed to check for the FIP-FS update issue"
+            $isAffectedByFIPFSUpdateIssue = $null
+            & $CatchActionFunction
+        }
+    } end {
+        return $isAffectedByFIPFSUpdateIssue
+    }
+}
---
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Tests/Get-FIPFSScanEngineVersionState.Tests.ps1
@@ -0,0 +1,60 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'Pester testing file')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidOverwritingBuiltInCmdlets', '', Justification = 'Pester testing file')]
+[CmdletBinding()]
+param()
+BeforeAll {
+    $Script:parentPath = (Split-Path -Parent $PSScriptRoot)
+    $Script:Server = $env:COMPUTERNAME
+    . $Script:parentPath\Get-FIPFSScanEngineVersionState.ps1
+
+    Function Invoke-CatchActions {
+        param()
+    }
+
+    Mock Invoke-ScriptBlockHandler -MockWith { return Import-Clixml $Script:parentPath\Tests\GetItemPipeline2Dll.xml } `
+        -ParameterFilter { $ArgumentList -eq "FIP-FS\Bin\pipeline2.dll" }
+
+    Mock Invoke-ScriptBlockHandler -MockWith { return Import-Clixml $Script:parentPath\Tests\GetChildItemInvalidPattern.xml }
+}
+
+Describe "Testing Get-FIPFSScanEngineVersionState.ps1" {
+
+    Context "Invalid Pattern Detected On E16/E19" {
+        BeforeAll {
+            $Script:results = Get-FIPFSScanEngineVersionState -ComputerName $Script:Server `
+                -CatchActionFunction ${Function:Invoke-CatchActions}
+        }
+
+        It "System Affected By Transport Queue Issue" {
+            $results | Should -Be $true
+        }
+    }
+
+    Context "Valid Pattern Detected On E16/E19" {
+        BeforeAll {
+            Mock Invoke-ScriptBlockHandler -MockWith { return Import-Clixml $Script:parentPath\Tests\GetChildItemValidPattern.xml }
+            $Script:results = Get-FIPFSScanEngineVersionState -ComputerName $Script:Server `
+                -CatchActionFunction ${Function:Invoke-CatchActions}
+        }
+
+        It "System NOT Affected By Transport Queue Issue" {
+            $results | Should -Be $false
+        }
+    }
+
+    Context "Invalid Pattern Detected On E15" {
+        BeforeAll {
+            Mock Invoke-ScriptBlockHandler -MockWith { return Import-Clixml $Script:parentPath\Tests\E15GetItemPipeline2Dll.xml } `
+                -ParameterFilter { $ArgumentList -eq "FIP-FS\Bin\pipeline2.dll" }
+            $Script:results = Get-FIPFSScanEngineVersionState -ComputerName $Script:Server `
+                -CatchActionFunction ${Function:Invoke-CatchActions}
+        }
+
+        It "System Affected By Pattern Download Issue" {
+            $results | Should -Be $true
+        }
+    }
+}
