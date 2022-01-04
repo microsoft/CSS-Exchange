23bb9690f665a1a577801438b4e8355ccf69f9e3
David Paulson <dpaul@microsoft.com>
2022-01-04 16:02:41 -0600

    Address issue with not finding FIP-FS agents
    Addressed issue to find the FIP-FS agent path or any agents in the directory.
    
    According to Bill, if we don't have any agents, some transport rules will cause a similar issue. Need to have agents on the server or things can occur.

--- Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Get-FIPFSScanEngineVersionState.ps1
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Get-FIPFSScanEngineVersionState.ps1
@@ -26,20 +26,23 @@ Function Get-FIPFSScanEngineVersionState {
             try {
                 $exSetupPath = (Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ExchangeServer\v15\Setup -ErrorAction Stop).MsiInstallPath
             } catch {
-                Invoke-CatchActions
+                # since this is a script block, can't call Invoke-CatchActions
                 $exSetupPath = $env:ExchangeInstallPath
             }
 
             $finalPath = Join-Path $exSetupPath $ExchangeSubDir
 
             if ($ExchangeSubDir -notmatch '\.[a-zA-Z0-9]+$') {
-                $getDir = Get-ChildItem -Path $finalPath -Attributes Directory
-                if ($null -ne $getDir) {
-                    return ([PSCustomObject]@{
-                            Name             = $getDir.Name
-                            LastWriteTimeUtc = $getDir.LastWriteTimeUtc
-                        })
+
+                if (Test-Path $finalPath) {
+                    $getDir = Get-ChildItem -Path $finalPath -Attributes Directory
                 }
+
+                return ([PSCustomObject]@{
+                        Name             = $getDir.Name
+                        LastWriteTimeUtc = $getDir.LastWriteTimeUtc
+                        Failed           = $null -eq $getDir
+                    })
             }
             return $null
         }
@@ -58,7 +61,12 @@ Function Get-FIPFSScanEngineVersionState {
                     -CatchActionFunction ${Function:Invoke-CatchActions}
 
                 if ($null -ne $scanEngineVersions) {
-                    [Int64]$highestScanEngineVersion = ($scanEngineVersions.Name | Measure-Object -Maximum).Maximum
+                    if ($scanEngineVersions.Failed) {
+                        Write-Verbose "Failed to find the scan engine directory"
+                        return $null
+                    } else {
+                        [Int64]$highestScanEngineVersion = ($scanEngineVersions.Name | Measure-Object -Maximum).Maximum
+                    }
                 } else {
                     Write-Verbose "No FIP-FS scan engine version(s) detected"
                 }
@@ -75,7 +83,8 @@ Function Get-FIPFSScanEngineVersionState {
             $highestScanEngineVersionNumber = GetHighestScanEngineVersionNumber -ComputerName $ComputerName
 
             if ($null -eq $highestScanEngineVersionNumber) {
-                Write-Verbose "No scan engine version found on the computer - no further testings required"
+                Write-Verbose "No scan engine version found on the computer - this can cause issues still with some transport rules"
+                $isAffectedByFIPFSUpdateIssue = $null
             } elseif ($highestScanEngineVersionNumber -ge 2201010000) {
                 Write-Verbose "Scan engine: $highestScanEngineVersionNumber will cause transport queue or pattern update issues"
                 $isAffectedByFIPFSUpdateIssue = $true
