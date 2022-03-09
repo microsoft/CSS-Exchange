0458e9436e9c73c0614a6933e717533777d49a44
David Paulson <dpaul@microsoft.com>
2022-03-08 22:30:05 -0600

    New Script Debug Methods

---
+++ Diagnostics/ExchangeLogCollector/RemoteScriptBlock/IO/Write-Verbose.ps1
@@ -0,0 +1,34 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+Function Write-Verbose {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidOverwritingBuiltInCmdlets', '', Justification = 'In order to log Write-Verbose from Shared functions')]
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string]$Message
+    )
+
+    process {
+
+        Microsoft.PowerShell.Utility\Write-Verbose $Message
+
+        if ($null -ne $Script:WriteVerboseDebugAction) {
+            & $Script:WriteVerboseDebugAction $Message
+        }
+
+        # $PSSenderInfo is set when in a remote context
+        if ($PSSenderInfo -and
+            $null -ne $Script:WriteRemoteVerboseDebugAction) {
+            & $Script:WriteRemoteVerboseDebugAction $Message
+        }
+    }
+}
+
+Function SetWriteVerboseAction ($DebugAction) {
+    $Script:WriteVerboseDebugAction = $DebugAction
+}
+
+Function SetWriteRemoteVerboseAction ($DebugAction) {
+    $Script:WriteRemoteVerboseDebugAction = $DebugAction
+}
---
+++ Diagnostics/ExchangeLogCollector/RemoteScriptBlock/IO/WriteFunctions.ps1
@@ -0,0 +1,38 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Small set of functions that are used to help override the Write-Host and Write-Verbose functions
+Function Get-ManipulateWriteHostValue {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string]$Message
+    )
+
+    process {
+        return "[$env:COMPUTERNAME] : $Message"
+    }
+}
+
+Function Get-ManipulateWriteVerboseValue {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string]$Message
+    )
+
+    process {
+        return "[$env:COMPUTERNAME - Script Debug] : $Message"
+    }
+}
+
+#Calls the $Script:Logger object to write the data to file only.
+Function Write-DebugLog($message) {
+    if ($null -ne $message -and
+        ![string]::IsNullOrEmpty($message) -and
+        $null -ne $Script:Logger) {
+        $Script:Logger = $Script:Logger | Write-LoggerInstance $message
+    }
+}
