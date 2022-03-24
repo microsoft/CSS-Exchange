ab24774d03fdc6621f70d0f5ebefad0f549c9840
David Paulson <dpaul@microsoft.com>
2022-03-24 12:53:32 -0500

    Improved Code Reading in Get-ArgumentList

--- Diagnostics/ExchangeLogCollector/Helpers/Get-ArgumentList.ps1
+++ Diagnostics/ExchangeLogCollector/Helpers/Get-ArgumentList.ps1
@@ -3,7 +3,6 @@
 
 . $PSScriptRoot\..\ExchangeServerInfo\Get-ServerObjects.ps1
 Function Get-ArgumentList {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '', Justification = 'TODO: Change this')]
     param(
         [Parameter(Mandatory = $true)][array]$Servers
     )
@@ -24,58 +23,57 @@ Function Get-ArgumentList {
         $Script:MasterServer = $Servers[0]
     }
 
-    $obj = New-Object PSCustomObject
-    $obj | Add-Member -Name FilePath -MemberType NoteProperty -Value $FilePath
-    $obj | Add-Member -Name RootFilePath -MemberType NoteProperty -Value $Script:RootFilePath
-    $obj | Add-Member -Name ServerObjects -MemberType NoteProperty -Value (Get-ServerObjects -ValidServers $Servers)
-    $obj | Add-Member -Name ManagedAvailabilityLogs -MemberType NoteProperty -Value $ManagedAvailabilityLogs
-    $obj | Add-Member -Name AppSysLogs -MemberType NoteProperty -Value $AppSysLogs
-    $obj | Add-Member -Name AppSysLogsToXml -MemberType NoteProperty -Value $AppSysLogsToXml
-    $obj | Add-Member -Name EWSLogs -MemberType NoteProperty -Value $EWSLogs
-    $obj | Add-Member -Name DailyPerformanceLogs -MemberType NoteProperty -Value $DailyPerformanceLogs
-    $obj | Add-Member -Name RPCLogs -MemberType NoteProperty -Value $RPCLogs
-    $obj | Add-Member -Name EASLogs -MemberType NoteProperty -Value $EASLogs
-    $obj | Add-Member -Name ECPLogs -MemberType NoteProperty -Value $ECPLogs
-    $obj | Add-Member -Name AutoDLogs -MemberType NoteProperty -Value $AutoDLogs
-    $obj | Add-Member -Name OWALogs -MemberType NoteProperty -Value $OWALogs
-    $obj | Add-Member -Name ADDriverLogs -MemberType NoteProperty -Value $ADDriverLogs
-    $obj | Add-Member -Name SearchLogs -MemberType NoteProperty -Value $SearchLogs
-    $obj | Add-Member -Name HighAvailabilityLogs -MemberType NoteProperty -Value $HighAvailabilityLogs
-    $obj | Add-Member -Name MapiLogs -MemberType NoteProperty -Value $MapiLogs
-    $obj | Add-Member -Name MessageTrackingLogs -MemberType NoteProperty -Value $MessageTrackingLogs
-    $obj | Add-Member -Name HubProtocolLogs -MemberType NoteProperty -Value $HubProtocolLogs
-    $obj | Add-Member -Name HubConnectivityLogs -MemberType NoteProperty -Value $HubConnectivityLogs
-    $obj | Add-Member -Name FrontEndConnectivityLogs -MemberType NoteProperty -Value $FrontEndConnectivityLogs
-    $obj | Add-Member -Name FrontEndProtocolLogs -MemberType NoteProperty -Value $FrontEndProtocolLogs
-    $obj | Add-Member -Name MailboxConnectivityLogs -MemberType NoteProperty -Value $MailboxConnectivityLogs
-    $obj | Add-Member -Name MailboxProtocolLogs -MemberType NoteProperty -Value $MailboxProtocolLogs
-    $obj | Add-Member -Name MailboxDeliveryThrottlingLogs -MemberType NoteProperty -Value $MailboxDeliveryThrottlingLogs
-    $obj | Add-Member -Name QueueInformation -MemberType NoteProperty -Value $QueueInformation
-    $obj | Add-Member -Name SendConnectors -MemberType NoteProperty -Value $SendConnectors
-    $obj | Add-Member -Name DAGInformation -MemberType NoteProperty -Value $DAGInformation
-    $obj | Add-Member -Name GetVdirs -MemberType NoteProperty -Value $GetVdirs
-    $obj | Add-Member -Name TransportConfig -MemberType NoteProperty -Value $TransportConfig
-    $obj | Add-Member -Name DefaultTransportLogging -MemberType NoteProperty -Value $DefaultTransportLogging
-    $obj | Add-Member -Name ServerInformation -MemberType NoteProperty -Value $ServerInformation
-    $obj | Add-Member -Name CollectAllLogsBasedOnLogAge -MemberType NoteProperty -Value $CollectAllLogsBasedOnLogAge
-    $obj | Add-Member -Name TimeSpan -MemberType NoteProperty -Value $LogAge
-    $obj | Add-Member -Name IISLogs -MemberType NoteProperty -Value $IISLogs
-    $obj | Add-Member -Name AnyTransportSwitchesEnabled -MemberType NoteProperty -Value $script:AnyTransportSwitchesEnabled
-    $obj | Add-Member -Name HostExeServerName -MemberType NoteProperty -Value ($env:COMPUTERNAME)
-    $obj | Add-Member -Name Experfwiz -MemberType NoteProperty -Value $Experfwiz
-    $obj | Add-Member -Name ExperfwizLogmanName -MemberType NoteProperty -Value $ExperfwizLogmanName
-    $obj | Add-Member -Name Exmon -MemberType NoteProperty -Value $Exmon
-    $obj | Add-Member -Name ExmonLogmanName -MemberType NoteProperty -Value $ExmonLogmanName
-    $obj | Add-Member -Name ScriptDebug -MemberType NoteProperty -Value $ScriptDebug
-    $obj | Add-Member -Name ExchangeServerInformation -MemberType NoteProperty -Value $ExchangeServerInformation
-    $obj | Add-Member -Name StandardFreeSpaceInGBCheckSize -MemberType NoteProperty $Script:StandardFreeSpaceInGBCheckSize
-    $obj | Add-Member -Name PopLogs -MemberType NoteProperty -Value $PopLogs
-    $obj | Add-Member -Name ImapLogs -MemberType NoteProperty -Value $ImapLogs
-    $obj | Add-Member -Name OABLogs -MemberType NoteProperty -Value $OABLogs
-    $obj | Add-Member -Name PowerShellLogs -MemberType NoteProperty -Value $PowerShellLogs
-    $obj | Add-Member -Name WindowsSecurityLogs -MemberType NoteProperty $WindowsSecurityLogs
-    $obj | Add-Member -Name MasterServer -MemberType NoteProperty -Value $Script:MasterServer
-    $obj | Add-Member -Name MitigationService -MemberType NoteProperty -Value $MitigationService
-
-    return $obj
+    return [PSCustomObject]@{
+        ADDriverLogs                   = $ADDriverLogs
+        AnyTransportSwitchesEnabled    = $Script:AnyTransportSwitchesEnabled
+        AppSysLogs                     = $AppSysLogs
+        AppSysLogsToXml                = $AppSysLogsToXml
+        AutoDLogs                      = $AutoDLogs
+        CollectAllLogsBasedOnLogAge    = $CollectAllLogsBasedOnLogAge
+        DAGInformation                 = $DAGInformation
+        DailyPerformanceLogs           = $DailyPerformanceLogs
+        DefaultTransportLogging        = $DefaultTransportLogging
+        EASLogs                        = $EASLogs
+        ECPLogs                        = $ECPLogs
+        EWSLogs                        = $EWSLogs
+        ExchangeServerInformation      = $ExchangeServerInformation
+        Exmon                          = $Exmon
+        ExmonLogmanName                = $ExmonLogmanName
+        Experfwiz                      = $Experfwiz
+        ExperfwizLogmanName            = $ExperfwizLogmanName
+        FilePath                       = $FilePath
+        FrontEndConnectivityLogs       = $FrontEndConnectivityLogs
+        FrontEndProtocolLogs           = $FrontEndProtocolLogs
+        GetVdirs                       = $GetVdirs
+        HighAvailabilityLogs           = $HighAvailabilityLogs
+        HostExeServerName              = $env:COMPUTERNAME
+        HubConnectivityLogs            = $HubConnectivityLogs
+        HubProtocolLogs                = $HubProtocolLogs
+        IISLogs                        = $IISLogs
+        ImapLogs                       = $ImapLogs
+        TimeSpan                       = $LogAge
+        MailboxConnectivityLogs        = $MailboxConnectivityLogs
+        MailboxDeliveryThrottlingLogs  = $MailboxDeliveryThrottlingLogs
+        MailboxProtocolLogs            = $MailboxProtocolLogs
+        ManagedAvailabilityLogs        = $ManagedAvailabilityLogs
+        MapiLogs                       = $MapiLogs
+        MasterServer                   = $Script:MasterServer
+        MessageTrackingLogs            = $MessageTrackingLogs
+        MitigationService              = $MitigationService
+        OABLogs                        = $OABLogs
+        OWALogs                        = $OWALogs
+        PopLogs                        = $PopLogs
+        PowerShellLogs                 = $PowerShellLogs
+        QueueInformation               = $QueueInformation
+        RootFilePath                   = $Script:RootFilePath
+        RPCLogs                        = $RPCLogs
+        SearchLogs                     = $SearchLogs
+        SendConnectors                 = $SendConnectors
+        ServerInformation              = $ServerInformation
+        ServerObjects                  = (Get-ServerObjects -ValidServers $Servers)
+        ScriptDebug                    = $ScriptDebug
+        StandardFreeSpaceInGBCheckSize = $Script:StandardFreeSpaceInGBCheckSize
+        TransportConfig                = $TransportConfig
+        WindowsSecurityLogs            = $WindowsSecurityLogs
+    }
 }
