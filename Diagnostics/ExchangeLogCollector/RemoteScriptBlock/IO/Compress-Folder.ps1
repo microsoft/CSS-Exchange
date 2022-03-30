f16d1794952c4916a3df1e7d5db6d10f50933559
David Paulson <dpaul@microsoft.com>
2022-03-30 14:38:55 -0500

    Optimized Compress-Folder
    Removed some of the old code that didn't need to be there.

--- Diagnostics/ExchangeLogCollector/RemoteScriptBlock/IO/Compress-Folder.ps1
+++ Diagnostics/ExchangeLogCollector/RemoteScriptBlock/IO/Compress-Folder.ps1
@@ -11,94 +11,65 @@ Function Compress-Folder {
         [Parameter(Position = 4)][bool]$ReturnCompressedLocation = $false
     )
 
-    Function Get-DirectorySize {
-        param(
-            [Parameter(Mandatory = $true)][string]$Directory,
-            [Parameter(Mandatory = $false)][bool]$IsCompressed = $false
-        )
-        $itemSize = 0
-        if ($IsCompressed) {
-            $itemSize = (Get-Item $Directory).Length
-        } else {
-            $childItems = Get-ChildItem $Directory -Recurse | Where-Object { -not($_.Mode.StartsWith("d-")) }
-            foreach ($item in $childItems) {
-                $itemSize += $item.Length
-            }
-        }
-        return $itemSize
-    }
-
-    if ($Folder.EndsWith("\")) {
-        $Folder = $Folder.TrimEnd("\")
-    }
+    $Folder = $Folder.TrimEnd("\")
+    $compressedLocation = [string]::Empty
     Write-Verbose "Calling: $($MyInvocation.MyCommand)"
     Write-Verbose "Passed - [string]Folder: $Folder | [bool]IncludeDisplayZipping: $IncludeDisplayZipping | [bool]ReturnCompressedLocation: $ReturnCompressedLocation"
 
-    $compressedLocation = [string]::Empty
-    if (Test-Path $Folder) {
-
-        $assemblies = [Appdomain]::CurrentDomain.GetAssemblies()
-        $successful = $false
-        foreach ($assembly in $assemblies) {
-            if ($assembly.Location -like "*System.IO.Compression.Filesystem*") {
-                $successful = $true
-                break
-            }
-        }
-
-        Write-Verbose "Found IO Compression loaded: $successful"
-
-        if ($successful -eq $false) {
-            # Try to load the IO Compression
-            $loadedIOCompression = $false
-            try {
-                Add-Type -AssemblyName System.IO.Compression.Filesystem -ErrorAction Stop
-                $loadedIOCompression = $true
-            } catch {
-                Write-Host "Failed to load .NET Compression assembly. Unable to compress up the data."
-            }
-
-            if ($loadedIOCompression -eq $false) {
-                Write-Verbose "Unable to compress folder $Folder"
-                Write-Verbose "Unable to enable IO compression on this system"
-                return $null
-            }
-        }
-
-        if ($IncludeMonthDay) {
-            $zipFolderNoEXT = "{0}-{1}" -f $Folder, (Get-Date -Format Md)
-        } else {
-            $zipFolderNoEXT = $Folder
-        }
-        Write-Verbose "[string]zipFolderNoEXT: $zipFolderNoEXT"
-        $zipFolder = "{0}.zip" -f $zipFolderNoEXT
-        if (Test-Path $zipFolder) {
-            [int]$i = 1
-            do {
-                $zipFolder = "{0}-{1}.zip" -f $zipFolderNoEXT, $i
-                $i++
-            }while (Test-Path $zipFolder)
-        }
-        Write-Verbose "Using Zip Folder Path: $zipFolder"
-
-        if ($IncludeDisplayZipping) {
-            Write-Host "Compressing Folder $Folder"
-        }
-        $sizeBytesBefore = Get-DirectorySize -Directory $Folder
-        $timer = [System.Diagnostics.Stopwatch]::StartNew()
-        [System.IO.Compression.ZipFile]::CreateFromDirectory($Folder, $zipFolder)
-        $timer.Stop()
-        $sizeBytesAfter = Get-DirectorySize -Directory $zipFolder -IsCompressed $true
-        Write-Verbose ("Compressing directory size of {0} MB down to the size of {1} MB took {2} seconds." -f ($sizeBytesBefore / 1MB), ($sizeBytesAfter / 1MB), $timer.Elapsed.TotalSeconds)
-        if ((Test-Path -Path $zipFolder)) {
-            Write-Verbose "Compress successful, removing folder."
-            Remove-Item $Folder -Force -Recurse
-        }
-        if ($ReturnCompressedLocation) {
-            $compressedLocation = $zipFolder
-        }
-    } else {
+    if (-not (Test-Path $Folder)) {
         Write-Host "Failed to find the folder $Folder"
+        return $null
+    }
+
+    $successful = ([Appdomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.Location -like "*System.IO.Compression.Filesystem*" }).Count -ge 1
+    Write-Verbose "Found IO Compression loaded: $successful"
+
+    if ($successful -eq $false) {
+        # Try to load the IO Compression
+        try {
+            Add-Type -AssemblyName System.IO.Compression.Filesystem -ErrorAction Stop
+            Write-Verbose "Loaded .NET Compression Assembly."
+        } catch {
+            Write-Host "Failed to load .NET Compression assembly. Unable to compress up the data."
+            return $null
+        }
+    }
+
+    if ($IncludeMonthDay) {
+        $zipFolderNoEXT = "{0}-{1}" -f $Folder, (Get-Date -Format Md)
+    } else {
+        $zipFolderNoEXT = $Folder
+    }
+    Write-Verbose "[string]zipFolderNoEXT: $zipFolderNoEXT"
+    $zipFolder = "{0}.zip" -f $zipFolderNoEXT
+    [int]$i = 1
+    while (Test-Path $zipFolder) {
+        $zipFolder = "{0}-{1}.zip" -f $zipFolderNoEXT, $i
+        $i++
+    }
+    Write-Verbose "Using Zip Folder Path: $zipFolder"
+
+    if ($IncludeDisplayZipping) {
+        Write-Host "Compressing Folder $Folder"
+    }
+    $sizeBytesBefore = 0
+    Get-ChildItem $Folder -Recurse |
+        Where-Object { -not ($_.Mode.StartsWith("d-")) } |
+        ForEach-Object { $sizeBytesBefore += $_.Length }
+
+    $timer = [System.Diagnostics.Stopwatch]::StartNew()
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($Folder, $zipFolder)
+    $timer.Stop()
+    $sizeBytesAfter = (Get-Item $zipFolder).Length
+    Write-Verbose ("Compressing directory size of {0} MB down to the size of {1} MB took {2} seconds." -f ($sizeBytesBefore / 1MB), ($sizeBytesAfter / 1MB), $timer.Elapsed.TotalSeconds)
+
+    if ((Test-Path -Path $zipFolder)) {
+        Write-Verbose "Compress successful, removing folder."
+        Remove-Item $Folder -Force -Recurse
+    }
+
+    if ($ReturnCompressedLocation) {
+        $compressedLocation = $zipFolder
     }
 
     Write-Verbose "Returning: $compressedLocation"
