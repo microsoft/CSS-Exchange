5122f8025c7b406645068380d663c2288f5af8fd
David Paulson <dpaul@microsoft.com>
2022-05-16 11:25:46 -0500

    winmgmt service not working action item
    If winmgmt service is not working correctly or disabled, handle that within the script and display to screen with a action plan for customers to follow. Then call exit to allow the finally block to still execute.

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
@@ -34,6 +34,17 @@ Function Get-WmiObjectCriticalHandler {
     $wmi = Get-WmiObjectHandler @params
 
     if ($null -eq $wmi) {
+        # Check for common issues that have been seen. If common issue, the do a Write-Error, custom message, then exit to maintain readability.
+
+        # Grab the English version of hte message and/or the error code. Could get a different error code if service is not disabled.
+        if ($Error[0].Exception.Message -like "The service cannot be started, either because it is disabled or because it has no enabled devices associated with it. *" -or
+            $Error[0].Exception.ErrorCode -eq 0x80070422) {
+            Write-Verbose "winmgmt service is disabled or not working."
+            Write-Error $Error[0]
+            Write-Warning "The 'winmgmt' service appears to not be working correctly. Please make sure it is set to Automatic and in a running state. This script will fail unless this is working correctly."
+            exit
+        }
+
         throw "Failed to get critical information. Stopping the script. InnerException: $($Error[0])"
     }
 
