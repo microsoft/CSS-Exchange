b8c017747ba92aed96bcb68e799d825a86a42079
David Paulson <dpaul@microsoft.com>
2022-05-16 11:34:11 -0500

    Illegal operation attempted handled
    If an Illegal operation was detected from Get-WmiObject on critical sections, provide custom warning on the scenario and exit the script.

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
@@ -36,6 +36,15 @@ Function Get-WmiObjectCriticalHandler {
     if ($null -eq $wmi) {
         # Check for common issues that have been seen. If common issue, the do a Write-Error, custom message, then exit to maintain readability.
 
+        if ($Error[0].Exception.ErrorCode -eq 0x800703FA) {
+            Write-Verbose "Registry key marked for deletion."
+            Write-Error $Error[0]
+            $message = "A registry key is marked for deletion that was attempted to read from for the cmdlet 'Get-WmiObject -Class $Class'.`r`n"
+            $message += "`tThis error goes away after some time and/or a reboot of the computer. At that time you should be able to run Health Checker again."
+            Write-Warning $message
+            exit
+        }
+
         # Grab the English version of hte message and/or the error code. Could get a different error code if service is not disabled.
         if ($Error[0].Exception.Message -like "The service cannot be started, either because it is disabled or because it has no enabled devices associated with it. *" -or
             $Error[0].Exception.ErrorCode -eq 0x80070422) {
