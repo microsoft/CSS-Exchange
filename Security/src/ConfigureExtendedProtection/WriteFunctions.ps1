781629128b24883e37cc2ebe9ba3d96239a12246
David Paulson <dpaul@microsoft.com>
2022-06-23 14:53:39 -0500

    Added Logging ability to the script

---
+++ Security/src/ConfigureExtendedProtection/WriteFunctions.ps1
@@ -0,0 +1,10 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Write-VerboseLog ($Message) {
+    $Script:Logger = $Script:Logger | Write-LoggerInstance $Message
+}
+
+function Write-HostLog ($Message) {
+    $Script:Logger = $Script:Logger | Write-LoggerInstance $Message
+}
