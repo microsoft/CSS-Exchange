fa90244597fc34cf07af8fc21886df265fe8b9c6
stevecharon <37655537+stevecharon@users.noreply.github.com>
2022-12-19 17:49:56 +0100

    Update Get-HealthCheckFilesItemsFromLocation.ps1
    fix formatting

--- Diagnostics/HealthChecker/Helpers/Get-HealthCheckFilesItemsFromLocation.ps1
+++ Diagnostics/HealthChecker/Helpers/Get-HealthCheckFilesItemsFromLocation.ps1
@@ -4,7 +4,7 @@
 function Get-HealthCheckFilesItemsFromLocation {
     ##This notation will break things if you have other xml files from runs with different parameters like exchangedccoreratio
     ##Filenames to exclude: *ExchangeDCCoreRatio* , all other reports generate txt-reports only
-    $items = Get-ChildItem $XMLDirectoryPath | Where-Object { $_.Name -like "HealthChecker-*-*.xml" -and $_.Name -notlike "HealthChecker-ExchangeDCCoreRatio-*.xml"}
+    $items = Get-ChildItem $XMLDirectoryPath | Where-Object { $_.Name -like "HealthChecker-*-*.xml" -and $_.Name -notlike "HealthChecker-ExchangeDCCoreRatio-*.xml" }
 
     if ($null -eq $items) {
         Write-Host("Doesn't appear to be any Health Check XML files here....stopping the script")
