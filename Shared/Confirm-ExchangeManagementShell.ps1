2aba1aede503a79b3820a5e2a23778e165746c28
Bill Long <bilong@microsoft.com>
2022-12-27 11:53:36 -0600

    Add new Confirm-ExchangeManagementShell shared script

---
+++ Shared/Confirm-ExchangeManagementShell.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+    Returns True if we are running inside Exchange Management Shell, and False otherwise.
+#>
+function Confirm-ExchangeManagementShell {
+    $cmd = Get-Command "Get-EventLogLevel" -ErrorAction SilentlyContinue
+    if ($null -eq $cmd) {
+        return $false
+    }
+
+    $level = Get-EventLogLevel | Select-Object -First 1
+    if ($level.GetType().Name -eq "EventCategoryObject") {
+        return $true
+    }
+
+    return $false
+}
