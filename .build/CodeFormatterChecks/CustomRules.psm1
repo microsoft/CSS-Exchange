f088ce7c5813dbffb39ee5a6438833cc40bafa41
David Paulson <dpaul@microsoft.com>
2023-01-05 16:57:40 -0600

    Create custom Rule for Read-Host

---
+++ .build/CodeFormatterChecks/CustomRules.psm1
@@ -0,0 +1,36 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function AvoidUsingReadHost {
+
+    [CmdletBinding()]
+    [OutputType([PSCustomObject[]])]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.Language.ScriptBlockAst]$ScriptBlockAst
+    )
+
+    process {
+
+        try {
+            $functions = $ScriptBlockAst.FindAll(
+                {
+                    $args[0] -is [System.Management.Automation.Language.CommandAst]
+                }, $true )
+            foreach ( $function in $functions ) {
+
+                if (($function.GetCommandName()) -eq "Read-Host") {
+                    [PSCustomObject]@{
+                        Message  = "Avoid using Read-Host. Use parameters to get information from the user. Use ShouldProcess to get Y/N confirmation on whether to proceed."
+                        Extent   = $function.Extent
+                        RuleName = $PSCmdlet.MyInvocation.InvocationName
+                        Severity = "Warning"
+                    }
+                }
+            }
+        } catch {
+            $PSCmdlet.ThrowTerminatingError( $_ )
+        }
+    }
+}
