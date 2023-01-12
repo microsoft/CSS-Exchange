4a8930e30e92fe835c9e3bd632ebec7b755382a2
Bill Long <bilong@microsoft.com>
2023-01-12 16:43:54 -0600

    Spell check scripts and docs

---
+++ .build/SpellCheck.ps1
@@ -0,0 +1,36 @@
+﻿function DoSpellCheck {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPositionalParameters', '', Justification = 'This is the correct syntax for cspell')]
+    param()
+
+    $nodeVersion = node -v
+
+    if ([string]::IsNullOrEmpty($nodeVersion)) {
+        Write-Host "Node.js is not installed. Please install Node.js and try again."
+        exit 1
+    }
+
+    $majorVersion = [int]::Parse($nodeVersion.Split(".")[0].Trim("v"))
+
+    if ($majorVersion -lt 14) {
+        Write-Host "Node.js version 14 or higher is required. Please upgrade Node.js and try again."
+        exit 1
+    }
+
+    $cspellModule = npm -g ls cspell | Select-String "cspell@"
+
+    if ([string]::IsNullOrEmpty($cspellModule)) {
+        Write-Host "Installing cspell..."
+        npm install -g cspell
+    }
+
+    if ([string]::IsNullOrEmpty($cspellModule)) {
+        Write-Host "Could not install cspell. Please install cspell and try again."
+        exit 1
+    }
+
+    $repoRoot = [IO.Path]::GetFullPath((Join-Path $PSScriptRoot ".."))
+
+    cspell lint --config "$PSScriptRoot\cspell.json" --no-progress (Join-Path $repoRoot "**" "*.md") (Join-Path $repoRoot "**" "*.ps1")
+}
+
+DoSpellCheck
