84a076e25054ae33eb948707b8bfd182b1789de3
EnzoLopez2023 <enlo@microsoft.com>
2023-01-26 14:07:17 -0500

    removed internal copywrite

--- Calendar/Get-CalendarDiagnosticObjectsSummary.ps1
+++ Calendar/Get-CalendarDiagnosticObjectsSummary.ps1
@@ -19,9 +19,6 @@
 #
 # Get-CalendarDiagnosticObjectsSummary.ps1 -Identity user@contoso.com -Subject Test_OneTime_Meeting_Subject
 #
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
-#
 
 [CmdLetBinding()]
 param(
