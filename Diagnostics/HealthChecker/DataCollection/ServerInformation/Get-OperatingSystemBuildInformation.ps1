e5dd82d882923e6f5e1719ad7023713df2da7a1d
David Paulson <dpaul@microsoft.com>
2023-02-03 09:58:08 -0600

    Make Operating System Build Info use same property name as Exchange Build Info
    BuildVersion should be what is used and as a System.Version type

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-OperatingSystemBuildInformation.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-OperatingSystemBuildInformation.ps1
@@ -16,7 +16,7 @@ function Get-OperatingSystemBuildInformation {
         $win32_OperatingSystem = Get-WmiObjectCriticalHandler -ComputerName $Server -Class Win32_OperatingSystem -CatchActionFunction ${Function:Invoke-CatchActions}
     } end {
         return [PSCustomObject]@{
-            VersionBuild    = $win32_OperatingSystem.Version
+            BuildVersion    = [System.Version]$win32_OperatingSystem.Version
             MajorVersion    = (Get-ServerOperatingSystemVersion -OsCaption $win32_OperatingSystem.Caption)
             FriendlyName    = $win32_OperatingSystem.Caption
             OperatingSystem = $win32_OperatingSystem
