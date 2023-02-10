56d8b94332e4f8875817f178348993d480e6c04a
David Paulson <dpaul@microsoft.com>
2023-02-10 14:18:08 -0600

    Get-NETFrameworkVersion to find by short name

--- Shared/Get-NETFrameworkVersion.ps1
+++ Shared/Get-NETFrameworkVersion.ps1
@@ -4,60 +4,76 @@
 . $PSScriptRoot\Get-RemoteRegistryValue.ps1
 
 function Get-NETFrameworkVersion {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "CollectFromServer")]
     param(
+        [Parameter(ParameterSetName = "CollectFromServer", Position = 1)]
         [string]$MachineName = $env:COMPUTERNAME,
+
+        [Parameter(ParameterSetName = "NetKey")]
         [int]$NetVersionKey = -1,
+
+        [Parameter(ParameterSetName = "NetName")]
+        [ValidateScript({ ValidateNetNameParameter $_ })]
+        [string]$NetVersionShortName,
+
         [ScriptBlock]$CatchActionFunction
     )
     begin {
         Write-Verbose "Calling: $($MyInvocation.MyCommand)"
         $friendlyName = [string]::Empty
         $minValue = -1
+        $netVersionDictionary = GetNetVersionDictionary
+
+        if ($PSCmdlet.ParameterSetName -eq "NetName") {
+            $NetVersionKey = $netVersionDictionary[$NetVersionShortName]
+        }
     }
     process {
 
         if ($NetVersionKey -eq -1) {
-            [int]$NetVersionKey = Get-RemoteRegistryValue -MachineName $MachineName `
-                -SubKey "SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" `
-                -GetValue "Release" `
-                -CatchActionFunction $CatchActionFunction
+            $params = @{
+                MachineName         = $MachineName
+                SubKey              = "SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+                GetValue            = "Release"
+                CatchActionFunction = $CatchActionFunction
+            }
+            [int]$NetVersionKey = Get-RemoteRegistryValue @params
         }
 
         #Using Minimum Version as per https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed?redirectedfrom=MSDN#minimum-version
-        if ($NetVersionKey -lt 378389) {
+        if ($NetVersionKey -lt $netVersionDictionary["Net4d5"]) {
             $friendlyName = "Unknown"
             $minValue = -1
-        } elseif ($NetVersionKey -lt 378675) {
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d5d1"]) {
             $friendlyName = "4.5"
-            $minValue = 378389
-        } elseif ($NetVersionKey -lt 379893) {
+            $minValue = $netVersionDictionary["Net4d5"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d5d2"]) {
             $friendlyName = "4.5.1"
-            $minValue = 378675
-        } elseif ($NetVersionKey -lt 393295) {
+            $minValue = $netVersionDictionary["Net4d5d1"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d6"]) {
             $friendlyName = "4.5.2"
-            $minValue = 379893
-        } elseif ($NetVersionKey -lt 394254) {
+            $minValue = $netVersionDictionary["Net4d5d2"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d6d1"]) {
             $friendlyName = "4.6"
-            $minValue = 393295
-        } elseif ($NetVersionKey -lt 394802) {
+            $minValue = $netVersionDictionary["Net4d6"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d6d2"]) {
             $friendlyName = "4.6.1"
-            $minValue = 394254
-        } elseif ($NetVersionKey -lt 460798) {
+            $minValue = $netVersionDictionary["Net4d6d1"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d7"]) {
             $friendlyName = "4.6.2"
-            $minValue = 394802
-        } elseif ($NetVersionKey -lt 461308) {
+            $minValue = $netVersionDictionary["Net4d6d2"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d7d1"]) {
             $friendlyName = "4.7"
-            $minValue = 460798
-        } elseif ($NetVersionKey -lt 461808) {
+            $minValue = $netVersionDictionary["Net4d7"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d7d2"]) {
             $friendlyName = "4.7.1"
-            $minValue = 461308
-        } elseif ($NetVersionKey -lt 528040) {
+            $minValue = $netVersionDictionary["Net4d7d1"]
+        } elseif ($NetVersionKey -lt $netVersionDictionary["Net4d8"]) {
             $friendlyName = "4.7.2"
-            $minValue = 461808
-        } elseif ($NetVersionKey -ge 528040) {
+            $minValue = $netVersionDictionary["Net4d7d2"]
+        } elseif ($NetVersionKey -ge $netVersionDictionary["Net4d8"]) {
             $friendlyName = "4.8"
-            $minValue = 528040
+            $minValue = $netVersionDictionary["Net4d8"]
         }
     }
     end {
@@ -68,4 +84,27 @@ function Get-NETFrameworkVersion {
             MinimumValue  = $minValue
         }
     }
+}
+
+function GetNetVersionDictionary {
+    return @{
+        "Net4d5"       = 378389
+        "Net4d5d1"     = 378675
+        "Net4d5d2"     = 379893
+        "Net4d5d2wFix" = 380035
+        "Net4d6"       = 393295
+        "Net4d6d1"     = 394254
+        "Net4d6d1wFix" = 394294
+        "Net4d6d2"     = 394802
+        "Net4d7"       = 460798
+        "Net4d7d1"     = 461308
+        "Net4d7d2"     = 461808
+        "Net4d8"       = 528040
+    }
+}
+
+function ValidateNetNameParameter {
+    param($name)
+    $netVersionNames = @((GetNetVersionDictionary).Keys)
+    $netVersionNames.Contains($name)
 }
