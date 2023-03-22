d5915a93745279f4b3807231265549b360a1518e
Lukas Sassl <lusassl@microsoft.com>
2023-03-22 17:06:40 +0100

    Preparing CertBasedAuth

---
+++ Shared/AzureFunctions/Get-NewJsonWebToken.ps1
@@ -0,0 +1,122 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Get-NewJsonWebToken {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$CertificateThumbprint,
+
+        [ValidateSet("CurrentUser", "LocalMachine")]
+        [Parameter(Mandatory = $false)]
+        [string]$CertificateStore = "CurrentUser",
+
+        [Parameter(Mandatory = $false)]
+        [string]$Issuer,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Audience,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Subject,
+
+        [Parameter(Mandatory = $false)]
+        [int]$TokenLifetimeInSeconds = 3600,
+
+        [ValidateSet("RS256", "RS384", "RS512")]
+        [Parameter(Mandatory = $false)]
+        [string]$SigningAlgorithm = "RS256"
+    )
+
+    <#
+        Shared function to create a signed Json Web Token (JWT) by using a certificate.
+        It is also possible to use a secret key to sign the token, but that is not supported in this function.
+        The function returns the token as a string if successful, otherwise it returns $null.
+        https://www.rfc-editor.org/rfc/rfc7519
+        https://learn.microsoft.com/azure/active-directory/develop/active-directory-certificate-credentials
+        https://learn.microsoft.com/azure/active-directory/develop/v2-oauth2-client-creds-grant-flow
+    #>
+
+    begin {
+        Write-Verbose "Calling $($MyInvocation.MyCommand)"
+    }
+    process {
+        try {
+            $certificate = Get-ChildItem Cert:\$CertificateStore\My\$CertificateThumbprint
+            if ($certificate.HasPrivateKey) {
+                $privateKey = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($certificate)
+                # Base64url-encoded SHA-1 thumbprint of the X.509 certificate's DER encoding
+                $x5t = [System.Convert]::ToBase64String($certificate.GetCertHash())
+                $x5t = ((($x5t).Replace("\+", "-")).Replace("/", "_")).Replace("=", "")
+                Write-Verbose "x5t is: $x5t"
+            } else {
+                Write-Verbose "We don't have a private key for certificate: $CertificateThumbprint and so cannot sign the token"
+                return
+            }
+        } catch {
+            Write-Verbose "Unable to import the certificate - Exception: $($Error[0].Exception.Message)"
+            return
+        }
+
+        $header = [ordered]@{
+            alg = $SigningAlgorithm
+            typ = "JWT"
+            x5t = $x5t
+        }
+
+        # "iat" (issued at) and "exp" (expiration time) must be UTC and in UNIX time format
+        $payload = @{
+            iat = [Math]::Round((Get-Date).ToUniversalTime().Subtract((Get-Date -Date "01/01/1970")).TotalSeconds)
+            exp = [Math]::Round((Get-Date).ToUniversalTime().Subtract((Get-Date -Date "01/01/1970")).TotalSeconds) + $TokenLifetimeInSeconds
+        }
+
+        # Issuer, Audience and Subject are optional as per RFC 7519
+        if (-not([System.String]::IsNullOrEmpty($Issuer))) {
+            Write-Verbose "Issuer: $Issuer will be added to payload"
+            $payload.Add("iss", $Issuer)
+        }
+
+        if (-not([System.String]::IsNullOrEmpty($Audience))) {
+            Write-Verbose "Audience: $Audience will be added to payload"
+            $payload.Add("aud", $Audience)
+        }
+
+        if (-not([System.String]::IsNullOrEmpty($Subject))) {
+            Write-Verbose "Subject: $Subject will be added to payload"
+            $payload.Add("sub", $Subject)
+        }
+
+        $headerJson = $header | ConvertTo-Json -Compress
+        $payloadJson = $payload | ConvertTo-Json -Compress
+
+        $headerBase64 = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($headerJson)).Split("=")[0].Replace("+", "-").Replace("/", "_")
+        $payloadBase64 = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($payloadJson)).Split("=")[0].Replace("+", "-").Replace("/", "_")
+
+        $signatureInput = [System.Text.Encoding]::ASCII.GetBytes("$headerBase64.$payloadBase64")
+
+        Write-Verbose "Header (Base64) is: $headerBase64"
+        Write-Verbose "Payload (Base64) is: $payloadBase64"
+        Write-Verbose "Signature input is: $signatureInput"
+
+        $signingAlgorithmToUse = switch ($SigningAlgorithm) {
+            ("RS384") { [Security.Cryptography.HashAlgorithmName]::SHA384 }
+            ("RS512") { [Security.Cryptography.HashAlgorithmName]::SHA512 }
+            default { [Security.Cryptography.HashAlgorithmName]::SHA256 }
+        }
+        Write-Verbose "Signing the Json Web Token using: $SigningAlgorithm"
+
+        $signature = $privateKey.SignData($signatureInput, $signingAlgorithmToUse, [Security.Cryptography.RSASignaturePadding]::Pkcs1)
+        $signature = [Convert]::ToBase64String($signature).Split("=")[0].Replace("+", "-").Replace("/", "_")
+    }
+    end {
+        if ((-not([System.String]::IsNullOrEmpty($headerBase64))) -and
+            (-not([System.String]::IsNullOrEmpty($payloadBase64))) -and
+            (-not([System.String]::IsNullOrEmpty($signature)))) {
+            Write-Verbose "Returning Json Web Token"
+            return ("$headerBase64.$payloadBase64.$signature")
+        } else {
+            Write-Verbose "Unable to create Json Web Token"
+            return
+        }
+    }
+}
