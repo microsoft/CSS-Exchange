7f46840682dbd0881d028186405f2c6c9c3c8729
David Paulson <dpaul@microsoft.com>
2023-04-18 13:27:59 -0500

    New file Invoke-SearchServiceState

---
+++ Search/Troubleshoot-ModernSearch/Invoke-SearchServiceState.ps1
@@ -0,0 +1,18 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\Exchange\Get-SearchProcessState.ps1
+. $PSScriptRoot\Write\Write-SearchProcessStateObject.ps1
+
+function Invoke-SearchServiceState {
+    [CmdletBinding()]
+    param(
+        [string[]]$Servers
+    )
+    process {
+        foreach ($server in $Servers) {
+            $searchProcessState = Get-SearchProcessState -ComputerName $server
+            Write-SearchProcessStateObject $searchProcessState
+        }
+    }
+}
