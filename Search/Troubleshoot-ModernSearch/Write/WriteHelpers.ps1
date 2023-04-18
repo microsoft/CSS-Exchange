8be7d001e927e0f26fbc71ed577c82f3c7b8b21f
David Paulson <dpaul@microsoft.com>
2023-04-18 13:38:23 -0500

    New file WriteHelpers

---
+++ Search/Troubleshoot-ModernSearch/Write/WriteHelpers.ps1
@@ -0,0 +1,24 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Write-DashLineBox {
+    [CmdletBinding()]
+    param(
+        [string]$Line
+    )
+    <#
+        This is to simply create a quick and easy display around a line
+        -------------------------------------
+        Line                           Length
+        -------------------------------------
+        # Empty Line
+    #>
+
+    $dashLine = [string]::Empty
+    1..$Line.Length | ForEach-Object { $dashLine += "-" }
+    Write-Host $dashLine
+    Write-Host $Line
+    Write-Host $dashLine
+    Write-Host
+}
+
