0824cd00c0e6c4d41bdf70c591abc301ed5cb3b6
David Paulson <dpaul@microsoft.com>
2023-04-19 15:21:04 -0500

    Update to the logic of Write-SearchProcessStateObject

--- Search/Troubleshoot-ModernSearch/Write/Write-SearchProcessStateObject.ps1
+++ Search/Troubleshoot-ModernSearch/Write/Write-SearchProcessStateObject.ps1
@@ -1,70 +1,77 @@
 ﻿# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-. $PSScriptRoot\..\Exchange\Get-SearchProcessState.ps1
 . $PSScriptRoot\Write-DisplayObjectInformation.ps1
-function Write-CheckSearchProcessState {
+. $PSScriptRoot\WriteHelpers.ps1
+
+function Write-SearchProcessStateObject {
     [CmdletBinding()]
     param(
-        [string]$ActiveServer
+        [object]$SearchProcessState # The object from Get-SearchProcessState.ps1
     )
-    begin {
-        $searchProcessState = Get-SearchProcessState -ComputerName $ActiveServer
-        $thirdPartyModuleFound = $false
-        $checksPassed = $true
-        $status = "Passed"
-    }
     process {
 
-        if (-not ($searchProcessState.ServicesCheckPass) -or
-            -not ($searchProcessState.ProcessesCheckPass)) {
-            $checksPassed = $false
+        # If any of the things we check for is false, we want to state the Search Process State is in a failed state
+        $statusReason = [string]::Empty
+
+        if (-not ($SearchProcessState.ServicesConfigCorrectly) -or
+            -not ($SearchProcessState.AllProcessesRunning) -or
+            -not ($SearchProcessState.RunForAnHour)) {
+
+            if (-not ($SearchProcessState.ServicesConfigCorrectly)) {
+                $statusReason = "Services aren't configured correctly. Must be running and automatic."
+            } elseif (-not ($SearchProcessState.AllProcessesRunning)) {
+                $statusReason = "Not all processes are running."
+            } else {
+                $statusReason = "Processes haven't been running for at least 1 hour. This could be caused by a crash or an unexpected restart."
+            }
             $status = "Failed"
+        } elseif ($SearchProcessState.ThirdPartyModuleFound) {
+            # Warning only if we didn't fail when we detect 3rd party modules
+            $status = "Warning - Third Party Modules Detected"
+        } else {
+            $status = "Success"
         }
 
-        Write-Host "----------------------------------------"
-        Write-Host "Search Processes Status: $status"
+        Write-DashLineBox "Search Process Status - $($SearchProcessState.ServerName): $status"
 
-        if (-not($checksPassed)) {
-            Write-Host "Latest Process Start: $($searchProcessState.LatestProcessStartTime)"
+        if ($statusReason -ne [string]::Empty) {
+            Write-Host "Failed Reason: $statusReason"
+            Write-Host ""
         }
 
-        foreach ($key in $searchProcessState.ProcessResults.Keys) {
-
-            $process = $searchProcessState.ProcessResults[$key]
-
-            Write-Verbose "------------------------"
-            Write-Verbose "Process: $key"
-            Write-Verbose "PID: $($process.PID)"
-            Write-Verbose "StartTime: $($process.StartTime)"
-
-            if ($process.StartTime -eq [DateTime]::MinValue) {
-                Write-Host "Process '$key' Isn't Started!!! This will cause search issues!!!"
-            } elseif ($process.StartTime -gt ([DateTime]::Now.AddHours(-1))) {
-                Write-Host "Process '$key' hasn't been running for at least an hour. This could mean it is crashing causing search issues."
-            }
-
-            if ($process.ThirdPartyModules.Count -ge 1) {
-
-                Write-Host "Third Party Modules Loaded into Process '$key'"
-
-                foreach ($module in $process.ThirdPartyModules) {
-                    Write-Verbose "----------"
-                    $module |
-                        Select-Object ModuleName, FileName, Company |
-                        Out-String |
-                        Write-Verbose
-                }
-                $thirdPartyModuleFound = $true
-            }
+        # Write out quick find info for debug
+        if ($status -ne "Success") {
+            Write-Verbose "Latest Process Start: $($SearchProcessState.LatestProcessStartTime)"
+            Write-Verbose "ServicesConfigCorrectly: $($SearchProcessState.ServicesConfigCorrectly)"
+            Write-Verbose "AllProcessesRunning: $($SearchProcessState.AllProcessesRunning)"
+            Write-Verbose "RunForAnHour: $($SearchProcessState.RunForAnHour)"
+            Write-Verbose "ThirdPartyModuleFound: $($SearchProcessState.ThirdPartyModuleFound)"
+            Write-Verbose ""
         }
 
-        if ($thirdPartyModuleFound) {
+        # Display the Services and Process Information
+        foreach ($service in $SearchProcessState.ProcessInformation.Services) {
+            Write-Host "Name: $($service.Name)"
+            Write-Host "Status: $($service.Status)"
+            Write-Host "StartType: $($service.StartType)"
+            Write-Host ""
+        }
+
+        foreach ($process in $SearchProcessState.ProcessInformation.Processes) {
+            Write-Host "Process Name: $($process.Name)"
+            Write-Host "PID: $($process.PID)"
+            Write-Host "Start Time: $($process.StartTime)"
+            Write-Host "Contains 3rd Party Modules: $($process.ThirdPartyModules.Count -ne 0)"
+            Write-Host ""
+        }
+
+        #TODO: Display the modules
+        if ($SearchProcessState.ThirdPartyModuleFound) {
             Write-Host "Please exclude AV from all Exchange processes: https://docs.microsoft.com/en-us/Exchange/antispam-and-antimalware/windows-antivirus-software?view=exchserver-2019"
+            Write-Host ""
         }
 
-        Write-Host ""
-        Write-Host "----------------------------------------"
         Write-Host ""
     }
 }
