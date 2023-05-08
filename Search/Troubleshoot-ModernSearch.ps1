32d8ae2364dd6df30e5974c1e7691168a3ab1b8d
David Paulson <dpaul@microsoft.com>
2023-05-08 11:23:52 -0500

    View the entire forest always

--- Search/Troubleshoot-ModernSearch.ps1
+++ Search/Troubleshoot-ModernSearch.ps1
@@ -420,6 +420,7 @@ try {
 
     Write-Verbose "Starting Script At: $([DateTime]::Now)"
     Write-Host "Exchange Troubleshot Modern Search Version $BuildVersion"
+    Set-ADServerSettings -ViewEntireForest $true
     Main
     Write-Verbose "Finished Script At: $([DateTime]::Now)"
 
