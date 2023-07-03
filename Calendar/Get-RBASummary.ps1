17c1f69201e0873e997d902425def4e15f415d4d
Shane Ferrell <ShaneFe@Microsoft.com>
2023-07-03 08:54:21 -0700

    Removed Read-Host

--- Calendar/Get-RBASummary.ps1
+++ Calendar/Get-RBASummary.ps1
@@ -29,7 +29,6 @@ if (Test-ScriptVersion -AutoUpdate) {
     Write-Host "Script was updated. Please rerun the command."  -ForegroundColor Yellow
     return
 }
-Read-Host -Prompt "Press Enter to continue"
 
 function ValidateMailbox {
     Write-Host -NoNewline "Running : "; Write-Host -ForegroundColor Cyan "Get-Mailbox -Identity $Identity"
