c374b6a3f9d7ae635e5a44855246a37da54e8070
Lukas Sassl <lusassl@microsoft.com>
2023-08-24 15:49:11 +0200

    Unit tests updated

--- Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
+++ Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
@@ -139,11 +139,11 @@ Describe "Testing Health Checker by Mock Data Imports - Exchange 2016" {
 
             $cveTests = GetObject "Security Vulnerability"
             $cveTests.Contains("CVE-2020-1147") | Should -Be $true
-            $cveTests.Count | Should -Be 39
+            $cveTests.Count | Should -Be 44
             $downloadDomains = GetObject "CVE-2021-1730"
             $downloadDomains.DownloadDomainsEnabled | Should -Be "false"
 
-            $Script:ActiveGrouping.Count | Should -Be 46
+            $Script:ActiveGrouping.Count | Should -Be 51
         }
     }
 
