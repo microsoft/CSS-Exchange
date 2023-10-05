1d171b2142c884d0f5090d03f44d670a342d2cac
Lukas Sassl <lusassl@microsoft.com>
2023-10-05 12:56:57 +0200

    October 2023 SU information and vulnerabilities

--- Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
+++ Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
@@ -140,11 +140,11 @@ Describe "Testing Health Checker by Mock Data Imports - Exchange 2016" {
 
             $cveTests = GetObject "Security Vulnerability"
             $cveTests.Contains("CVE-2020-1147") | Should -Be $true
-            $cveTests.Count | Should -Be 44
+            $cveTests.Count | Should -Be 45
             $downloadDomains = GetObject "CVE-2021-1730"
             $downloadDomains.DownloadDomainsEnabled | Should -Be "false"
 
-            $Script:ActiveGrouping.Count | Should -Be 51
+            $Script:ActiveGrouping.Count | Should -Be 52
         }
     }
 
