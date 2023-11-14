446ef03f3c760e75891be8c0475cf9417dc96031
Bill Long <bilong@microsoft.com>
2023-11-14 12:11:53 -0800

    Refactor VSSTester

--- Databases/VSSTester/Logging/Get-WindowsEventLogs.ps1
+++ Databases/VSSTester/Logging/Get-WindowsEventLogs.ps1
@@ -2,26 +2,33 @@
 # Licensed under the MIT License.
 
 function Get-WindowsEventLogs {
+    [OutputType([System.Void])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [DateTime]
+        $StartTime,
 
-    function Get-WindowEventsPerServer {
-        param(
-            [string]$ComputerName
-        )
-        "Getting application log events..."
-        Get-WinEvent -FilterHashtable @{LogName = "Application"; StartTime = $startTime } -ComputerName $ComputerName | Export-Clixml $path\events-$ComputerName-App.xml
-        "Getting system log events..."
-        Get-WinEvent -FilterHashtable @{LogName = "System"; StartTime = $startTime } -ComputerName $ComputerName | Export-Clixml $path\events-$ComputerName-System.xml
-        "Getting TruncationDebug log events..."
-        Get-WinEvent -FilterHashtable @{LogName = "Microsoft-Exchange-HighAvailability/TruncationDebug"; StartTime = $startTime } -ComputerName $ComputerName -ErrorAction SilentlyContinue | Export-Clixml $path\events-$ComputerName-TruncationDebug.xml
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ComputerName,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $OutputPath
+    )
+
+    Write-Host "$(Get-Date) Getting events from the application and system logs since the script's start time of ($StartTime)"
+
+    $timeString = $StartTime.ToUniversalTime().ToString("O")
+    Write-Host "  Getting application log events..."
+    if (Test-Path $OutputPath\events-Application.evtx) {
+        Remove-Item $OutputPath\events-Application.evtx
     }
-    " "
-    Get-Date
-    Write-Host "Getting events from the application and system logs since the script's start time of ($startInfo)" -ForegroundColor Green $nl
-    Write-Host "--------------------------------------------------------------------------------------------------------------"
-    " "
-    "Getting application log events..."
-    Get-EventLog -LogName Application -After $startInfo | Export-Clixml $path\events-App.xml
-    "Getting system log events..."
-    Get-EventLog -LogName System -After $startInfo | Export-Clixml $path\events-System.xml
-    "Getting events complete!"
+    wevtutil epl Application $OutputPath\events-Application.evtx /q:"Event/System/TimeCreated[@SystemTime > '$timeString']"
+    Write-Host "  Getting system log events..."
+    if (Test-Path $OutputPath\events-System.evtx) {
+        Remove-Item $OutputPath\events-System.evtx
+    }
+    wevtutil epl System $OutputPath\events-System.evtx /q:"Event/System/TimeCreated[@SystemTime > '$timeString']"
+    Write-Host "  Getting events complete!"
 }
