df87078f826fe7951fe3c75b4892f7a593695f51
David Paulson <dpaul@microsoft.com>
2023-12-20 11:44:53 -0600

    New IIS Configuration Framework

---
+++ Security/src/IISManagement/Invoke-IISConfigurationRemoteAction.ps1
@@ -0,0 +1,138 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+<#
+.DESCRIPTION
+    Execute all the actions on the remote server. This is the script block that is to be sent to the server.
+
+    InputObject
+        [array]Actions
+            Set
+                [string]ParametersToString
+                [hashtable]Parameters
+                [string]Cmdlet
+            Get
+                [string]ParametersToString
+                [hashtable]Parameters
+                [string]Cmdlet
+            Restore
+                [string]Cmdlet
+                [hashtable]Parameters
+        [Nullable][int]ProgressParentId
+        [string]BackupFileName
+#>
+function Invoke-IISConfigurationRemoteAction {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [object]$InputObject
+    )
+    begin {
+        Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+        $restoreActions = New-Object System.Collections.Generic.List[object]
+        $errorContext = New-Object System.Collections.Generic.List[object]
+        $gatheredAllRestoreActions = $true
+        $restoreActionsSaved = $false
+        $allActionsPerformed = $true
+        $backupActionsCount = $InputObject.Actions.Count
+        $backupProgressCounter = 0
+        $totalActions = $InputObject.Actions.Count
+        $progressCounter = 0
+        $remoteActionProgressParams = @{
+            ParentId        = if ($null -eq $InputObject.ProgressParentId) { 1 } else { $InputObject.ProgressParentId }
+            Activity        = "Executing Actions on $env:ComputerName"
+            Status          = [string]::Empty
+            PercentComplete = 0
+        }
+    }
+    process {
+
+        if (-not ([string]::IsNullOrEmpty($InputObject.BackupFileName)) -or
+            $VerbosePreference) {
+            # If verbose is set or if we are backing up files, we want to collect the current values. Otherwise, skip over.
+            # Backup the current value that we are changing.
+            Write-Verbose "Attempting to get the current value of the action items to backup."
+            foreach ($actionItem in $InputObject.Actions) {
+
+                try {
+                    $backupProgressCounter++
+                    $progressCounter++
+                    $remoteActionProgressParams.Status = "Gathering current values. $backupProgressCounter of $backupActionsCount"
+                    $remoteActionProgressParams.PercentComplete = ($progressCounter / $totalActions * 100)
+                    Write-Progress @remoteActionProgressParams
+                    Write-Verbose "Working on '$($actionItem.Get.Cmdlet) $($actionItem.Get.ParametersToString)"
+                    $params = $actionItem.Get.Parameters
+                    $currentValue = & $actionItem.Get.Cmdlet @params
+
+                    #TODO: Need to determine if this is the correct course of logic when not dealing with a true value or a Set-WebConfigProp
+                    if ($null -ne $currentValue) {
+                        Write-Verbose "Current value set on the server: $currentValue"
+                        $actionItem.Restore.Parameters.Add("Value", $currentValue)
+                        $restoreActions.Add($actionItem.Restore)
+                    } else {
+                        #TODO: need a test case here
+                        throw "NULL Current Value Address Logic"
+                    }
+                } catch {
+                    Write-Verbose "Failed to collect restore actions."
+                    $gatheredAllRestoreActions = $false
+                    $errorContext.Add($_)
+                    # We don't want to continue so throw to break out.
+                    throw "Failed to get the restore actions, therefore we are unable to set the configuration. Inner Exception: $_"
+                }
+            }
+
+            # Save the restore information
+            try {
+                # TODO Improve logic here.
+                if ([string]::IsNullOrEmpty($InputObject.BackupFileName)) {
+                    Write-Verbose "No Backup File Name Provided, so we aren't going to backup what we have on the server."
+                } else {
+                    $outFilePath = "IISManagementRestoreCmdlets-$($InputObject.BackupFileName)"
+                    $outFilePath = [System.IO.Path]::Join("$($env:WINDIR)\System32\inetSrv\config\", $outFilePath)
+                    $restoreActions | ConvertTo-Json -ErrorAction Stop | Out-File $outFilePath -ErrorAction Stop
+                    $restoreActionsSaved = $true
+                }
+            } catch {
+                Write-Verbose "Failed to Save Out the Restore Cmdlets. Inner Exception: $_"
+            }
+        } else {
+            $restoreActionsSaved = $true
+        }
+
+        # Proceed to set the configuration
+        foreach ($actionItem in $InputObject.Actions.Set) {
+            $commandParameters = $actionItem.Parameters
+
+            $location = if ($null -ne $commandParameters["Location"]) { $commandParameters["Location"] } { $commandParameters["PSPath"] }
+            $progressCounter++
+            $remoteActionProgressParams.Status = "Setting $($commandParameters["Name"]) at '$location'"
+            $remoteActionProgressParams.PercentComplete = ($progressCounter / $totalActions * 100)
+            Write-Progress @remoteActionProgressParams
+
+            try {
+                & $actionItem.Cmdlet @commandParameters
+            } catch {
+                $location = if ([string]::IsNullOrEmpty($commandParameters["Location"])) { $commandParameters["PSPath"] } else { $commandParameters["Location"] }
+                Write-Verbose "$($env:COMPUTERNAME): Failed to set '$($commandParameters["Name"])' for '$location' with the value '$($commandParameters["Value"])'. Inner Exception $_"
+                $allActionsPerformed = $false
+                $errorContext.Add($_)
+            }
+        }
+    }
+
+    end {
+        Write-Progress @remoteActionProgressParams -Completed
+
+        return [PSCustomObject]@{
+            ComputerName              = $env:COMPUTERNAME
+            AllActionsPerformed       = $allActionsPerformed
+            GatheredAllRestoreActions = $gatheredAllRestoreActions
+            RestoreActions            = $restoreActions
+            RestoreActionsSaved       = $restoreActionsSaved
+            SuccessfulExecution       = $allActionsPerformed -and $gatheredAllRestoreActions -and $restoreActions -and
+            $restoreActionsSaved -and $errorContext.Count -eq 0
+            ErrorContext              = $errorContext
+        }
+    }
+}
---
+++ Security/src/IISManagement/New-IISConfigurationAction.ps1
@@ -0,0 +1,85 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\Get-ParameterString.ps1
+
+<#
+.DESCRIPTION
+    Creates the configuration action object and validates the parameters that is added to it.
+#>
+function New-IISConfigurationAction {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Justification = 'No state change.')]
+    [CmdletBinding()]
+    param(
+        # A PSCustomObject that contains a property of [string]Cmdlet and [hashtable]Parameters that is required.
+        # Cmdlet is the one that you are going to use and Parameters is what is passed to the cmdlet.
+        # An optional property is a description of the action
+        [Parameter(Mandatory = $true)]
+        [object]$Action,
+
+        [string]$OverrideErrorAction = "Stop",
+
+        [bool]$OverrideWhatIf = $WhatIfPreference
+    )
+    begin {
+
+        if (([string]::IsNullOrEmpty($Action.Cmdlet)) -or
+            $null -eq $Action.Parameters -or
+            $Action.Parameters.GetType().Name -ne "hashtable") {
+            throw "Invalid Action parameter provided"
+        }
+
+        $Action.Parameters["ErrorAction"] = $OverrideErrorAction
+        $Action.Parameters["WhatIf"] = $OverrideWhatIf
+        $cmdParameters = $Action.Parameters
+        Write-Verbose "Provided Action Cmdlet: '$($Action.Cmdlet)' Parameters: '$(Get-ParameterString $cmdParameters)'"
+        $setWebConfigPropCmdlet = "Set-WebConfigurationProperty"
+        $getCurrentValueAction = $null
+        $restoreAction = $null
+    }
+    process {
+        #TODO: Validate the Action.Parameters Pester Testing.
+        # Validate the Action to make sure it passes prior to trying to execute.
+        if ($Action.Cmdlet -eq $setWebConfigPropCmdlet) {
+            # Set-WebConfigurationProperty requires Filter, Name, and Value.
+            # We will also be requiring PSPath for this.
+            # We currently are always using it and it should help clarify where we are making the change at.
+            if (([string]::IsNullOrEmpty($cmdParameters["Filter"])) -or
+                ([string]::IsNullOrEmpty($cmdParameters["Name"])) -or
+                ([string]::IsNullOrEmpty($cmdParameters["Value"])) -or
+                ([string]::IsNullOrEmpty($cmdParameters["PSPath"]))) {
+                throw "Invalid cmdlet parameters provided for $setWebConfigPropCmdlet." +
+                " Expected value for Filter, Name, Value, and PSPath. Provided: '$(Get-ParameterString $cmdParameters)'"
+            }
+            $currentValueActionParams = @{
+                Filter      = $cmdParameters["Filter"]
+                Name        = $cmdParameters["Name"]
+                PSPath      = $cmdParameters["PSPath"]
+                ErrorAction = "Stop"
+            }
+
+            if (-not([string]::IsNullOrEmpty($cmdParameters["Location"]))) {
+                $currentValueActionParams.Add("Location", $cmdParameters["Location"])
+            }
+            $getCurrentValueAction = [PSCustomObject]@{
+                Cmdlet             = "Get-WebConfigurationProperty"
+                Parameters         = $currentValueActionParams
+                ParametersToString = (Get-ParameterString $currentValueActionParams)
+            }
+            $restoreAction = [PSCustomObject]@{
+                Cmdlet     = $setWebConfigPropCmdlet
+                Parameters = $currentValueActionParams # Should be the same, then when executing on the server, add the value.
+            }
+        }
+
+        return [PSCustomObject]@{
+            Set     = [PSCustomObject]@{
+                Cmdlet             = $Action.Cmdlet
+                Parameters         = $cmdParameters
+                ParametersToString = (Get-ParameterString $cmdParameters)
+            }
+            Get     = $getCurrentValueAction
+            Restore = $restoreAction
+        }
+    }
+}
