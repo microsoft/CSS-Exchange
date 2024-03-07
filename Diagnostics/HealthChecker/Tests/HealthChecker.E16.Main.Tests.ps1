7b114fd0a49a673252f38fa6d92b858e4a404ab8
David Paulson <dpaul@microsoft.com>
2024-03-07 13:35:13 -0600

    Add CVE-2024-26198 to HC

--- Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
+++ Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
@@ -141,11 +141,11 @@ Describe "Testing Health Checker by Mock Data Imports - Exchange 2016" {
             $cveTests = GetObject "Security Vulnerability"
             $cveTests.Contains("CVE-2020-1147") | Should -Be $true
             $cveTests.Contains("CVE-2023-36039") | Should -Be $true
-            $cveTests.Count | Should -Be 49
+            $cveTests.Count | Should -Be 50
             $downloadDomains = GetObject "CVE-2021-1730"
             $downloadDomains.DownloadDomainsEnabled | Should -Be "false"
 
-            $Script:ActiveGrouping.Count | Should -Be 56
+            $Script:ActiveGrouping.Count | Should -Be 57
         }
     }
 
