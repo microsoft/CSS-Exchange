5eb7bdb5b5ec2de650b9ebbc754f925d0f6e7de5
Lukas Sassl <lusassl@microsoft.com>
2024-04-24 12:07:47 +0200

    Improve handling of ECC certificates in HC

--- Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
+++ Diagnostics/HealthChecker/Tests/HealthChecker.E16.Main.Tests.ps1
@@ -132,7 +132,7 @@ Describe "Testing Health Checker by Mock Data Imports - Exchange 2016" {
             TestObjectMatch "Pattern service" "Unreachable`r`n`t`tMore information: https://aka.ms/HelpConnectivityEEMS" -WriteType "Yellow"
             TestObjectMatch "Telemetry enabled" "False"
 
-            $Script:ActiveGrouping.Count | Should -Be 99
+            $Script:ActiveGrouping.Count | Should -Be 102
         }
 
         It "Display Results - Security Vulnerability" {
