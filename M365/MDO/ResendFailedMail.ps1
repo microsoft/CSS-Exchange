174b16e14a1cdde8edbb052d623c2ae448c5a8aa
Ignacio Serrano <103440830+iserrano76@users.noreply.github.com>
2024-09-25 12:06:56 +0200

    Initial commit ResendFailedMail

---
+++ M365/MDO/ResendFailedMail.ps1
@@ -0,0 +1,287 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Get-MgContext
+#Requires -Modules Microsoft.Graph.Authentication
+# Get-MgUserMessage
+#Requires -Modules Microsoft.Graph.Mail
+# Send-MgUserMail Send-MgUserMessage
+#Requires -Modules Microsoft.Graph.Users.Actions
+#Requires -Modules ExchangeOnlineManagement -Version 3.0.0
+
+# How to connect:
+# Connect-MgGraph -TenantId "[YOUR TENANT ID HERE]" -ClientSecretCredential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "[YOUR APP ID HERE]", (ConvertTo-SecureString -String "[VALUE FIELD OF YOUR SECRET HERE]" -AsPlainText -Force)) -NoWelcome
+# Connect-ExchangeOnline -ShowBanner:$false
+
+[CmdletBinding(DefaultParameterSetName = 'ResendCopyFailed', SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+param(
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [string[]]$Sender,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [string[]]$Subject,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [string[]]$Recipient,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [string[]]$MessageId,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [switch]$IncludeDuplicates,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [switch]$Force,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [int]$Days = 1,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [switch]$SkipConnectionCheck,
+    [Parameter(Mandatory = $false, ParameterSetName = "ResendCopyFailed")]
+    [switch]$SkipVersionCheck,
+    [Parameter(Mandatory = $true, ParameterSetName = "ScriptUpdateOnly")]
+    [switch]$ScriptUpdateOnly
+)
+
+. $PSScriptRoot\..\..\Shared\ScriptUpdateFunctions\Test-ScriptVersion.ps1
+. $PSScriptRoot\..\..\Shared\LoggerFunctions.ps1
+. $PSScriptRoot\..\..\Shared\OutputOverrides\Write-Host.ps1
+. $PSScriptRoot\..\..\Shared\OutputOverrides\Write-Progress.ps1
+. $PSScriptRoot\..\..\Shared\OutputOverrides\Write-Verbose.ps1
+. $PSScriptRoot\..\..\Shared\OutputOverrides\Write-Warning.ps1
+
+$recipientCache = @{}
+
+function Write-DebugLog ($message) {
+    if (![string]::IsNullOrEmpty($message)) {
+        $Script:DebugLogger = $Script:DebugLogger | Write-LoggerInstance $message
+    }
+}
+
+function Write-HostLog ($message) {
+    if (![string]::IsNullOrEmpty($message)) {
+        $Script:HostLogger = $Script:HostLogger | Write-LoggerInstance $message
+    }
+    # all write-host should be logged in the debug log as well.
+    Write-DebugLog $message
+}
+
+$LogFileName = "ResendFailedMail"
+$StartDate = Get-Date
+$StartDateFormatted = ($StartDate).ToString("yyyyMMddhhmmss")
+$Script:DebugLogger = Get-NewLoggerInstance -LogName "$LogFileName-Debug-$StartDateFormatted" -LogDirectory $PSScriptRoot -AppendDateTimeToFileName $false -ErrorAction SilentlyContinue
+$Script:HostLogger = Get-NewLoggerInstance -LogName "$LogFileName-Results-$StartDateFormatted" -LogDirectory $PSScriptRoot -AppendDateTimeToFileName $false -ErrorAction SilentlyContinue
+SetWriteHostAction ${Function:Write-HostLog}
+SetWriteProgressAction ${Function:Write-DebugLog}
+SetWriteVerboseAction ${Function:Write-DebugLog}
+SetWriteWarningAction ${Function:Write-HostLog}
+
+$BuildVersion = ""
+
+Write-Host ("ResendFailedMail.ps1 script version $($BuildVersion)") -ForegroundColor Green
+
+if ($ScriptUpdateOnly) {
+    switch (Test-ScriptVersion -AutoUpdate -VersionsUrl "https://aka.ms/ResendFailedMail-VersionsURL" -Confirm:$false) {
+        ($true) { Write-Host ("Script was successfully updated.") -ForegroundColor Green }
+        ($false) { Write-Host ("No update of the script performed.") -ForegroundColor Yellow }
+        default { Write-Host ("Unable to perform ScriptUpdateOnly operation.") -ForegroundColor Red }
+    }
+    return
+}
+
+if ((-not($SkipVersionCheck)) -and (Test-ScriptVersion -AutoUpdate -VersionsUrl "https://aka.ms/ResendFailedMail-VersionsURL" -Confirm:$false)) {
+    Write-Host ("Script was updated. Please re-run the command.") -ForegroundColor Yellow
+    return
+}
+
+function Test-GraphContext {
+    [OutputType([bool])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]]$Scopes,
+        [Parameter(Mandatory = $true)]
+        [string[]]$ExpectedScopes
+    )
+
+    $validScope = $true
+    foreach ($expectedScope in $ExpectedScopes) {
+        if ($Scopes -contains $expectedScope) {
+            Write-Verbose "Scopes $expectedScope is present."
+        } else {
+            Write-Host "The following scope is missing: $expectedScope" -ForegroundColor Red
+            $validScope = $false
+        }
+    }
+    return $validScope
+}
+
+if (-not $SkipConnectionCheck) {
+    #Validate EXO PS Connection
+    $exoConnection = $null
+    try {
+        $exoConnection = Get-ConnectionInformation -ErrorAction Stop
+    } catch {
+        Write-Host "Error checking EXO connection:`n$_" -ForegroundColor Red
+        Write-Host "Verify that you have ExchangeOnlineManagement module installed." -ForegroundColor Yellow
+        Write-Host "You need a connection to Exchange Online; you can use:" -ForegroundColor Yellow
+        Write-Host "Connect-ExchangeOnline" -ForegroundColor Yellow
+        Write-Host "Exchange Online Powershell Module is required." -ForegroundColor Red
+        Write-Verbose "$_"
+        exit
+    }
+    if ($null -eq $exoConnection) {
+        Write-Host "Not connected to EXO" -ForegroundColor Red
+        Write-Host "You need a connection to Exchange Online; you can use:" -ForegroundColor Yellow
+        Write-Host "Connect-ExchangeOnline" -ForegroundColor Yellow
+        Write-Host "Exchange Online Powershell Module is required." -ForegroundColor Red
+        exit
+    } elseif ($exoConnection.count -eq 1) {
+        Write-Host " "
+        Write-Host "Connected to EXO"
+        Write-Host "Session details"
+        Write-Host "Tenant Id: $($exoConnection.TenantId)"
+        Write-Host "User: $($exoConnection.UserPrincipalName)"
+    } else {
+        Write-Host "You have more than one EXO session. Please use just one session." -ForegroundColor Red
+        exit
+    }
+
+    #Validate Graph is connected
+    $graphConnection = $null
+    Write-Host " "
+    try {
+        $graphConnection = Get-MgContext -ErrorAction Stop
+    } catch {
+        Write-Host "Error checking Graph connection:`n$_" -ForegroundColor Red
+        Write-Host "Verify that you have Microsoft.Graph.Mail and Microsoft.Graph.Users.Actions modules installed and loaded." -ForegroundColor Yellow
+        Write-Host "You could use:" -ForegroundColor Yellow
+        Write-Host "`tConnect-MgGraph -TenantId ""[YOUR TENANT ID HERE]"" -ClientSecretCredential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ""[YOUR APP ID HERE]"", (ConvertTo-SecureString -String ""[VALUE FIELD OF YOUR SECRET HERE]"" -AsPlainText -Force)) -NoWelcome" -ForegroundColor Yellow
+        Write-Verbose "$_"
+        exit
+    }
+    if ($null -eq $graphConnection) {
+        Write-Host "Not connected to Graph" -ForegroundColor Red
+        Write-Host "Verify that you have Microsoft.Graph.Mail and Microsoft.Graph.Users.Actions modules installed and loaded." -ForegroundColor Yellow
+        Write-Host "You could use:" -ForegroundColor Yellow
+        Write-Host "`tConnect-MgGraph -TenantId ""[YOUR TENANT ID HERE]"" -ClientSecretCredential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ""[YOUR APP ID HERE]"", (ConvertTo-SecureString -String ""[VALUE FIELD OF YOUR SECRET HERE]"" -AsPlainText -Force)) -NoWelcome" -ForegroundColor Yellow
+        exit
+    } elseif ($graphConnection.count -eq 1) {
+        $expectedScopes = 'Mail.Read', 'Mail.Send'
+        if (Test-GraphContext -Scopes $graphConnection.Scopes -ExpectedScopes $expectedScopes) {
+            Write-Host "Connected to Graph"
+            Write-Host "Session details"
+            Write-Host "TenantID: $(($graphConnection).TenantId)"
+            Write-Host "AuthType: $(($graphConnection).AuthType)"
+        } else {
+            Write-Host "We cannot continue without Graph Powershell session without Expected Scopes." -ForegroundColor Red
+            Write-Host "Verify that you have Microsoft.Graph.Mail and Microsoft.Graph.Users.Actions modules installed and loaded." -ForegroundColor Yellow
+            Write-Host "You could use:" -ForegroundColor Yellow
+            Write-Host "`tConnect-MgGraph -TenantId ""[YOUR TENANT ID HERE]"" -ClientSecretCredential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ""[YOUR APP ID HERE]"", (ConvertTo-SecureString -String ""[VALUE FIELD OF YOUR SECRET HERE]"" -AsPlainText -Force)) -NoWelcome" -ForegroundColor Yellow
+            exit
+        }
+    } else {
+        Write-Host "You have more than one Graph sessions. Please use just one session." -ForegroundColor Red
+        exit
+    }
+    if (($graphConnection.TenantId) -ne ($exoConnection.TenantId) ) {
+        Write-Host "`nThe Tenant Id from Graph and EXO are different. Please use the same tenant." -ForegroundColor Red
+        exit
+    }
+}
+
+$acceptedDomains = $null
+try {
+    $acceptedDomains = Get-AcceptedDomain -ErrorAction Stop
+} catch {
+    Write-Host "Error getting Accepted Domains:`n$_" -ForegroundColor Red
+    exit
+}
+
+if ($null -eq $acceptedDomains) {
+    Write-Host "We do not get accepted domains." -ForegroundColor Red
+    exit
+}
+
+if ($acceptedDomains.count -eq 0) {
+    Write-Host "No accepted domains found." -ForegroundColor Red
+    exit
+} else {
+    $acceptedDomainList = New-Object System.Collections.Generic.List[string]
+    $acceptedDomains | ForEach-Object { $acceptedDomainList.Add($_.DomainName.ToString()) }
+}
+
+$failedMessages = $null
+$failedMessages = Get-MessageTrace -StartDate (Get-Date).AddDays(-$Days) -EndDate (Get-Date) | Where-Object { $_.Status -eq "Failed" }
+
+if ($Sender) { $failedMessages = $failedMessages | Where-Object { $Sender -contains $_.SenderAddress } }
+if ($Subject) { $failedMessages = $failedMessages | Where-Object { $Subject -contains $_.Subject } }
+if ($Recipient) { $failedMessages = $failedMessages | Where-Object { $Recipient -contains $_.RecipientAddress } }
+if ($MessageId) { $failedMessages = $failedMessages | Where-Object { $MessageId -contains $_.MessageId } }
+if (-not $IncludeDuplicates) { $failedMessages = $failedMessages | Sort-Object MessageId -Unique }
+
+$failedMessages = $failedMessages | Where-Object { $acceptedDomainList -contains $_.SenderAddress.Split("@")[1] }
+
+$verifiedAcceptedSenderMessages = @()
+foreach ($failedMessage in $failedMessages) {
+    Write-Verbose "Checking $($failedMessage.SenderAddress)"
+    $tempAddress = $null
+    if ($recipientCache.ContainsKey($failedMessage.SenderAddress)) {
+        Write-Verbose "Recipient $($failedMessage.SenderAddress) found in cache"
+        $verifiedAcceptedSenderMessages += $failedMessage
+    } else {
+        try {
+            $tempAddress = Get-EXORecipient $failedMessage.SenderAddress -ErrorAction Stop
+            if ($null -eq $tempAddress) {
+                Write-Host "Sender $($failedMessage.RecipientAddress) is not a recipient in this tenant." -ForegroundColor Yellow
+                Write-Host "Discarded $($failedMessage.MessageId) - Subject: $($failedMessage.Subject)" -ForegroundColor Yellow
+            } else {
+                Write-Verbose "Added to cache Recipient $($failedMessage.SenderAddress) with Id $($failedMessage.SenderAddress)"
+                $recipientCache[$failedMessage.SenderAddress] = $failedMessage.SenderAddress
+                Write-Verbose "Verified $($failedMessage.SenderAddress)"
+                $verifiedAcceptedSenderMessages += $failedMessage
+            }
+        } catch {
+            Write-Host "Error getting Sender $($failedMessage.SenderAddress)" -ForegroundColor Yellow
+            Write-Host "Discarded $($failedMessage.MessageId) - Subject: $($failedMessage.Subject)" -ForegroundColor Yellow
+            Write-Verbose "$_"
+        }
+    }
+}
+
+if ( $verifiedAcceptedSenderMessages ) {
+    if ($verifiedAcceptedSenderMessages.Count) {
+        $totalMessages = $verifiedAcceptedSenderMessages.Count
+    } else {
+        $totalMessages = 1
+    }
+
+    if (-not $Force) {
+        Write-Host "`nWe are going to resend the following messages:"
+        Write-Host ($verifiedAcceptedSenderMessages | Format-Table -AutoSize Received, MessageId, SenderAddress, RecipientAddress, Subject | Out-String)
+        Write-Host "Total number of messages: $totalMessages`n"
+    }
+
+    if ($Force -or $PSCmdlet.ShouldContinue("Are you sure you want to do it?", "Resend messages")) {
+        $count = 0
+        foreach ( $failedMessage in $verifiedAcceptedSenderMessages ) {
+            $count++
+            Write-Progress -Activity "Resending Progress" -Status "$count of $totalMessages" -PercentComplete ($count / $totalMessages * 100) -CurrentOperation "Resending message $($failedMessage.MessageId) - Subject: $($failedMessage.Subject)"
+            try {
+                $fullMessage = $null
+                $fullMessage = Get-MgUserMessage -UserId $failedMessage.SenderAddress -Filter "InternetMessageId eq '$($failedMessage.MessageId)'" -ExpandProperty Attachments -ErrorAction Stop | Sort-Object ReceivedDateTime | Select-Object -First 1
+            } catch {
+                Write-Host "Error getting message $($failedMessage.MessageId) - Subject: $($failedMessage.Subject)" -ForegroundColor Red
+                Write-Verbose "$_"
+            }
+            if ($fullMessage.Count -eq 0) {
+                Write-Host "Message not found for $($failedMessage.MessageId)" -ForegroundColor Yellow
+            } else {
+                Write-Verbose "Resending message $($failedMessage.MessageId) - Subject: $($fullMessage.Subject)"
+                try {
+                    #Send-MgUserMail -UserId $failedMessage.SenderAddress -Message $fullMessage -ErrorAction Stop
+                    Send-MgUserMessage -UserId $failedMessage.SenderAddress -MessageId $fullMessage.Id -ErrorAction Stop
+                    Write-Host "Resent Message: $($failedMessage.MessageId) - Subject: $($fullMessage.Subject)"
+                } catch {
+                    Write-Host "Error resending message $($failedMessage.MessageId) - Subject: $($fullMessage.Subject)" -ForegroundColor Red
+                    Write-Verbose "$_"
+                }
+            }
+        }
+    }
+} else {
+    Write-Host "No messages found" -ForegroundColor Yellow
+}
---
+++ docs/M365/MDO/ResendFailedMail.md
@@ -0,0 +1,131 @@
+# ResendFailedMail
+
+Download the latest release: [ResendFailedMail.ps1](https://github.com/microsoft/CSS-Exchange/releases/latest/download/ResendFailedMail.ps1)
+
+### Need to be included ####
+Use this script to find inconsistencies or redundancies in user membership and policy application of Microsoft Defender for Office 365 and Exchange Online Protection threat policies that lead to missed or unexpected coverage of users by the policy. If issues are found, the script provides guidance on how to resolve them.
+
+The script also helps you identify which threat policies cover a particular user, including anti-malware, anti-phishing, inbound and outbound anti-spam, as well as Safe Attachments and Safe Links policies in case these are licensed for your tenant.
+
+The script can help with such questions as:
+
+- Are there confusing policies with conditions that lead to unexpected coverage or coverage gaps?
+
+- Which threat policies apply to a recipient, **or should have applied** but did not? **No actual detection or Network Message ID needed.**
+
+- Which actions would be taken on an email for each policy matched?
+
+The script runs only in Read mode from Exchange Online and Microsoft Graph PowerShell. It does not modify any policies, and only provides actionable guidance for administrators for remediation.
+
+## Prerequisites
+The script uses Powershell cmdlets from the Exchange Online module and from the Microsoft.Graph.Authentication, Microsoft.Graph.Groups, and Microsoft.Graph.Users modules.
+
+To run the Graph cmdlets used in this script, you only need the following modules from the Microsoft.Graph PowerShell SDK:
+
+- Microsoft.Graph.Groups: for managing groups, including `Get-MgGroup` and `Get-MgGroupMember`.
+
+- Microsoft.Graph.Users: for managing users, such as `Get-MgUser`.
+
+- Microsoft.Graph.Authentication: for authentication purposes and to run any cmdlet that interacts with Microsoft Graph.
+
+You can find the Microsoft Graph modules in the following link:<br>
+&nbsp;&nbsp;&nbsp;&nbsp;https://www.powershellgallery.com/packages/Microsoft.Graph/<br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0#installation
+
+
+Here's how you can install the required submodules for the PowerShell Graph SDK cmdlets:
+
+```powershell
+Install-Module -Name Microsoft.Graph.Authentication -Scope CurrentUser
+Install-Module -Name Microsoft.Graph.Groups -Scope CurrentUser
+Install-Module -Name Microsoft.Graph.Users -Scope CurrentUser
+```
+
+!!! warning "NOTE"
+
+    Remember to run these commands in a PowerShell session with the appropriate permissions. The -Scope CurrentUser parameter installs the modules for the current user only, which doesn't require administrative privileges.
+
+
+In the Graph connection, you will need the following scopes 'Group.Read.All','User.Read.All'<br>
+```powershell
+Connect-MgGraph -Scopes 'Group.Read.All','User.Read.All'
+```
+<br><br>
+You also need an Exchange Online session.<br>
+```powershell
+Connect-ExchangeOnline
+```
+
+You can find the Exchange module and information in the following links:<br>
+&nbsp;&nbsp;&nbsp;&nbsp;https://learn.microsoft.com/en-us/powershell/exchange/exchange-online-powershell-v2?view=exchange-ps<br>
+&nbsp;&nbsp;&nbsp;&nbsp;https://www.powershellgallery.com/packages/ExchangeOnlineManagement
+
+
+## Parameters and Use Cases:
+Run the script without any parameters to review all threat protection policies and to find inconsistencies with user inclusion and/or exclusion conditions:
+
+!['No Logical inconsistencies found'](img/No-Logical-Inconsistencies.png)
+
+**Script Output 1: No logical inconsistencies found** message if the policies are configured correctly, and no further corrections are required.
+
+![Potentially illogical inclusions found.](img/Logical-Inconsistency-Found.png)
+
+**Script Output 2: Logical inconsistencies found**. Inconsistencies found in the antispam policy named 'Custom antispam policy', and consequent recommendations shown -- illogical inclusions as both users and groups are specified. This policy will only apply to the users who are also members of the specified group.
+
+- IncludeMDOPolicies
+
+Add the parameter -IncludeMDOPolicies to view Microsoft Defender for Office 365 Safe Links and Safe Attachments policies:
+
+![Policies, including MDO.](img/Show-Policies-Including-MDO.png)
+
+**Script Output 3: Parameters -EmailAddress and -IncludeMDOPoliciesEOP** specified to validate Microsoft Defender for Office 365 Safe Attachments and Safe Links policies, on top of Exchange Online Protection policies.
+
+- ShowDetailedPolicies
+
+To see policy details, run the script with the -ShowDetailedPolicies parameter:
+
+![Show policy actions.](img/Show-Detailed-Policies-1.png)
+
+![Show policy actions.](img/Show-Detailed-Policies-2.png)
+
+![Show policy actions.](img/Show-Detailed-Policies-3.png)
+
+![Show policy actions.](img/Show-Detailed-Policies-4.png)
+
+**Script Output 4: Policy actions**. Use -ShowDetailedPolicies to see the details and actions for each policy.
+
+## Additional examples
+
+To provide multiple email addresses by command line and see only EOP policies, run the following:<br>
+```powershell
+.\MDOThreatPolicyChecker.ps1 -EmailAddress user1@contoso.com,user2@fabrikam.com
+```
+
+To provide a CSV input file with email addresses and see both EOP and MDO policies, run the following:<br>
+```powershell
+.\MDOThreatPolicyChecker.ps1 -CsvFilePath [Path\filename.csv] -IncludeMDOPolicies
+```
+
+To provide an email address and see only MDO (Safe Attachment and Safe Links) policies, run the following:<br>
+```powershell
+.\MDOThreatPolicyChecker.ps1 -EmailAddress user1@contoso.com -OnlyMDOPolicies
+```
+
+To get all mailboxes in your tenant and print out their EOP and MDO policies, run the following:<br>
+```powershell
+.\MDOThreatPolicyChecker.ps1 -IncludeMDOPolicies -EmailAddress @(Get-ExOMailbox -ResultSize unlimited | Select-Object -ExpandProperty PrimarySmtpAddress)
+```
+
+## Parameters
+
+Parameter | Description |
+----------|-------------|
+CsvFilePath | Allows you to specify a CSV file with a list of email addresses to check. Csv file must include a first line with header Email.
+EmailAddress | Allows you to specify email address or multiple addresses separated by commas.
+IncludeMDOPolicies | Checks both EOP and MDO (Safe Attachment and Safe Links) policies for user(s) specified in the CSV file or EmailAddress parameter.
+OnlyMDOPolicies | Checks only MDO (Safe Attachment and Safe Links) policies for user(s) specified in the CSV file or EmailAddress parameter.
+ShowDetailedPolicies | In addition to the policy applied, show any policy details that are set to True, On, or not blank.
+SkipConnectionCheck | Skips connection check for Graph and Exchange Online.
+SkipVersionCheck | Skips the version check of the script.
+ScriptUpdateOnly | Just updates script version to latest one.
