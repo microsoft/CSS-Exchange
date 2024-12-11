8f889c9074c0f404c446370f5f9a4e88cbc1056d
David Paulson <dpaul@microsoft.com>
2024-12-11 14:33:41 -0600

    Return Script Block vs String

--- Shared/ScriptBlock/Add-ScriptBlockInjection.ps1
+++ Shared/ScriptBlock/Add-ScriptBlockInjection.ps1
@@ -36,7 +36,6 @@
 #>
 function Add-ScriptBlockInjection {
     [CmdletBinding()]
-    [OutputType([string])]
     param(
         [Parameter(Mandatory = $true)]
         [ScriptBlock]$PrimaryScriptBlock,
@@ -163,8 +162,7 @@ function Add-ScriptBlockInjection {
                 $scriptBlockFinalized += $_.ToString() + [System.Environment]::NewLine
             }
 
-            #Need to return a string type otherwise run into issues.
-            return $scriptBlockFinalized
+            return ([ScriptBlock]::Create($scriptBlockFinalized))
         } catch {
             Write-Verbose "Failed to add to the script block"
             Invoke-CatchActionError $CatchActionFunction
