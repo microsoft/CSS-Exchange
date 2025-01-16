0fe66a9d6486d2ba965c67536f013a2c3255129b
David Paulson <dpaul@microsoft.com>
2025-01-16 13:23:51 -0600

    Add the file of Sample scenarios

---
+++ Shared/ScriptBlock/Tests/SampleScenarios.NotPublished.ps1
@@ -0,0 +1,6 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+<#
+    Sample functions that are going to be used for testing out the dev of Script Block injection for remote execution and logging
+#>
