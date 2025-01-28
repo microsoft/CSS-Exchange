16d25f87f4cdbd6df0166b8337039c30b0734cac
David Paulson <dpaul@microsoft.com>
2025-01-28 09:41:17 -0600

    Initial Add of Get-HealthCheckerDataCollection file

---
+++ Diagnostics/HealthChecker/Features/Get-HealthCheckerDataCollection.ps1
@@ -0,0 +1,10 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Get-HealthCheckerData {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$ServerNames
+    )
+}
