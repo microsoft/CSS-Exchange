55b283c6874c47f5b4681c8dafcd4e1ad0834a9e
David Paulson <dpaul@microsoft.com>
2025-01-28 14:24:21 -0600

    Initial add of Invoke-DefaultConnectExchangeShell file

---
+++ Diagnostics/HealthChecker/Helpers/Invoke-DefaultConnectExchangeShell.ps1
@@ -0,0 +1,23 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Invoke-DefaultConnectExchangeShell {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ExchangeServerName
+    )
+    process {
+        $currentWarningPreference = $WarningPreference
+        $WarningPreference = 'SilentlyContinue'
+        Import-PSSession (New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$ExchangeServerName/powershell" -Authentication Kerberos) | Out-Null
+
+        try {
+            Get-EventLogLevel -ErrorAction Stop | Out-Null
+            Write-Verbose "Successfully loaded Exchange Shell"
+        } catch {
+            throw "Failed to load Exchange Management Shell against server $ExchangeServerName."
+        }
+        $WarningPreference = $currentWarningPreference
+    }
+}
