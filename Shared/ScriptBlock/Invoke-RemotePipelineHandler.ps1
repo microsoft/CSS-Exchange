0058bf5914b5f04b4f57d0eec4b05f797cc5699c
David Paulson <dpaul@microsoft.com>
2025-01-29 15:07:35 -0600

    Move Invoke-RemotePipelineHandler to it's own file

---
+++ Shared/ScriptBlock/Invoke-RemotePipelineHandler.ps1
@@ -0,0 +1,37 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+<#
+    This function is used for when you are in a remote context to still be able to have
+    debug logging within a secondary function that you just called and returning a object from that function.
+    This then prevents all the objects from New-RemoteLoggingPipelineObject to also be stored in your variable.
+#>
+function Invoke-RemotePipelineHandler {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true)]
+        [object[]]$Object,
+
+        [Parameter(Mandatory = $true)]
+        [ref]$Result
+    )
+    process {
+        $nonLoggingInfo = New-Object System.Collections.Generic.List[object]
+        foreach ($instance in $Object) {
+            $type = $instance.RemoteLoggingType
+
+            if ($type -match "Verbose|Progress|Host") {
+                #place it back onto the pipeline
+                $instance
+            } else {
+                $nonLoggingInfo.Add($instance)
+            }
+        }
+        # If only a single result, return that vs a list.
+        if ($nonLoggingInfo.Count -eq 1) {
+            $Result.Value = $nonLoggingInfo[0]
+        } else {
+            $Result.Value = $nonLoggingInfo
+        }
+    }
+}
