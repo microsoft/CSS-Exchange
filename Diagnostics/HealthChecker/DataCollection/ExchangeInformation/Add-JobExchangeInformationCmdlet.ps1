c5fa74a7d16fc118f8e51e40bc02beadbf20f275
David Paulson <dpaul@microsoft.com>
2025-01-30 20:31:41 -0600

    add Add-JobExchangeInformationCmdlet.ps1 file

---
+++ Diagnostics/HealthChecker/DataCollection/ExchangeInformation/Add-JobExchangeInformationCmdlet.ps1
@@ -0,0 +1,23 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+. $PSScriptRoot\..\..\Helpers\Get-HCDefaultSBInjection.ps1
+. $PSScriptRoot\..\..\Helpers\Invoke-DefaultConnectExchangeShell.ps1
+
+function Add-JobExchangeInformationCmdlet {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ServerName
+    )
+    process {
+        <#
+            Non Default Script Block Dependencies
+                Invoke-DefaultConnectExchangeShell
+        #>
+        function Invoke-JobExchangeInformationCmdlet {
+
+        }
+        Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+    }
+}
