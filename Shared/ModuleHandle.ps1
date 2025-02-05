9c1cb713541583889255a54bf9bb125693d17818
iserrano76 <iserrano76@hotmail.com>
2025-02-05 16:00:29 +0100

    Installation for all users

--- Shared/ModuleHandle.ps1
+++ Shared/ModuleHandle.ps1
@@ -25,16 +25,21 @@ $requestModule = Request-Module -Modules "ExchangeOnlineManagement" -MinModuleVe
 This example checks if the "ExchangeOnlineManagement" module with a specified minimum version is installed. If it is not found, the script attempts to install it.
 #>
 
+. $PSScriptRoot\..\Shared\Confirm-Administrator.ps1
+
 function Request-Module {
     [OutputType([bool])]
     param (
         [Parameter(Mandatory = $true)]
         [string[]]$Module,
         [Parameter(Mandatory = $false)]
-        [System.Version]$MinModuleVersion = $null
+        [System.Version]$MinModuleVersion = $null,
+        [Parameter(Mandatory = $false)]
+        [switch]$InstallAllUsersIfNotAvailable
     )
 
     $noFoundError = $true
+
     foreach ($m in $Module) {
         Write-Verbose "Checking $m PowerShell Module"
         $getParams = @{
@@ -51,31 +56,39 @@ function Request-Module {
 
         if ($null -eq $installed -or $installed.Name -notcontains $m) {
             Write-Host "The following module is missing: $m" -ForegroundColor Yellow
-            $confirmed = $null
-            try {
-                Write-Verbose "Installing $m"
-                $installParams = @{
-                    Name        = $m
-                    Scope       = "CurrentUser"
-                    ErrorAction = 'Stop'
-                }
-                if ($MinModuleVersion) {
-                    $installParams["MinimumVersion"] = $MinModuleVersion
-                    Write-Verbose "with minimum version $minModuleVersion"
-                } else {
-                    Write-Verbose "without minimum version"
-                }
-                Install-Module @installParams -Force
+            if ($InstallAllUsersIfNotAvailable -and (-not (Confirm-Administrator))) {
+                Write-Warning "Module $m is not available and cannot be installed for all users because this PowerShell is not running in elevated mode."
+                $noFoundError = $false
+            } else {
+                $confirmed = $null
+                try {
+                    Write-Verbose "Installing $m"
+                    $installParams = @{
+                        Name        = $m
+                        Scope       = "CurrentUser"
+                        ErrorAction = 'Stop'
+                    }
+                    if ($InstallAllUsersIfNotAvailable) {
+                        $installParams.Scope = "AllUsers"
+                    }
+                    if ($MinModuleVersion) {
+                        $installParams["MinimumVersion"] = $MinModuleVersion
+                        Write-Verbose "with minimum version $minModuleVersion"
+                    } else {
+                        Write-Verbose "without minimum version"
+                    }
+                    Install-Module @installParams -Force
 
-                Write-Verbose "Checking $m"
-                $confirmed = Get-InstalledModule @getParams
-                if (-not $confirmed) {
-                    Write-Host "We could not install module: $m" -ForegroundColor Red
+                    Write-Verbose "Checking $m"
+                    $confirmed = Get-InstalledModule @getParams
+                    if (-not $confirmed) {
+                        Write-Host "We could not install module: $m" -ForegroundColor Red
+                        $noFoundError = $false
+                    }
+                } catch {
+                    Write-Host "Installation process fails. Error: `n$_" -ForegroundColor Red
                     $noFoundError = $false
                 }
-            } catch {
-                Write-Host "Installation process fails. Error: `n$_" -ForegroundColor Red
-                $noFoundError = $false
             }
         } else {
             Write-Verbose "Found $m module installed"
