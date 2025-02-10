32b8c928b808388ce2619712dc9fa41446f7a6e3
Andrei Chuloshnikov <1703690@gmail.com>
2025-02-10 12:59:37 +0000

    Update Test-ExchAVExclusions.ps1 - CU 15 changes
    In CU 15 we add DocParserWrapper.dll to scanningprocess , need to remove the false positive warning.

--- Diagnostics/AVTester/Test-ExchAVExclusions.ps1
+++ Diagnostics/AVTester/Test-ExchAVExclusions.ps1
@@ -370,7 +370,8 @@ while ($currentDiff -gt 0) {
         #Exchange 2019
         $ModuleAllowList.add("Microsoft.Exchange.BigFunnelFlow.28.dll")
         $ModuleAllowList.add("BigFunnel.NeuralTree.dll")
-
+        $ModuleAllowList.add("DocParserWrapper.dll")
+	
         #.NET Foundation
         $ModuleAllowList.add("Microsoft.AspNet.SignalR.Core.dll")
         $ModuleAllowList.add("Microsoft.AspNet.SignalR.SystemWeb.dll")
