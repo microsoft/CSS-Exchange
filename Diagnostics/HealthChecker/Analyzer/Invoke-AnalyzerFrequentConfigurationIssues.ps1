81f41a746a2443285853218e949dc38fe1043fb7
David Paulson <dpaul@microsoft.com>
2025-02-24 10:32:48 -0600

    Update Invoke-AnalyzerFrequentConfigurationIssues squash

--- Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerFrequentConfigurationIssues.ps1
+++ Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerFrequentConfigurationIssues.ps1
@@ -84,8 +84,11 @@ function Invoke-AnalyzerFrequentConfigurationIssues {
 
     $detailsValue = $exchangeInformation.RegistryValues.EnableEccCertificateSupport
     $displayWriteType = "Grey"
+    $isNov24SUPlus = $null
+    Test-ExchangeBuildGreaterOrEqualThanSecurityPatch -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -SUName "Nov24SU" |
+        Invoke-RemotePipelineHandler -Result ([ref]$isNov24SUPlus)
 
-    if (-not (Test-ExchangeBuildGreaterOrEqualThanSecurityPatch -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -SUName "Nov24SU") -and $detailsValue -eq "1") {
+    if (-not ($isNov24SUPlus) -and $detailsValue -eq "1") {
         $detailsValue = "1 --- Warning: On a build that doesn't support this configuration yet.`r`n`t`tMore Information: https://aka.ms/HC-EccCertificateChange"
         $displayWriteType = "Yellow"
     }
@@ -252,7 +255,8 @@ function Invoke-AnalyzerFrequentConfigurationIssues {
             [xml]$ianaTimeZoneMappingXml = $exchangeInformation.IanaTimeZoneMappingsRaw
 
             # Test IanaTimeZoneMapping.xml content to ensure it doesn't contain invalid or duplicate entries
-            $ianaTimeZoneMappingStatus = Test-IanaTimeZoneMapping -IanaMappingFile $ianaTimeZoneMappingXml
+            $ianaTimeZoneMappingStatus = $null
+            Test-IanaTimeZoneMapping -IanaMappingFile $ianaTimeZoneMappingXml | Invoke-RemotePipelineHandler -Result ([ref]$ianaTimeZoneMappingStatus)
 
             $ianaTimeZoneStatusMissingAttributes = $ianaTimeZoneMappingStatus.NodeMissingAttributes
             $ianaTimeZoneStatusDuplicateEntries = $ianaTimeZoneMappingStatus.DuplicateEntries
@@ -297,9 +301,15 @@ function Invoke-AnalyzerFrequentConfigurationIssues {
             $displayWriteType = "Red"
             $displayValue = "Error --- Accepted Domain `"$($wildCardAcceptedDomain.Id)`" is set to a Wild Card (*) Domain Name with a domain type of $($wildCardAcceptedDomain.DomainType.ToString()). This is not recommended as this is an open relay for the entire environment.`r`n`t`tMore Information: https://aka.ms/HC-OpenRelayDomain"
 
+            $isE16CU22Plus = $null
+            $isE19CU11Plus = $null
+            Test-ExchangeBuildGreaterOrEqualThanBuild -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -Version "Exchange2016" -CU "CU22" |
+                Invoke-RemotePipelineHandler -Result ([ref]$isE16CU22Plus)
+            Test-ExchangeBuildGreaterOrEqualThanBuild -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -Version "Exchange2019" -CU "CU11" |
+                Invoke-RemotePipelineHandler -Result ([ref]$isE19CU11Plus)
+
             if ($wildCardAcceptedDomain.DomainType.ToString() -eq "InternalRelay" -and
-                ((Test-ExchangeBuildGreaterOrEqualThanBuild -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -Version "Exchange2016" -CU "CU22") -or
-                (Test-ExchangeBuildGreaterOrEqualThanBuild -CurrentExchangeBuild $exchangeInformation.BuildInformation.VersionInformation -Version "Exchange2019" -CU "CU11"))) {
+                ($isE16CU22Plus -or $isE19CU11Plus)) {
                 $additionalDisplayValue = "`r`n`t`tERROR: You have an open relay set as Internal Replay Type and on a CU that is known to cause issues with transport services crashing. Follow the above article for more information."
             } elseif ($wildCardAcceptedDomain.DomainType.ToString() -eq "InternalRelay") {
                 $additionalDisplayValue = "`r`n`t`tWARNING: You have an open relay set as Internal Relay Type. You are not on a CU yet that is having issue, recommended to change this prior to upgrading. Follow the above article for more information."
