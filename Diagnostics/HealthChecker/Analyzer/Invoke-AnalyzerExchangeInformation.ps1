2f5ae7dcaae25caf1ad397304041f283b4afa8b9
David Paulson <dpaul@microsoft.com>
2025-03-12 09:46:44 -0500

    Update Invoke-AnalyzerExchangeInformation squash

--- Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerExchangeInformation.ps1
+++ Diagnostics/HealthChecker/Analyzer/Invoke-AnalyzerExchangeInformation.ps1
@@ -627,7 +627,7 @@ function Invoke-AnalyzerExchangeInformation {
             Add-AnalyzedResultInformation @params
 
             if ($getExchangeServer.FeaturesEnabled.Count -gt 0) {
-                $details = ([string]::Join(", ", $getExchangeServer.FeaturesEnabled))
+                $details = ([string]::Join(", ", @($getExchangeServer.FeaturesEnabled)))
             } else {
                 $details = "None Enabled"
             }
@@ -641,28 +641,28 @@ function Invoke-AnalyzerExchangeInformation {
             if ($getExchangeServer.FeaturesApproved.Count -gt 0) {
                 $params = $flightingBaseParams + @{
                     Name    = "Features Approved"
-                    Details = ([string]::Join(", ", $getExchangeServer.FeaturesApproved))
+                    Details = ([string]::Join(", ", @($getExchangeServer.FeaturesApproved)))
                 }
                 Add-AnalyzedResultInformation @params
             }
             if ($getExchangeServer.FeaturesAwaitingAdminApproval.Count -gt 0) {
                 $params = $flightingBaseParams + @{
                     Name    = "Features Awaiting Admin Approval"
-                    Details = ([string]::Join(", ", $getExchangeServer.FeaturesAwaitingAdminApproval))
+                    Details = ([string]::Join(", ", @($getExchangeServer.FeaturesAwaitingAdminApproval)))
                 }
                 Add-AnalyzedResultInformation @params
             }
             if ($getExchangeServer.FeaturesBlocked.Count -gt 0) {
                 $params = $flightingBaseParams + @{
                     Name    = "Features Blocked"
-                    Details = ([string]::Join(", ", $getExchangeServer.FeaturesBlocked))
+                    Details = ([string]::Join(", ", @($getExchangeServer.FeaturesBlocked)))
                 }
                 Add-AnalyzedResultInformation @params
             }
             if ($getExchangeServer.FeaturesDisabled.Count -gt 0) {
                 $params = $flightingBaseParams + @{
                     Name    = "Features Disabled"
-                    Details = ([string]::Join(", ", $getExchangeServer.FeaturesDisabled))
+                    Details = ([string]::Join(", ", @($getExchangeServer.FeaturesDisabled)))
                 }
                 Add-AnalyzedResultInformation @params
             }
