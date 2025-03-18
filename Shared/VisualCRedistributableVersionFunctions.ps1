69311a1b3b39b301f3e56e9dac432196d77f2f64
David Paulson <dpaul@microsoft.com>
2025-03-18 13:52:45 -0500

    Update VisualCRedistributableVersionFunctions to handle Remote Pipeline - squash

--- Shared/VisualCRedistributableVersionFunctions.ps1
+++ Shared/VisualCRedistributableVersionFunctions.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 . $PSScriptRoot\Invoke-ScriptBlockHandler.ps1
-. $PSScriptRoot\ScriptBlock\Invoke-RemotePipelineHandler.ps1
+. $PSScriptRoot\ScriptBlock\RemotePipelineHandlerFunctions.ps1
 
 function Get-VisualCRedistributableInstalledVersion {
     [CmdletBinding()]
