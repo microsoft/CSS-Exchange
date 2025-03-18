7e16b1ee6a36299c4b97e390c57855e0a6783163
David Paulson <dpaul@microsoft.com>
2025-03-18 14:06:45 -0500

    Update Get-WmiObjectCriticalHandler to handle Remote Pipeline - squash

--- Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
+++ Diagnostics/HealthChecker/DataCollection/ServerInformation/Get-WmiObjectCriticalHandler.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 . $PSScriptRoot\..\..\..\..\Shared\Get-WmiObjectHandler.ps1
-. $PSScriptRoot\..\..\..\..\Shared\ScriptBlock\Invoke-RemotePipelineHandler.ps1
+. $PSScriptRoot\..\..\..\..\Shared\ScriptBlock\RemotePipelineHandlerFunctions.ps1
 
 function Get-WmiObjectCriticalHandler {
     [CmdletBinding()]
