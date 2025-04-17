6fa09adb59c0413afc61b11a21f2ffa416b5cde7
David Paulson <dpaul@microsoft.com>
2025-04-17 17:00:49 -0500

    update Invoke-HealthCheckerMainReport squash

--- Diagnostics/HealthChecker/Features/Invoke-HealthCheckerMainReport.ps1
+++ Diagnostics/HealthChecker/Features/Invoke-HealthCheckerMainReport.ps1
@@ -3,6 +3,7 @@
 
 . $PSScriptRoot\Get-HealthCheckerDataCollection.ps1
 . $PSScriptRoot\Get-HealthCheckerData.ps1
+. $PSScriptRoot\..\Analyzer\Invoke-AnalyzerEngineHandler.ps1
 # The main functionality of Exchange Health Checker.
 # Collect information and report it to the screen and export out the results.
 function Invoke-HealthCheckerMainReport {
@@ -27,6 +28,17 @@ function Invoke-HealthCheckerMainReport {
     }
 
     Invoke-ErrorCatchActionLoopFromIndex $currentErrors
-    Get-HealthCheckerDataCollection $ServerNames
+    $hcDataCollection = Get-HealthCheckerDataCollection $ServerNames
+    $analyzedEngineResults = Invoke-AnalyzerEngineHandler -ServerDataCollection $hcDataCollection -RunType "StartNow"
+
+    # TODO: Handle this better.
+    $stopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    foreach ($key in $analyzedEngineResults.Keys) {
+        $serverName = $analyzedEngineResults[$key].HealthCheckerExchangeServer.ServerName
+        Invoke-SetOutputInstanceLocation -Server $serverName -FileName "HealthChecker" -IncludeServerName $true
+        Write-ResultsToScreen -ResultsToWrite $analyzedEngineResults[$key].DisplayResults
+    }
+    Write-Verbose "Writing the results to the screen took $($stopWatch.Elapsed.TotalSeconds) seconds." -Verbose
+
     # Get-HealthCheckerData $ServerNames $EdgeServer
 }
