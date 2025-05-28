6389358fdfaaf970d5e390983d1afd54fe6b4864
David Paulson <dpaul@microsoft.com>
2025-05-28 13:16:27 -0500

    Adding Save Debug Object for Diagnostics

---
+++ Shared/ScriptDebugFunctions.ps1
@@ -0,0 +1,34 @@
+﻿# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+function Get-DebugObject {
+    [CmdletBinding()]
+    param()
+    process {
+        if ($null -eq $Script:savedDebugObject) {
+            Write-Verbose "Creating Get-DebugObject Hashtable"
+            $Script:savedDebugObject = @{}
+        }
+        $Script:savedDebugObject
+    }
+}
+
+function Add-DebugObject {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$ObjectKeyName,
+
+        [Parameter(Mandatory = $false)]
+        [object]$ObjectValueEntry
+    )
+    begin {
+        $getDebugObject = Get-DebugObject
+    }
+    process {
+        if (-not ($getDebugObject.ContainsKey($ObjectKeyName))) {
+            $getDebugObject.Add($ObjectKeyName, (New-Object System.Collections.Generic.List[object]))
+        }
+        $getDebugObject[$ObjectKeyName].Add($ObjectValueEntry)
+    }
+}
