2556790a18838d157a4e7226fdb78e35b3865b2f
David Paulson <dpaul@microsoft.com>
2025-06-04 13:36:41 -0500

    Improve Wait-JobQueue to auto do Write-Progress and add additional debug logging

--- Shared/JobManagementFunctions/Wait-JobQueue.ps1
+++ Shared/JobManagementFunctions/Wait-JobQueue.ps1
@@ -10,12 +10,6 @@
 .DESCRIPTION
     From all the jobs that were added to the queue from Add-JobQueue, it will attempt to execute all the jobs and store them back in the hashtable
     This is the function that you call to execute a batch of jobs that you wish to complete before moving onto the next job.
-    TODO:
-        Add in logic for Write-Progress, we want to know how many jobs are running and a summary of what they are.
-        Improve the debug logic here
-        Add in timeout logic to timeout job requests
-        Add in logic for maxJobsRunning
-        Add in Read ME page for this section of code
 #>
 
 function Wait-JobQueue {
@@ -32,6 +26,9 @@ function Wait-JobQueue {
         if ($getJobQueue.Count -eq 0) {
             throw "No Jobs in Queue"
         }
+
+        Write-Verbose "Calling: $($MyInvocation.MyCommand)"
+        $stopwatchMain = [System.Diagnostics.Stopwatch]::StartNew()
     }
     process {
         do {
@@ -42,16 +39,43 @@ function Wait-JobQueue {
                 Invoke-TryStartJobQueue
             }
 
-            # Check all the current jobs running to see if they have finished
-            [array]$completedJobsToProcess = $getJobQueue.Values | Where-Object { $null -ne $_.Job -and $_.JobEndTime -eq [DateTime]::MinValue  -and $_.Job.State -ne "Running" }
+            [array]$completedJobs = $getJobQueue.Values | Where-Object { $null -ne $_.Job -and $_.JobEndTime -ne [DateTime]::MinValue -and $_.Job.State -eq "Completed" }
+            [array]$completedJobsToProcess = $getJobQueue.Values | Where-Object { $null -ne $_.Job -and $_.JobEndTime -eq [DateTime]::MinValue -and $_.Job.State -ne "Running" }
 
-            if ($completedJobsToProcess.Count -gt 0) {
+            $jobQueueProgressParams = @{
+                Activity         = "Waiting for $($getJobQueue.Values.Count) Total Jobs to Complete"
+                Status           = "$($completedJobs.Count) jobs completed. Running for $($stopwatchMain.Elapsed.TotalSeconds) seconds."
+                CurrentOperation = [string]::Empty
+                ParentId         = 0
+                Id               = 1
+                PercentComplete  = ($completedJobs.Count * 100 / $getJobQueue.Values.Count)
+            }
+
+            if ($completedJobsToProcess.Count -eq 0) {
+                Write-Progress @jobQueueProgressParams
+            } else {
+                $jobReceivedCompleted = 0
                 foreach ($jobInfo in $completedJobsToProcess) {
                     $JobError = $null
+                    $jobReceivedCompleted++
+                    $jobQueueProgressParams.CurrentOperation = "Receiving Job '$($jobInfo.JobId)' $jobReceivedCompleted of $($completedJobsToProcess.Count)"
+                    Write-Progress @jobQueueProgressParams
+
                     Write-Verbose "Attempting to receive job $($jobInfo.JobId)"
                     $jobInfo.JobEndTime = [DateTime]::Now
+                    $receiveJobStopWatch = [System.Diagnostics.Stopwatch]::StartNew()
                     $result = Receive-Job $jobInfo.Job -ErrorVariable "JobError"
+                    Write-Verbose "Receive-Job took $($receiveJobStopWatch.Elapsed.TotalSeconds) seconds to complete"
+
+                    if ($jobInfo.Job.ChildJobs.Progress.Count -gt 1) {
+                        $lastProgress = $jobInfo.Job.ChildJobs.Progress | Select-Object -Last 1
+                        if ($lastProgress) {
+                            Write-Progress -Activity $lastProgress.Activity -ParentId 1 -Id ($jobInfo.Job.Id + 1) -Completed
+                        }
+                    }
+
                     Write-Verbose "Successfully received the job"
+
                     if ($null -ne $ProcessReceiveJobAction -and
                         $null -ne $result) {
                         $stopWatch = [System.Diagnostics.Stopwatch]::StartNew()
@@ -73,5 +97,7 @@ function Wait-JobQueue {
             Start-Sleep 1
             $continue = $null -ne ($getJobQueue.Values | Where-Object { $_.JobEndTime -eq [DateTime]::MinValue })
         } while ($continue)
+        Write-Progress @jobQueueProgressParams -Completed
+        Write-Verbose "End $($MyInvocation.MyCommand) and took $($stopwatchMain.Elapsed.TotalSeconds) seconds"
     }
 }
