594ee3218f95ced14e4505144280fe9a3b81f798
David Paulson <dpaul@microsoft.com>
2025-08-07 16:24:11 -0500

    Revert "Configure 3p to have HU to be SU"
    This reverts commit 479aac0129b9e6ae8bc46cda91ed10ad05c727bc.

--- Hybrid/ConfigureExchangeHybridApplication/ConfigureExchangeHybridApplication.ps1
+++ Hybrid/ConfigureExchangeHybridApplication/ConfigureExchangeHybridApplication.ps1
@@ -459,7 +459,7 @@ begin {
         Write-Verbose "Script was run to only configure TargetSharingEpr or AuthServer - dedicated Exchange hybrid application feature build check will be skipped"
     } elseif ($isAutomatedCertificateUpload -or
         $Script:EnableExchangeHybridApplicationOverride) {
-        $exchangeServersList = Get-ProcessedServerList -MinimumSU "Apr25SU" -DisplayOutdatedServers $false
+        $exchangeServersList = Get-ProcessedServerList -MinimumSU "Apr25HU" -DisplayOutdatedServers $false
 
         $isLocalServerMailboxServer = ($exchangeServersList.GetExchangeServer | Where-Object {
                 $_.Fqdn -eq $localServerFqdn
