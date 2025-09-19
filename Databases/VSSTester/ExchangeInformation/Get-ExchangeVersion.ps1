63a2fb325f131709fd3cdccfd1d5dee86e4d6a67
satyamtripathii <satyamtripathi798@gmail.com>
2025-09-20 01:26:22 +0530

    Fix #2289: Align Get-ExchangeVersion with Shared function and pass SpellCheck

--- Databases/VSSTester/ExchangeInformation/Get-ExchangeVersion.ps1
+++ Databases/VSSTester/ExchangeInformation/Get-ExchangeVersion.ps1
@@ -11,36 +11,29 @@ function Get-ExchangeVersion {
 
     Write-Host "$(Get-Date) Verifying Exchange version..."
     $exchVer = (Get-ExchangeServer $ServerName).AdminDisplayVersion
-    if ($exchVer -is [string]) {
-    # Agar string hai (deserialize ho gaya), split karke major/minor nikal lo
-    $parts = $exchVer.Split('.')
-    $exchVerMajor = [int]$parts[0]
-    $exchVerMinor = if ($parts.Count -gt 1) { [int]$parts[1] } else { 0 }
-} else {
-    # Agar object hai, normal property access
-    $exchVerMajor = $exchVer.Major
-    $exchVerMinor = $exchVer.Minor
-}
 
+    # Handle both deserialized string values and proper System.Version objects
+    if ($exchVer -is [string]) {
+        $parts = $exchVer.Split('.')
+        $exchVerMajor = [int]$parts[0]
+        $exchVerMinor = if ($parts.Count -gt 1) { [int]$parts[1] } else { 0 }
+    }
+    else {
+        $exchVerMajor = $exchVer.Major
+        $exchVerMinor = $exchVer.Minor
+    }
 
     switch ($exchVerMajor) {
-        "14" {
+        14 {
             $exchVer = "2010"
         }
-        "15" {
+        15 {
             switch ($exchVerMinor) {
-                "0" {
-                    $exchVer = "2013"
-                }
-                "1" {
-                    $exchVer = "2016"
-                }
-                "2" {
-                    $exchVer = "2019"
-                }
+                0 { $exchVer = "2013" }
+                1 { $exchVer = "2016" }
+                2 { $exchVer = "2019" }
             }
         }
-
         default {
             Write-Host "  This script is only for Exchange 2013, 2016, and 2019 servers."
             exit
